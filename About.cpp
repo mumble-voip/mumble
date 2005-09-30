bd5f2d42302e5300d13abb116b3c36b2d5c58061
Thorvald Natvig <slicer@users.sourceforge.net>
2005-09-30 00:35:53 +0000

    Version to 0.9.1
    git-svn-id: https://mumble.svn.sourceforge.net/svnroot/mumble/trunk@247 05730e5d-ab1b-0410-a4ac-84af385074fa

--- About.cpp
+++ About.cpp
@@ -57,7 +57,7 @@ AboutDialog::AboutDialog(QWidget *p) : QDialog(p) {
 
 	QLabel *text=new QLabel(about);
 	text->setText(tr(
-		"<h3>Mumble v0.9.0</h3>"
+		"<h3>Mumble v0.9.1</h3>"
 		"<p>Copyright 2005 Thorvald Natvig<br />slicer@users.sourceforge.net</p>"
 		"<p><b>A voicechat utility for gamers</b></p>"
 		"<p><tt>http://mumble.sourceforge.net/</tt></p>"
