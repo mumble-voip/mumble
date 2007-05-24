3be4aec86a167f6faa262a2baf3a32a912046e5f
Thorvald Natvig <slicer@users.sourceforge.net>
2007-05-24 09:06:39 +0000

    No longer manually reset preprocessor loudness.
    git-svn-id: https://mumble.svn.sourceforge.net/svnroot/mumble/trunk@443 05730e5d-ab1b-0410-a4ac-84af385074fa

--- AudioInput.cpp
+++ AudioInput.cpp
@@ -234,10 +234,8 @@ void AudioInput::encodeAudioFrame() {
 			if (sesEcho)
 				speex_echo_state_destroy(sesEcho);
 			sesEcho = speex_echo_state_init(iFrameSize, iFrameSize*20);
-#ifndef SPEEX_ANCIENT
 			iArg = SAMPLE_RATE;
 			speex_echo_ctl(sesEcho, SPEEX_SET_SAMPLING_RATE, &iArg);
-#endif
 			qWarning("AudioInput: ECHO CANCELLER ACTIVE");
 		}
 
