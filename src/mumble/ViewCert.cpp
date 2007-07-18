990f22073eb217133cb0aaffeeab0ae493cb8c48
Thorvald Natvig <slicer@users.sourceforge.net>
2007-07-18 21:35:07 +0000

    Proper certificate viewer.
    git-svn-id: https://mumble.svn.sourceforge.net/svnroot/mumble/trunk@628 05730e5d-ab1b-0410-a4ac-84af385074fa

---
+++ src/mumble/ViewCert.cpp
@@ -0,0 +1,103 @@
+/* Copyright (C) 2005-2007, Thorvald Natvig <thorvald@natvig.com>
+
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   - Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+   - Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+   - Neither the name of the Mumble Developers nor the names of its
+     contributors may be used to endorse or promote products derived from this
+     software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "ViewCert.h"
+
+ViewCert::ViewCert(QList<QSslCertificate> cl, QWidget *p) : QDialog(p) {
+	qlCerts = cl;
+
+	setWindowTitle(tr("Certificate Chain Details"));
+
+	QHBoxLayout *h;
+	QVBoxLayout *v;
+	QGroupBox *qcbChain, *qcbDetails;
+
+	qcbChain=new QGroupBox(tr("Certificate chain"));
+	qcbDetails=new QGroupBox(tr("Certifitcate details"));
+
+	h = new QHBoxLayout();
+	qlwChain = new QListWidget();
+	qlwChain->setObjectName(QLatin1String("Chain"));
+
+	foreach(QSslCertificate c, qlCerts)
+		qlwChain->addItem(tr("%1 %2").arg(c.subjectInfo(QSslCertificate::CommonName)).arg(c.subjectInfo(QSslCertificate::Organization)));
+
+	h->addWidget(qlwChain);
+	qcbChain->setLayout(h);
+
+	h = new QHBoxLayout();
+	qlwCert = new QListWidget();
+	h->addWidget(qlwCert);
+	qcbDetails->setLayout(h);
+
+	QDialogButtonBox *qdbb = new QDialogButtonBox(QDialogButtonBox::Ok);
+
+	v = new QVBoxLayout();
+	v->addWidget(qcbChain);
+	v->addWidget(qcbDetails);
+	v->addWidget(qdbb);
+
+	setLayout(v);
+
+	QMetaObject::connectSlotsByName(this);
+	connect(qdbb, SIGNAL(accepted()), this, SLOT(accept()));
+
+	resize(500,300);
+}
+
+void ViewCert::on_Chain_currentRowChanged(int idx) {
+	qlwCert->clear();
+	if ((idx < 0) || (idx >= qlCerts.size()))
+		return;
+
+	QStringList l;
+	const QSslCertificate &c=qlCerts.at(idx);
+	l << tr("Common Name: %1").arg(c.subjectInfo(QSslCertificate::CommonName));
+        l << tr("Organization: %1").arg(c.subjectInfo(QSslCertificate::Organization));
+	l << tr("Subunit: %1").arg(c.subjectInfo(QSslCertificate::OrganizationalUnitName));
+	l << tr("Country: %1").arg(c.subjectInfo(QSslCertificate::CountryName));
+	l << tr("Locality: %1").arg(c.subjectInfo(QSslCertificate::LocalityName));
+	l << tr("State: %1").arg(c.subjectInfo(QSslCertificate::StateOrProvinceName));
+	l << tr("Valid from: %1").arg(c.effectiveDate().toString());
+	l << tr("Valid to: %1").arg(c.expiryDate().toString());
+	l << tr("Serial: %1").arg(QString::fromLatin1(c.serialNumber().toHex()));
+	l << tr("Public Key: %1 bits %2").arg(c.publicKey().length()).arg((c.publicKey().algorithm() == QSsl::Rsa) ? tr("RSA") : tr("DSA"));
+	l << tr("Digest (MD5): %1").arg(QString::fromLatin1(c.digest().toHex()));
+	l << QString();
+	l << tr("Issued by:");
+	l << tr("Common Name: %1").arg(c.issuerInfo(QSslCertificate::CommonName));
+	l << tr("Organization: %1").arg(c.issuerInfo(QSslCertificate::Organization));
+	l << tr("Unit Name: %1").arg(c.issuerInfo(QSslCertificate::OrganizationalUnitName));
+	l << tr("Country: %1").arg(c.issuerInfo(QSslCertificate::CountryName));
+	l << tr("Locality: %1").arg(c.issuerInfo(QSslCertificate::LocalityName));
+	l << tr("State: %1").arg(c.issuerInfo(QSslCertificate::StateOrProvinceName));
+
+	qlwCert->addItems(l);
+}
