04d7e5fe86558b31a9b1eb712d54c05320a27fc2
Thorvald Natvig <slicer@users.sourceforge.net>
2007-07-19 18:27:14 +0000

    LARGE_INTEGER fixes for murmur
    git-svn-id: https://mumble.svn.sourceforge.net/svnroot/mumble/trunk@638 05730e5d-ab1b-0410-a4ac-84af385074fa

--- src/murmur/murmur_pch.h
+++ src/murmur/murmur_pch.h
@@ -1,5 +1,10 @@
 #ifndef _MURMUR_PCH_H
 #define _MURMUR_PCH_H
+#ifdef WIN32
+#define _WIN32_IE 0x0600
+#include <windows.h>
+#include <shellapi.h>
+#endif
 #include <QtCore>
 #include <QtNetwork>
 #include <QtSql>
