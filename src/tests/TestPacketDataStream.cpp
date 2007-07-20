3fdb39f8d7f08face93e7b19c51cd5c266aa5348
Thorvald Natvig <slicer@users.sourceforge.net>
2007-07-20 01:28:59 +0000

    Test indenting
    git-svn-id: https://mumble.svn.sourceforge.net/svnroot/mumble/trunk@647 05730e5d-ab1b-0410-a4ac-84af385074fa

--- src/tests/TestPacketDataStream.cpp
+++ src/tests/TestPacketDataStream.cpp
@@ -6,7 +6,7 @@
 #include "Message.h"
 
 class TestPacketDataStream : public QObject {
-	Q_OBJECT
+		Q_OBJECT
 	private slots:
 		void integer();
 		void integer_data();
@@ -17,11 +17,11 @@ class TestPacketDataStream : public QObject {
 
 void TestPacketDataStream::integer_data() {
 	QTest::addColumn<quint64>("value");
-	for(int i=0;i<64;i++) {
+	for (int i=0;i<64;i++) {
 		quint64 v = 1ULL << i;
 		QTest::newRow("Integer") << v;
 		QTest::newRow("~Integer") << ~v;
-	}		
+	}
 }
 
 void TestPacketDataStream::integer() {
@@ -33,7 +33,7 @@ void TestPacketDataStream::integer() {
 	out << value;
 	PacketDataStream in(buff, out.size());
 	in >> v;
-	
+
 	QCOMPARE(value, v);
 	QVERIFY(in.isValid());
 	QVERIFY(in.left() == 0);
@@ -48,10 +48,10 @@ void TestPacketDataStream::string_data() {
 
 void TestPacketDataStream::string() {
 	QFETCH(QString, string);
-	
+
 	char buff[256];
 	QString v;
-	
+
 	PacketDataStream out(buff, 256);
 	out << string;
 	PacketDataStream in(buff, out.size());
@@ -63,30 +63,30 @@ void TestPacketDataStream::string() {
 
 void TestPacketDataStream::space() {
 	char buff[256];
-	for(int i=0;i<256;i++)
+	for (int i=0;i<256;i++)
 		buff[i]= 0x55;
 
 	PacketDataStream out(buff, 1);
-	
+
 	char val = -2;
-	
+
 	out << val;
 	QVERIFY(out.isValid());
 	QVERIFY(out.size() == 1);
 	QVERIFY(out.left() == 0);
 	QVERIFY(buff[1] == 0x55);
-	
+
 	out << val;
-	
+
 	QVERIFY(! out.isValid());
 	QVERIFY(out.size() == 1);
 	QVERIFY(out.left() == 0);
 	QVERIFY(buff[1] == 0x55);
-	
+
 	PacketDataStream in(buff, 1);
-	
+
 	char v;
-	
+
 	in >> v;
 	QCOMPARE(v, val);
 	QVERIFY(in.isValid());
