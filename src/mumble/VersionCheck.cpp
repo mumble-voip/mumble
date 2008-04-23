83793af46879d323e6310cd75e699409e7b27b35
Mikkel Krautz <mkrautz@users.sourceforge.net>
2008-04-23 15:47:33 +0000

    Add VersionCheck QueryItem for OS X.
    git-svn-id: https://mumble.svn.sourceforge.net/svnroot/mumble/trunk@1106 05730e5d-ab1b-0410-a4ac-84af385074fa

--- src/mumble/VersionCheck.cpp
+++ src/mumble/VersionCheck.cpp
@@ -42,8 +42,10 @@ VersionCheck::VersionCheck(bool autocheck, QObject *p) : QObject(p) {
 	quUrl.addQueryItem(QLatin1String("ver"), QLatin1String(QUrl::toPercentEncoding(QLatin1String(MUMBLE_RELEASE))));
 	quUrl.addQueryItem(QLatin1String("date"), QLatin1String(QUrl::toPercentEncoding(QLatin1String(__DATE__))));
 	quUrl.addQueryItem(QLatin1String("time"), QLatin1String(QUrl::toPercentEncoding(QLatin1String(__TIME__))));
-#ifdef Q_OS_WIN
+#if defined(Q_OS_WIN)
 	quUrl.addQueryItem(QLatin1String("os"), QLatin1String("Win32"));
+#elif defined(Q_OS_MAC)
+	quUrl.addQueryItem(QLatin1String("os"), QLatin1String("MacOSX"));
 #endif
 	if (autocheck)
 		quUrl.addQueryItem(QLatin1String("auto"), QLatin1String("1"));
