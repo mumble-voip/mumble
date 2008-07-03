38b6224106a462d635d7dbc53669e8018ecf6ff8
Thorvald Natvig <slicer@users.sourceforge.net>
2008-07-03 12:34:38 +0000

    Include weblist.php using ICE
    git-svn-id: https://mumble.svn.sourceforge.net/svnroot/mumble/trunk@1191 05730e5d-ab1b-0410-a4ac-84af385074fa

---
+++ scripts/weblist.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>Userlist</title>
+</head>
+<body>
+<?php
+
+Ice_loadProfile();
+
+try {
+  $base = $ICE->stringToProxy("Meta:tcp -h 127.0.0.1 -p 49152");
+  $meta = $base->ice_checkedCast("::Murmur::Meta");
+
+  $servers = $meta->getBootedServers();
+  $default = $meta->getDefaultConf();
+  foreach($servers as $s) {
+    $name = $s->getConf("registername");
+    if (! $name) {
+      $name =  $default["registername"];
+    }
+    echo "<h1>SERVER #" . $s->id() . " " .$name ."</h1>\n";
+    echo "<table><tr><th>Name</th><th>Channel</th></tr>\n";
+    
+    $channels = $s->getChannels();
+    $players = $s->getPlayers();
+    
+    foreach($players as $id => $state) {
+      $chan = $channels[$state->channel];
+      echo "<tr><td>".$state->name."</td><td>".$chan->name."</td></tr>\n";
+    }
+    echo "</table>\n";
+  }
+} catch (Ice_LocalException $ex) {
+  print_r($ex);
+}
+
+?>
+</body>
+</html>
