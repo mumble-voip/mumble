29c04642a482d61614cc8ef0e68cd3d6de5d0243
Thorvald Natvig <slicer@users.sourceforge.net>
2009-02-08 14:20:18 +0000

    Patch #2579216: cod5 new version 1.2 pa patch [dd0t]
    git-svn-id: https://mumble.svn.sourceforge.net/svnroot/mumble/trunk@1500 05730e5d-ab1b-0410-a4ac-84af385074fa

--- plugins/cod5/cod5.cpp
+++ plugins/cod5/cod5.cpp
@@ -38,7 +38,7 @@ static bool peekProc(VOID *base, VOID *dest, SIZE_T len) {
 }
 
 static void about(HWND h) {
-	::MessageBox(h, L"Reads audio position information from COD5 (v 1.0.1017)", L"Mumble COD5 Plugin", MB_OK);
+	::MessageBox(h, L"Reads audio position information from COD5 (v 1.0.1054)", L"Mumble COD5 Plugin", MB_OK);
 }
 
 
@@ -56,15 +56,15 @@ static int fetch(float *pos, float *front, float *top) {
 
 			Address			Type	Description
 			===================================
-			0x008CD034		float	Z-Coordinate
-			0x008CD038		float	X-Coordinate
-			0x008CD03C		float	Y-Coordinate
-			0x008CD044		float	Horizontal view (degrees)
-			0x008CD040		float	Vertical view (degrees)
+			0x008D1034		float	Z-Coordinate
+			0x008D1038		float	X-Coordinate
+			0x008D103C		float	Y-Coordinate
+			0x008D1044		float	Horizontal view (degrees)
+			0x008D1040		float	Vertical view (degrees)
 
-			0x0095EAAC		byte	Magical state value
+			0x00982AE4		byte	Magical state value
 	*/
-	ok = peekProc((BYTE *) 0x0095EAAC, &state, 1); // Magical state value
+	ok = peekProc((BYTE *) 0x00982AE4, &state, 1); // Magical state value
 	if (! ok)
 		return false;
 	/*
@@ -79,11 +79,11 @@ static int fetch(float *pos, float *front, float *top) {
 	if (state != 4)
 		return true; // This results in all vectors beeing zero which tells mumble to ignore them.
 
-	ok = peekProc((BYTE *) 0x008CD034, pos+2, 4) &&	//Z
-	     peekProc((BYTE *) 0x008CD038, pos, 4) &&	//X
-	     peekProc((BYTE *) 0x008CD03C, pos+1, 4) && //Y
-	     peekProc((BYTE *) 0x008CD044, &viewHor, 4) && //Hor
-	     peekProc((BYTE *) 0x008CD040, &viewVer, 4); //Ver
+	ok = peekProc((BYTE *) 0x008D1034, pos+2, 4) &&	//Z
+	     peekProc((BYTE *) 0x008D1038, pos, 4) &&	//X
+	     peekProc((BYTE *) 0x008D103C, pos+1, 4) && //Y
+	     peekProc((BYTE *) 0x008D1044, &viewHor, 4) && //Hor
+	     peekProc((BYTE *) 0x008D1040, &viewVer, 4); //Ver
 
 	if (! ok)
 		return false;
@@ -155,7 +155,7 @@ static void unlock() {
 
 static MumblePlugin cod5plug = {
 	MUMBLE_PLUGIN_MAGIC,
-	L"Call of Duty 5 v1.0.1017",
+	L"Call of Duty 5 v1.0.1054",
 	L"Call of Duty 5",
 	about,
 	NULL,
