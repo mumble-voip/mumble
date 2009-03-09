080c5867bab4b4d4c0ec8e9116d07339cb7ed607
Thorvald Natvig <slicer@users.sourceforge.net>
2009-03-09 23:40:53 +0100

    Create .ini files based on template

---
+++ scripts/mkini.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+cp murmur.ini murmur.ini.win32
+cp murmur.ini murmur.ini.osx
+cp murmur.ini murmur.ini.system
+
+perl -pi -e 's/^(#|)ice=/ice=/' murmur.ini.win32
+
+perl -pi -e 's/^(#|)ice=/ice=/' murmur.ini.osx
+perl -pi -e 's/^(#|)dbus=/#dbus=/' murmur.ini.osx
+
+perl -pi -e 's/^(#|)ice=/ice=/' murmur.ini.system
+perl -pi -e 's/^(#|)dbus=.*$/dbus=system/' murmur.ini.system
+perl -pi -e 's/^(#|)logfile=.*$/logfile=\/var\/log\/mumble-server\/mumble-server.log/' murmur.ini.system
+perl -pi -e 's/^(#|)pidfile=.*$/pidfile=\/var\/run\/mumble-server\/mumble-server.pid/' murmur.ini.system
+perl -pi -e 's/^(#|)database=.*$/database=\/var\/lib\/mumble-server\/mumble-server.sqlite/' murmur.ini.system
+perl -pi -e 's/^(#|)uname=.*$/uname=mumble-server/' murmur.ini.system
--- scripts/release.pl
+++ scripts/release.pl
@@ -29,7 +29,9 @@ my $ver;
 my %filevars = ( 'sources' => 1, 'headers' => 1, 'rc_file' => 1, 'dist' => 1, 'forms' => 1, 'resources' => 1, 'precompiled_header' => 1, 'translations' => 1);
 
 system("rm mumble-*");
-
+chdir("scripts");
+system("bash mkini.sh");
+chdir("..");
 
 my @pro = ("main.pro");
 #, "src/mumble.pri");
