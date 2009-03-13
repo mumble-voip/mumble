a9775463f68e465c193e3f129d7996008ba3fd4c
Mikkel Krautz <mikkel@krautz.dk>
2009-03-13 16:20:51 +0100

    Remove old osxdist.sh script.

--- scripts/osxdist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ver=`cat src/Version.h | grep '#define MUMBLE_VERSION' | sed 's,.*MUMBLE_VERSION\ ,,'`
-if [ "$ver" != "" ]; then
-	python scripts/osxdist.py $ver
-else
-	python scripts/osxdist.py
-fi
