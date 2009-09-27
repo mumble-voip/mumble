e9b84b4d68fce4801f35d0fd60292e9c2cf990f9
Mikkel Krautz <mikkel@krautz.dk>
2009-09-27 14:41:42 +0200

    Don't do TextMessage symlinks in mklinks.sh

--- src/mumble11x/mklinks.sh
+++ src/mumble11x/mklinks.sh
@@ -1,5 +1,5 @@
 
-for F in ASIOInput.h ASIOInput.cpp DirectSound.h DirectSound.cpp WASAPI.h WASAPI.cpp AudioStats.h AudioStats.cpp AudioStats.ui ConfigDialog.h ConfigDialog.cpp ConfigDialog.ui DBus.h GlobalShortcut_win.cpp GlobalShortcut_win.h licenses.h mumble_pch.hpp os_macx.cpp smallft.h smallft.cpp TextMessage.h TextMessage.cpp TextMessage.ui TextToSpeech.h TextToSpeech_win.cpp Usage.h Usage.cpp VersionCheck.h VersionCheck.cpp ViewCert.h ViewCert.cpp ConfigWidget.h ConfigWidget.cpp; do
+for F in ASIOInput.h ASIOInput.cpp DirectSound.h DirectSound.cpp WASAPI.h WASAPI.cpp AudioStats.h AudioStats.cpp AudioStats.ui ConfigDialog.h ConfigDialog.cpp ConfigDialog.ui DBus.h GlobalShortcut_win.cpp GlobalShortcut_win.h licenses.h mumble_pch.hpp os_macx.cpp smallft.h smallft.cpp TextToSpeech.h TextToSpeech_win.cpp Usage.h Usage.cpp VersionCheck.h VersionCheck.cpp ViewCert.h ViewCert.cpp ConfigWidget.h ConfigWidget.cpp; do
   [ ! -f $F ] || rm $F
   ln ../mumble/$F
 done
