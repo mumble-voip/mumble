62aa53bfa5fdce3d197f5b0c551f5026d15d49d8
Thorvald Natvig <slicer@users.sourceforge.net>
2009-11-24 09:21:31 +0100

    Don't deref the mainwindow before it's created

--- src/mumble/main.cpp
+++ src/mumble/main.cpp
@@ -73,8 +73,10 @@ class QAppMumble:public QApplication {
 
 void QAppMumble::commitData(QSessionManager &) {
 	// Make sure the config is saved and supress the ask on quite message
-	g.s.save();
-	g.mw->bSuppressAskOnQuit = true;
+	if (g.mw) {
+		g.s.save();
+		g.mw->bSuppressAskOnQuit = true;
+	}
 }
 
 int main(int argc, char **argv) {
