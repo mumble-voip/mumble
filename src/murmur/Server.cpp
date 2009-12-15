718da15ed34054a36c2dfa9090ac6d9931678411
Thorvald Natvig <slicer@users.sourceforge.net>
2009-12-15 22:22:37 +0100

    Avoid a potential divide by 0 under really bizarre circumstances

--- src/murmur/Server.cpp
+++ src/murmur/Server.cpp
@@ -441,6 +441,9 @@ BandwidthRecord::BandwidthRecord() {
 bool BandwidthRecord::addFrame(int size, int maxpersec) {
 	quint64 elapsed = a_qtWhen[iRecNum].elapsed();
 	
+	if (elapsed == 0)
+		return false;
+	
 	int nsum = iSum-a_iBW[iRecNum]+size;
 	int bw = static_cast<int>((nsum * 1000000LL) / elapsed);
 
