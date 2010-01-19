02160003290d45fa0b820bcf77af45142ca32d19
Mikkel Krautz <mikkel@krautz.dk>
2010-01-19 17:57:42 +0100

    Fix OSX dist script for new libcelt-naming.

--- scripts/osxdist.py
+++ scripts/osxdist.py
@@ -234,7 +234,7 @@ class AppBundle(object):
 		print ' * Copying CELT libraries.'
 		dst = os.path.join(self.bundle, 'Contents', 'Codecs')
 		os.makedirs(dst)
-		shutil.copy('release/libcelt.0.7.0.dylib', dst)
+		shutil.copy('release/libcelt0.0.7.0.dylib', dst)
 
 	def copy_plugins(self):
 		'''
@@ -453,6 +453,7 @@ if __name__ == '__main__':
 			'release/Mumble.app/Contents/Overlay/mumble-overlay',
 			'release/Mumble.app/Contents/Plugins/liblink.dylib',
 			'release/Mumble.app/Contents/Overlay/libmumbleoverlay.dylib',
+			'release/Mumble.app/Contents/Codecs/libcelt0.0.7.0.dylib',
 			# 1.1.x
 			'release/Mumble11x.app/',
 			'release/Mumble11x.app/Contents/MacOS/mumble-g15-helper',
