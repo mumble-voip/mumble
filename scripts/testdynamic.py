79fc87489595eebbc6c9f40d9d79a74af4e7494d
Thorvald Natvig <slicer@users.sourceforge.net>
2010-02-15 19:20:21 +0100

    Expand dynamic slice-fetch example to show fallback

--- scripts/testdynamic.py
+++ scripts/testdynamic.py
@@ -5,13 +5,19 @@ import Ice, IcePy, sys, tempfile
 ice = Ice.initialize(sys.argv)
 
 proxy = ice.stringToProxy('Meta:tcp -h 127.0.0.1 -p 6502')
-slice = IcePy.Operation('getSlice', Ice.OperationMode.Idempotent, Ice.OperationMode.Idempotent, True, (), (), (), IcePy._t_string, ()).invoke(proxy, ((), None))
 
-slicefile = tempfile.NamedTemporaryFile(suffix = '.ice')
-slicefile.write(slice)
-slicefile.flush()
-Ice.loadSlice(slicefile.name)
-slicefile.close()
+try:
+  slice = IcePy.Operation('getSlice', Ice.OperationMode.Idempotent, Ice.OperationMode.Idempotent, True, (), (), (), IcePy._t_string, ()).invoke(proxy, ((), None))
+
+  slicefile = tempfile.NamedTemporaryFile(suffix = '.ice')
+  slicefile.write(slice)
+  slicefile.flush()
+  Ice.loadSlice(slicefile.name)
+  slicefile.close()
+  print 'Using dynamic slice'
+except:
+  Ice.loadSlice('Murmur.ice')
+  print 'Using bundled slice'
 
 import Murmur
 
