f5f526d2ee8f3386461798a5943adae59d05243d
Tuck Therebelos <snares@users.sourceforge.net>
2010-07-26 23:48:31 -0500

    Fix multiple issues with ArmA 2 plugin, and update it to 1.07

--- plugins/arma2/arma2.cpp
+++ plugins/arma2/arma2.cpp
@@ -42,6 +42,7 @@ static int fetch(float *avatar_pos, float *avatar_front, float *avatar_top, floa
 	// char state;
 	bool ok;
 
+	/*
 	float front_corrector1;
 	float front_corrector2;
 	float front_corrector3;
@@ -49,28 +50,36 @@ static int fetch(float *avatar_pos, float *avatar_front, float *avatar_top, floa
 	float top_corrector1;
 	float top_corrector2;
 	float top_corrector3;
+	*/
 
 	/*
 		value is 0 when one is not in a game, 4 when one is
+	
+	   ok = peekProc((BYTE *) 0x, &state, 1); // Magical state value
+	   if (! ok)
+	 	return false;
+
+	   if (state == 0)
+	          return true; // This results in all vectors beeing zero which tells Mumble to ignore them.
 	*/
-	// ok = peekProc((BYTE *) 0x, &state, 1); // Magical state value
-	// if (! ok)
-	// 	return false;
-
-	// if (state == 0)
-	//          return true; // This results in all vectors beeing zero which tells Mumble to ignore them.
-
+	
 	ok = peekProc(posptr, avatar_pos, 12) &&
-	     peekProc(frontptr, &front_corrector1, 4) &&
+	     peekProc(frontptr, avatar_front, 12) &&
+		 peekProc(topptr, avatar_top, 12);
+		 
+		 /*
+		 peekProc(frontptr, &front_corrector1, 4) &&
 	     peekProc(frontptr + 0xC, &front_corrector2, 4) &&
 	     peekProc(frontptr + 0x18, &front_corrector3, 4) &&
 	     peekProc(topptr, &top_corrector1, 4) &&
 	     peekProc(topptr + 0xC, &top_corrector2, 4) &&
 	     peekProc(topptr + 0x18, &top_corrector3, 4);
+		 */
 
 	if (! ok)
 		return false;
 
+	/*
 	avatar_front[0] = front_corrector1;
 	avatar_front[1] = front_corrector2;
 	avatar_front[2] = front_corrector3;
@@ -78,6 +87,7 @@ static int fetch(float *avatar_pos, float *avatar_front, float *avatar_top, floa
 	avatar_top[0] = top_corrector1;
 	avatar_top[1] = top_corrector2;
 	avatar_top[2] = top_corrector3;
+	*/
 
 	for (int i=0;i<3;i++) {
 		camera_pos[i] = avatar_pos[i];
@@ -90,26 +100,32 @@ static int fetch(float *avatar_pos, float *avatar_front, float *avatar_top, floa
 
 static int trylock(const std::multimap<std::wstring, unsigned long long int> &pids) {
 	posptr = NULL;
+	
 
 	if (! initialize(pids, L"arma2.exe"))
 		return false;
+	
+	/*
+	BYTE bState;
+	peekProc((BYTE *) 0x00BF64D0, &bState, 1);
+	if (bState == 0)
+		return false;
+	*/
+		
+	/* 
+	   Comment out code we don't need
+	   BYTE *pModule=getModuleAddr(L"<module name, if you need it>.dll");
+	   if (!pModule)
+	*/
+	
+	BYTE *ptr1 = peekProc<BYTE *>((BYTE *) 0x00BF2C28);
+	
+	BYTE *base = ptr1 + 0x80;
 
-	// Comment out code we don't need
-	// BYTE *pModule=getModuleAddr(L"<module name, if you need it>.dll");
-	// if (!pModule)
-	// 	return false;
-
-	BYTE *ptr1 = peekProc<BYTE *>((BYTE *) 0x00C10F80);
-	BYTE *ptr2 = peekProc<BYTE *>(ptr1 + 0x280);
-
-	posptr = ptr2 + 0x40;
-
-	BYTE *ptr3 = peekProc<BYTE *>((BYTE *) 0x00C10B00);
-	BYTE *ptr4 = peekProc<BYTE *>(ptr1 + 0x18);
-
-	frontptr = ptr4 + 0xA8;
-	topptr = ptr4 + 0xA4;
-
+	posptr = base + 0x18;
+	frontptr = base;
+	topptr = base + 0xC;
+	
 	float apos[3], afront[3], atop[3], cpos[3], cfront[3], ctop[3];
 	std::string context;
 	std::wstring identity;
@@ -123,10 +139,10 @@ static int trylock(const std::multimap<std::wstring, unsigned long long int> &pi
 }
 
 static const std::wstring longdesc() {
-	return std::wstring(L"Supports Armed Assault 2 v1.05. No identity or context support yet.");
+	return std::wstring(L"Supports Armed Assault 2 v1.07. No identity or context support yet.");
 }
 
-static std::wstring description(L"ArmA 2 v1.05");
+static std::wstring description(L"ArmA 2 v1.07");
 static std::wstring shortname(L"ArmA 2");
 
 static int trylock1() {
