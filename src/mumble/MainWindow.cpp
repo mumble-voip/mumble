092784e183ad8d0c42c5b30bf43ff8d3083b1350
Mikkel Krautz <mikkel@krautz.dk>
2010-08-29 22:20:47 +0200

    Disallow clicking links in the log when embedded into the overlay on OSX.

--- src/mumble/MainWindow.cpp
+++ src/mumble/MainWindow.cpp
@@ -2422,6 +2422,15 @@ void MainWindow::customEvent(QEvent *evt) {
 
 
 void MainWindow::on_qteLog_anchorClicked(const QUrl &url) {
+#ifdef Q_OS_MAC
+	// Clicking a link can cause the user's default browser to pop up while
+	// we're intercepting all events. This can be very confusing (because
+	// the user can't click on anything before they dismiss the overlay
+	// by hitting their toggle hotkey), so let's disallow clicking links
+	//when embedded into the overlay for now.
+	if (g.ocIntercept)
+		return;
+#endif
 	if (!handleSpecialContextMenu(url, QCursor::pos(), true)) {
 		if (url.scheme() != QLatin1String("file")
 		        && url.scheme() != QLatin1String("qrc")
