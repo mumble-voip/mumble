99be1ae20308f3be056eb530f82d422c2bed4e33
Kissaki <kissaki@gmx.de>
2011-04-18 17:31:16 +0200

    comment

--- overlay_gl/overlay.c
+++ overlay_gl/overlay.c
@@ -608,7 +608,7 @@ static void drawContext(Context * ctx, int width, int height) {
 	glViewport(viewport[0], viewport[1], viewport[2], viewport[3]);
 	glUseProgram(program);
 	
-	//TODO eh, what? This is supposed to produce an endless loop with no body?
+	// drain opengl error queue
 	while (glGetError() != GL_NO_ERROR);
 }
 
