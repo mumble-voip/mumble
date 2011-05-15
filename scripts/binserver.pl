5459d4db9525934e2d34235169fd1d3515df3364
Thorvald Natvig <slicer@users.sourceforge.net>
2011-05-15 14:49:21 -0700

    Make -j for binserver

--- scripts/binserver.pl
+++ scripts/binserver.pl
@@ -49,14 +49,14 @@ if ($#ARGV < 0) {
 }
 
 schroot("qmake CONFIG+=release CONFIG+=no-client -recursive");
-schroot("make distclean");
+schroot("make -j distclean");
 unlink("src/murmur/Murmur.h");
 unlink("src/murmur/Murmur.cpp");
 unlink("src/murmur/Mumble.pb.h");
 unlink("src/murmur/Mumble.pb.cc");
 unlink("murmur.x86");
 schroot("qmake CONFIG+=release CONFIG+=no-client DEFINES+=MUMBLE_VERSION=\"$ver-ermine\" -recursive");
-schroot("make");
+schroot("make -j");
 system("strip release/murmurd");
 schroot("\$HOME/.ermine/ErminePro.i386 --ld_assume_kernel=2.6.15 --output=murmur.x86 --with-gconv=noentry --with-locale=noentry --with-xlocale=noentry --with-nss=internal --config=scripts/ermine.conf release/murmurd");
 
