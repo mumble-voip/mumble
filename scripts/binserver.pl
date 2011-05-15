03136ba1a5af0396327f62979bd76dbb1cca14fb
Thorvald Natvig <slicer@users.sourceforge.net>
2011-05-15 16:49:54 -0700

    quiet-build-log for binserver

--- scripts/binserver.pl
+++ scripts/binserver.pl
@@ -48,14 +48,14 @@ if ($#ARGV < 0) {
   $ver=$ARGV[0];
 }
 
-schroot("qmake CONFIG+=release CONFIG+=no-client -recursive");
+schroot("qmake CONFIG+=release CONFIG+=no-client CONFIG+=quiet-build-log -recursive");
 schroot("make -j distclean");
 unlink("src/murmur/Murmur.h");
 unlink("src/murmur/Murmur.cpp");
 unlink("src/murmur/Mumble.pb.h");
 unlink("src/murmur/Mumble.pb.cc");
 unlink("murmur.x86");
-schroot("qmake CONFIG+=release CONFIG+=no-client DEFINES+=MUMBLE_VERSION=\"$ver-ermine\" -recursive");
+schroot("qmake CONFIG+=release CONFIG+=no-client CONFIG+=quiet-build-log DEFINES+=MUMBLE_VERSION=\"$ver-ermine\" -recursive");
 schroot("make -j");
 system("strip release/murmurd");
 schroot("\$HOME/.ermine/ErminePro.i386 --ld_assume_kernel=2.6.15 --output=murmur.x86 --with-gconv=noentry --with-locale=noentry --with-xlocale=noentry --with-nss=internal --config=scripts/ermine.conf release/murmurd");
