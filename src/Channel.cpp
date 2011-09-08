94a4621563d21bea23fdc07e9d4fd6e98cbf6942
Jamie Fraser <jamie.f@mumbledog.com>
2011-09-08 13:00:00 -0400

    Fixed UTC on dates, used secsTo() instead of toTime_t() and some math.

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -44,7 +44,7 @@ Channel::Channel(int id, const QString &name, QObject *p) : QObject(p) {
 	qsName = name;
 	bInheritACL = true;
 	bTemporary = false;
-	qdtLastUsed = QDateTime::currentDateTime();
+	qdtLastUsed = QDateTime::currentDateTime().toUTC();
 	cParent = qobject_cast<Channel *>(p);
 	if (cParent)
 		cParent->addChannel(this);
@@ -183,22 +183,20 @@ void Channel::addUser(User *p) {
 		p->cChannel->removeUser(p);
 	p->cChannel = this;
 	qlUsers << p;
-	
-	qWarning("Channel was last used: %d seconds ago", QDateTime::currentDateTime().toTime_t() - qdtLastUsed.toTime_t());
 }
 
 void Channel::removeUser(User *p) {
 	qlUsers.removeAll(p);
 	
 	if (qlUsers.isEmpty())
-	        qdtLastUsed = QDateTime::currentDateTime();
+	        qdtLastUsed = QDateTime::currentDateTime().toUTC();
 }
 
 QDateTime Channel::getLastUsed() const {
         if (qlUsers.isEmpty())
                 return qdtLastUsed;
         else
-                return QDateTime::currentDateTime();
+                return QDateTime::currentDateTime().toUTC();
 }
 
 Channel::operator const QString() const {
--- src/murmur/MurmurIce.cpp
+++ src/murmur/MurmurIce.cpp
@@ -110,7 +110,7 @@ static void channelToChannel(const ::Channel *c, Murmur::Channel &mc) {
 	mc.parent = c->cParent ? c->cParent->iId : -1;
 	mc.description = u8(c->qsDesc);
 	mc.position = c->iPosition;
-	mc.lastUsed = QDateTime::currentDateTime().toTime_t() - c->getLastUsed().toTime_t();
+	mc.lastUsed = c->getLastUsed().secsTo(QDateTime::currentDateTime().toUTC());
 	mc.links.clear();
 	foreach(::Channel *chn, c->qsPermLinks)
 		mc.links.push_back(chn->iId);
