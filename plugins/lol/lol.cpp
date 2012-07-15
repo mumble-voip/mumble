442a9fdde8e3dfbcab16e384c252f6fc84e3ee17
Piotr Chodań <dark.skeleton@gmail.com>
2012-07-15 17:20:21 +0200

    Further updates and optimizations to the LoL plugin - made the code simplier, less nested in fetch(). - strings are now ensured to be zero-terminated - replaced calcout() call from trylock() with fetch call that can actually   fail. - Disabled identity support since it still needs some work.

--- plugins/lol/lol.cpp
+++ plugins/lol/lol.cpp
@@ -40,13 +40,11 @@ static BYTE *atopptr;
 
 static BYTE *hostipptr;
 static BYTE *hostportptr;
-static BYTE *summonerptr;
 
 static BYTE *gameptr;
 
 static char prev_hostip[16]; // These should never change while the game is running, but just in case...
 static int prev_hostport;
-static char prev_summoner[17];
 
 static bool calcout(float *pos, float *cam, float *opos, float *ocam) {
 	// Seems League of Legends is in centimeters? ;o Well it's not inches for sure :)
@@ -72,13 +70,12 @@ static bool refreshPointers(void) {
 	Avatar top vector address: 		+0x2ac0
 
 	D9 9E E8 01 00 00 D9 40 70 D9 9E EC 01 00 00 D9 40 74 D9 9E F0 01 00 00				:02F2DE68 
-	Avatar position vector address:		+0x1e8
+	Avatar position vector address:		+0x1e8 (pointer offset)
 
 	IP: Look for a non-unicode string that will contain server's IP. 28 bytes further from IP, there should be server's port
 																						:0AF395B8 
 	PORT:					+0x1C (offset, not pointer!)
-	IDENTITY: Just look for your nickname saved in non-unicode that is static. Length is  16
-																						:0AF3957C 
+	IDENTITY: Still to be found
 	*/
 
 	posptr = camptr = camfrontptr = camtopptr = afrontptr = atopptr = NULL;
@@ -117,19 +114,16 @@ static bool refreshPointers(void) {
 	hostipptr = (BYTE *)0xAF395B8;
 	hostportptr = hostipptr + 0x1C;
 
-	summonerptr = (BYTE *)0xAF3957C;
-
 	return true;
 }
 
-static int fetch(float *avatar_pos, float *avatar_front, float *avatar_top, float *camera_pos, float *camera_front, float *camera_top, std::string &context, std::wstring &identity) {
+static int fetch(float *avatar_pos, float *avatar_front, float *avatar_top, float *camera_pos, float *camera_front, float *camera_top, std::string &context, std::wstring &/*identity*/) {
 	for (int i = 0; i < 3; i++)
 		avatar_pos[i] = avatar_front[i] = avatar_top[i] = camera_pos[i] = camera_front[i] = camera_top[i] = 0.0f;
 
 	float ipos[3], cam[3];
 	int hostport;
 	char hostip[16];
-	char summoner[17];
 	bool ok;
 
 	// Player not in game (or something broke), unlink
@@ -143,66 +137,51 @@ static int fetch(float *avatar_pos, float *avatar_front, float *avatar_top, floa
 		 peekProc(afrontptr, avatar_front, 12) &&
 		 peekProc(atopptr, avatar_top, 12) &&
 		 peekProc(hostipptr, hostip) &&
-		 peekProc(hostportptr, &hostport, 4) &&
-		 peekProc(summonerptr, summoner);
+		 peekProc(hostportptr, &hostport, 4) /*&&
+		 peekProc(summonerptr, summoner)*/;
 
-	if (ok) {
-		// Ensure strings are zero terminated
-		summoner[sizeof(summoner) - 1] = '\0';
-		hostip[sizeof(hostip) - 1] = '\0';
+	if (!ok) 
+		return false;
 
-		int res = calcout(ipos, cam, avatar_pos, camera_pos);
-		if (res) {
-			if (strcmp(hostip, prev_hostip) != 0 || hostport != prev_hostport) {
-				context.clear();
+	// Ensure strings are zero terminated
+	hostip[sizeof(hostip) - 1] = '\0';
 
-				strcpy_s(prev_hostip, sizeof(prev_hostip), hostip);
-				prev_hostport = hostport;
+	calcout(ipos, cam, avatar_pos, camera_pos); //calculate proper position values
+	
+	if (strcmp(hostip, prev_hostip) != 0 || hostport != prev_hostport) {
+		context.clear();
 
-				if (strcmp(hostip, "") != 0) {
-					char buffer[50];
-					sprintf_s(buffer, sizeof(buffer), "{\"ipport\": \"%s:%d\"}", hostip, hostport);
-					context.assign(buffer);
-				}
-			}
-			if (strcmp(summoner, prev_summoner) != 0) {
-				identity.clear();
+		strcpy_s(prev_hostip, sizeof(prev_hostip), hostip);
+		prev_hostport = hostport;
 
-				strcpy_s(prev_summoner, sizeof(prev_summoner), summoner);
-				
-				if (strcmp(summoner, "") != 0) {
-					wchar_t tmp[sizeof(summoner)];
-					mbstowcs_s(NULL, tmp, summoner, sizeof(summoner));
-					wchar_t buffer[50];
-					swprintf_s(buffer, 50, L"{\"summoner\": \"%s\"}", tmp);
-					identity.assign(buffer);
-				}
-			}
+		if (strcmp(hostip, "") != 0) {
+			char buffer[50];
+			sprintf_s(buffer, sizeof(buffer), "{\"ipport\": \"%s:%d\"}", hostip, hostport);
+			context.assign(buffer);
 		}
-		return res;
 	}
 
-	return false;
+	return true;
 }
 
 static int trylock(const std::multimap<std::wstring, unsigned long long int> &pids) {
 	if (! initialize(pids, L"League of Legends.exe"))
 		return false;
 
-	float pos[3], opos[3];
-	float cam[3], ocam[3];
-
-	// unlink plugin if this fails
-	if (!refreshPointers()) {
-		generic_unlock();
-		return false;
-	}
-
-	if (calcout(pos, cam, opos, ocam)) { // make sure values are OK
+	if (refreshPointers()) { // unlink plugin if this fails
 		*prev_hostip = '\0';
 		prev_hostport = 0;
-		*prev_summoner = '\0';
-		return true;
+
+		float avatar_pos[3], avatar_front[3], avatar_top[3];
+		float camera_pos[3], camera_front[3], camera_top[3];
+		std::string context;
+		std::wstring identity;
+
+		if (fetch(avatar_pos, avatar_front, avatar_top,
+				camera_pos, camera_front, camera_top,
+				context, identity)) {
+			return true;
+		}
 	}
 
 	generic_unlock();
@@ -210,7 +189,7 @@ static int trylock(const std::multimap<std::wstring, unsigned long long int> &pi
 }
 
 static const std::wstring longdesc() {
-	return std::wstring(L"Supports League of Legends v1.0.0.142 with context and identity support.");
+	return std::wstring(L"Supports League of Legends v1.0.0.142 with context. No identity support yet.");
 }
 
 static std::wstring description(L"League of Legends (v1.0.0.142)");
