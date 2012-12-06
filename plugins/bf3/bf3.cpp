7243342f6d48eff94ae9c37dd7ebe74d2c3db3e3
bogie <priivet@gmail.com>
2012-12-06 06:57:05 +0100

    - updated bf3 plugin to Aftermath patch

--- plugins/bf3/bf3.cpp
+++ plugins/bf3/bf3.cpp
@@ -36,15 +36,15 @@ static bool ptr_chain_valid = false;
 static BYTE *pmodule_bf3;
 
 // Magic ptrs
-static BYTE* const state_ptr = (BYTE *) 0x0234A36C;
+static BYTE* const state_ptr = (BYTE *) 0x02347E3C;
 
 // Vector ptrs
-static BYTE* const avatar_pos_ptr = (BYTE *) 0x0234A300;
-static BYTE* const avatar_front_ptr = (BYTE *) 0x234A330;
-static BYTE* const avatar_top_ptr = (BYTE *) 0x234A320;
+static BYTE* const avatar_pos_ptr = (BYTE *) 0x0234CC60;
+static BYTE* const avatar_front_ptr = (BYTE *) 0x234CC90;
+static BYTE* const avatar_top_ptr = (BYTE *) 0x234CC80;
 
 // Context ptrs
-static BYTE* const ipport_ptr = (BYTE *) 0x023344D0;
+static BYTE* const ipport_ptr = (BYTE *) 0x023340D0;
 
 // Identity ptrs
 static BYTE *team_state_ptr;
@@ -52,20 +52,20 @@ static BYTE *squad_state_ptr;
 static BYTE *squad_lead_state_ptr;
 
 // Offsets
-static const int base_offset = 0x01F4583C;
-static const int identity_offset1 = 0x43C;
+static const int base_offset = 0x01F42AE4;
+static const int identity_offset1 = 0x8;
 static const int identity_offset2 = 0xBC;
-static const int identity_offset3 = 0x348;
-static const int identity_offset4 = 0x4;
-static const int squad_state_offset = 0x48;
-static const int squad_lead_state_offset = 0x4C;
+static const int identity_offset3 = 0x36C;
+static const int identity_offset4 = 0x8;
+static const int squad_state_offset = 0x104;
+static const int squad_lead_state_offset = 0x108;
 static const int team_state_offset = 0x31C;
 
 enum state_values {
     STATE_UNKN = 0,
-    STATE_LOADING = 1,
-    STATE_IN_GAME = 2,
-    STATE_IN_MENU = 3
+    STATE_IN_GAME = 1,
+    STATE_IN_MENU = 2,
+    STATE_LOADING = 3
 };
 
 inline bool resolve_ptrs() {
@@ -73,16 +73,16 @@ inline bool resolve_ptrs() {
 
     /*
     Magic:
-        state : 0x0234A36C                                                       BYTE        2 when playing
+        state : 0x02347E3C                                                       BYTE        2 when playing
                                                                                              3 while in menu/dead
 
     Context:
-        IP:Port of server: 0x023344D0                                            char[128]   ip:port of the server
+        IP:Port of server: 0x023340D0                                            char[128]   ip:port of the server
 
     Identity:
-        Squad state: BF3.exe+0x01F4583C + 0x43C + 0xBC + 0x348 + 0x4 + 0x48      BYTE        0 is not in squad; 1 is in Alpha squad, 2 Bravo, ... , 9 India
-        SLead state: BF3.exe+0x01F4583C + 0x43C + 0xBC + 0x348 + 0x4 + 0x4C      BYTE        0 is not lead; 1 is lead
-        Team state:  BF3.exe+0x01F4583C + 0x43C + 0xBC + 0x31C                   BYTE        1 is blufor (US team, for example), 2 is opfor (RU), 0 is probably upcoming spec mode
+        Squad state: BF3.exe+0x01F42AE4 + 0x8 + 0xBC + 0x36C + 0x8 + 0x104      BYTE        0 is not in squad; 1 is in Alpha squad, 2 Bravo, ... , 9 India
+        SLead state: BF3.exe+0x01F42AE4 + 0x8 + 0xBC + 0x36C + 0x8 + 0x108      BYTE        0 is not lead; 1 is lead
+        Team state:  BF3.exe+0x01F42AE4 + 0x8 + 0xBC + 0x31C                    BYTE        1 is blufor (US team, for example), 2 is opfor (RU), 0 is probably upcoming spec mode
     */
 
     BYTE *base_bf3 = peekProc<BYTE *>(pmodule_bf3 + base_offset);
