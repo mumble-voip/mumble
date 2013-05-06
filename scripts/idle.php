c5e61212171f1a71a862da673e0e217b9e14014d
hamilton5 <ham5@tehwebz.net>
2013-05-06 12:23:35 -0300

    Update idle.php
    make compatible with the new php-ice v3.4

--- scripts/idle.php
+++ scripts/idle.php
@@ -59,7 +59,7 @@ if (!extension_loaded('ice')) {
 if ((!function_exists('Ice_intVersion') || Ice_intVersion() < 30400) || !function_exists('Ice_loadProfile')) {
 	exit('Error: This example script is compatible only for php-ice versions prior to 3.4.');
 }
-
+##FIXME: make php-ice 3.4 compatible
 Ice_loadProfile();
 $base = $ICE->stringToProxy($ice);
 $murmur = $base->ice_checkedCast('::Murmur::Meta');
