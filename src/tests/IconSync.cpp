f92455b513f912b203865b1a841288a4c06b72f0
Kissaki <kissaki@gmx.de>
2013-06-15 21:25:11 +0200

    Refac: Reduce code duplication in IconSync.cpp

--- src/tests/IconSync.cpp
+++ src/tests/IconSync.cpp
@@ -3,11 +3,10 @@
 #include <QtGui>
 #include <QtSvg>
 
-int main(int argc, char **argv) {
-	QCoreApplication a(argc, argv);
-
-	QSvgRenderer svg(QLatin1String("../../icons/mumble.svg"));
-	QImage original(512,512,QImage::Format_ARGB32);
+void svg2png(const QString & svgpath, const QList<int> & sizes, QStringList & qslImages)
+{
+	QSvgRenderer svg(svgpath);
+	QImage original(512, 512, QImage::Format_ARGB32);
 	original.fill(Qt::transparent);
 
 	QPainter painter(&original);
@@ -17,41 +16,6 @@ int main(int argc, char **argv) {
 	painter.setRenderHint(QPainter::HighQualityAntialiasing);
 	svg.render(&painter);
 
-	QList<int> sizes;
-	sizes << 32;
-	sizes << 48;
-	sizes << 64;
-	sizes << 96;
-	sizes << 128;
-	sizes << 256;
-
-
-	QSvgRenderer svgSmall(QLatin1String("../../icons/mumble_small.svg"));
-	QImage originalSmall(512,512,QImage::Format_ARGB32);
-	originalSmall.fill(Qt::transparent);
-
-	QPainter painterSmall(&originalSmall);
-	painterSmall.setRenderHint(QPainter::Antialiasing);
-	painterSmall.setRenderHint(QPainter::TextAntialiasing);
-	painterSmall.setRenderHint(QPainter::SmoothPixmapTransform);
-	painterSmall.setRenderHint(QPainter::HighQualityAntialiasing);
-	svgSmall.render(&painterSmall);
-    
-	QList<int> sizesSmall;
-	sizesSmall << 16;
-	sizesSmall << 24;
-
-
-	QStringList qslImages;
-	foreach(int size, sizesSmall) {
-		QImage img = originalSmall.scaled(size,size,Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
-		QString png = QDir::temp().absoluteFilePath(QString::fromLatin1("mumble.%1.png").arg(size));
-
-		QImageWriter qiw(png);
-		qiw.write(img);
-
-		qslImages << png;
-	}
 	foreach(int size, sizes) {
 		QImage img = original.scaled(size,size,Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
 		QString png = QDir::temp().absoluteFilePath(QString::fromLatin1("mumble.%1.png").arg(size));
@@ -61,6 +25,29 @@ int main(int argc, char **argv) {
 
 		qslImages << png;
 	}
+}
+
+int main(int argc, char **argv) {
+	QCoreApplication a(argc, argv);
+
+
+	QStringList qslImages;
+
+	QString svgSmallPath = QLatin1String("../../icons/mumble_small.svg");
+	QList<int> sizesSmall;
+	sizesSmall << 16;
+	sizesSmall << 24;
+	svg2png(svgSmallPath, sizesSmall, qslImages);
+
+	QLatin1String svgPath("../../icons/mumble.svg");
+	QList<int> sizes;
+	sizes << 32;
+	sizes << 48;
+	sizes << 64;
+	sizes << 96;
+	sizes << 128;
+	sizes << 256;
+	svg2png(svgPath, sizes, qslImages);
 
 
 	QStringList args;
