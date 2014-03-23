c03d8fcc731b0279bf9f23113bbc479effc3df23
Stefan Hacker <dd0t@users.sourceforge.net>
2014-03-23 21:49:13 +0100

    Drastically simplify release.pl.
    Previously release.pl did complex parsing of .pro
    files to figure out which files to include in the
    tar ball.
    
    This change removes this logic and instead performs
    the minimal modifications needed under the assumption
    of a clean tree and then uses system tools to create
    the tar.gz and zip files.

--- scripts/release.pl
+++ scripts/release.pl
@@ -3,182 +3,14 @@
 use strict;
 use warnings;
 use Carp;
-use Switch;
-use Archive::Tar;
-use Archive::Tar::Constant;
-use Archive::Zip qw( :ERROR_CODES :CONSTANTS );
-use Compress::Zlib;
 use POSIX;
-use File::Copy;
-use File::stat;
 
-sub adddir($$) {
-  my ($dir, $ref) = @_;
-  $dir =~ s/^\.\///;
-  my $base = $dir;
-  $base =~ s/^.+\/([^\/]+)$/$1/;
-  if (-f "$dir/$base.pro") {
-    push @{$ref}, "$dir/$base.pro";
-  }
-  opendir(DIR, $dir);
-  foreach my $e (grep { ! /^\./ } readdir(DIR)) {
-    if (-d "$dir/$e") {
-      adddir("$dir/$e", $ref);
-    }
-  }
-}
-
-my %files;
 my $ver;
-my %filevars = ( 'sources' => 1, 'objective_sources' => 1, 'headers' => 1, 'rc_file' => 1, 'dist' => 1, 'forms' => 1, 'resources' => 1, 'precompiled_header' => 1, 'translations' => 1);
 
 system("rm mumble-*");
-chdir("scripts");
-system("bash mkini.sh");
-chdir("..");
-
-my @pro = ("main.pro");
-#, "src/mumble.pri");
-#adddir(".", \@pro);
-
-my @resources;
-
-while (my $pro = shift @pro) {
-  open(F, $pro) or croak "Failed to open $pro";
-  print "Processing $pro\n";
-  $files{$pro}=1;
-  my $basedir=$pro;
-  $basedir =~ s/[^\/]+\Z//g;
-  my @vpath = ($basedir);
-
-  my $file = join("", <F>);
-  $file =~ s/\\\n/\n/g;
-  print $file;
-
-  foreach my $line (split(/\n/, $file)) {
-    $_ = $line;
-    chomp();
-    if (/^include\((.+)\)/) {
-      my $f = $basedir . $1;
-      while ($f =~ /\.\./) {
-        $f =~ s/(\/|\A)[^\/]+\/\.\.\//$1/g;
-      }
-      push @pro, $f;
-    } elsif (/^\s*(\w+)\s*?[\+\-\*]{0,1}=\s*(.+)$/) {
-      my ($var,$value)=(lc $1,$2);
-      switch ($var) {
-        case "version" {
-          if ($value !~ /\$\$/) {
-              croak "Versions don't match: $ver vs $value" if (defined($ver) && ($ver ne $value));
-              $ver=$value;
-          }
-        }
-        case "vpath" {
-          my $vdir = $basedir;
-          if ($value eq '../$$SOURCEDIR/libcelt') {
-            $vdir =~ s/-build/-src/;
-            push @vpath, $vdir.'libcelt/';
-          } elsif ($value eq '../$$SOURCEDIR/lib') { # sbcelt lib
-            $vdir =~ s/-lib-build/-src/;
-            push @vpath, $vdir.'lib/';
-          } elsif ($value eq '../$$SOURCEDIR/helper') { # sbcelt helper
-            $vdir =~ s/-helper-build/-src/;
-            push @vpath, $vdir.'helper/';
-          } elsif ($vdir eq '3rdparty/fx11-build/' and $value eq '../$$SOURCEDIR') { # fx11
-            $vdir =~ s/-build/-src/;
-            push @vpath, $vdir;
-          } else {
-            push @vpath,map { "$basedir$_/"} map { s/\$\$PWD/./; $_;} split(/\s/, $value);
-          }
-        }
-        case "subdirs" {
-          push @pro,map { my ($b,$p) = ($_,$_); $p =~ s/^.+\///g; "$basedir$b/$p.pro" } split(/\s/, $value);
-        }
-        case %filevars {
-          foreach my $f (split(/\s+/,$value)) {
-              next if ($f =~ /^Murmur\.(h|cpp)$/);
-              next if ($f =~ /^Mumble\.pb\.(h|cc)$/);
-              my $ok = 0;
-              foreach my $d (@vpath) {
-                if (-f "$d$f") {
-                  $f = $d.$f;
-                  $ok = 1;
-                  last;
-                }
-              }
-              if (! $ok) {
-                croak "Failed to find $f in ".join(" ",@vpath);
-              } else {
-                while ($f =~ /\.\./) {
-                  $f =~ s/(\/|\A)[^\/]+\/\.\.\//$1/g;
-                }
-                $files{$f}=1;
-                if ($var eq "resources") {
-                  push @resources,$f;
-                }
-              }
-          }
-        }
-      }
-    }
-  }
-  close(F);
-}
-
-foreach my $resfile (@resources) {
-  open(F, $resfile);
-  my $basedir=$resfile;
-  $basedir =~ s/[^\/]+\Z//g;
-  while(<F>) {
-    chomp();
-    if (/\>(.+)<\/file\>/) {
-      my $f = $basedir.$1;
-      next if $f =~ /\.qm$/;
-      while ($f =~ /\.\./) {
-                  $f =~ s/(\/|\A)[^\/]+\/\.\.\//$1/g;
-      }
-      
-      $files{$f}=1;
-    }
-  }
-  close(F);
-}
-
-my @fulldirs = ('speex','speex/include/speex','speex/libspeex','man');
-foreach my $cver ('0.7.0', '0.11.0') {
-  push @fulldirs, "celt-$cver-src";
-  push @fulldirs, "celt-$cver-src/libcelt";
-}
-push @fulldirs, "sbcelt-src";
-push @fulldirs, "sbcelt-src/helper";
-push @fulldirs, "sbcelt-src/lib";
-push @fulldirs, "opus-src";
-push @fulldirs, "opus-src/celt";
-push @fulldirs, "opus-src/celt/arm";
-push @fulldirs, "opus-src/celt/x86";
-push @fulldirs, "opus-src/silk";
-push @fulldirs, "opus-src/silk/arm";
-push @fulldirs, "opus-src/silk/float";
-push @fulldirs, "opus-src/silk/fixed";
-push @fulldirs, "opus-src/src";
-push @fulldirs, "opus-src/include";
-
-foreach my $dir (@fulldirs) {
-  opendir(D, $dir) or croak "Could not open $dir";
-  foreach my $f (grep(! /^\./,readdir(D))) {
-    next if ($f =~ /\~$/);
-    my $ff=$dir . '/' . $f;
-    if (-f $ff) {
-      $files{$ff}=1;
-    }
-  }
-  closedir(D);
-}
-
-delete($files{'LICENSE'});
 
 if ($#ARGV < 0) {
-  open(F, "git describe origin/master|");
+  open(F, "git describe origin/master|") or croak "Failed to get version string";
   while (<F>) {
     chomp();
     s/^(.+)-([0-9]+)-(g.+)$/$1|$2|$3/;
@@ -192,34 +24,30 @@ if ($#ARGV < 0) {
   $ver = $ARGV[0];
 }
 
-my $tar = new Archive::Tar();
-my $zip = new Archive::Zip();
-my $blob;
-my $dir="mumble-$ver/";
+print "Adjusting Version.cpp\n";
 
-my $zipdir = $zip->addDirectory($dir);
+open(F, "<src/Version.h") or croak "Could not open src/Version.h for reading";
+my @lines = <F>;
+close(F);
+my $content = join('', @lines);
+$content =~ s/(\#ifndef MUMBLE_VERSION)/$1\n\#define MUMBLE_VERSION $ver\n\#endif\n$1/;
 
-foreach my $file ('LICENSE', sort keys %files) {
-  print "Adding $file\n";
-  open(F, $file) or croak "Missing $file";
-  sysread(F, $blob, stat($file)->size);
+open(F, ">src/Version.h") or croak "Could not open src/Version.h for writing";
+print F $content;
+close(F);
 
-  if ($file eq "src/Version.h") {
-    $blob =~ s/(\#ifndef MUMBLE_VERSION)/$1\n\#define MUMBLE_VERSION $ver\n\#endif\n$1/;
-  }
+print "Compressing tree\n";
+my $ballname = "mumble-${ver}";
+my $exclusions = "--exclude=*/.git* --exclude=${ballname}.*";
 
-  if (-l $file) {
-    $tar->add_data($dir . $file, "", { linkname => readlink($file), type => Archive::Tar::Constant::SYMLINK });
-  } else {
-    $tar->add_data($dir . $file, $blob);
-  }
-  my $zipmember=$zip->addString($blob, $dir . $file);
-  $zipmember->desiredCompressionMethod( COMPRESSION_DEFLATED );
-  $zipmember->desiredCompressionLevel( 9 );
-  close(F);
-}
+system("mkdir ${ballname}") == 0 or croak "Could not create target directory ${ballname}";
+system("mv * ${ballname}/");
+eval {
+	system("tar ${exclusions} -zchvf ${ballname}.tar.gz ${ballname}") == 0 or croak "Failed to create tar.gz";
+	system("zip -9 -r ${exclusions} ${ballname}.zip ${ballname}") == 0 or croak "Failed to create zip";
+};
+system("mv ${ballname}/* .");
+system("rmdir ${ballname}");
 
-my $gz=gzopen("mumble-${ver}.tar.gz", "w");
-$gz->gzwrite($tar->write());
-$gz->gzclose();
-$zip->writeToFileNamed("mumble-${ver}.zip");
+print "Done\n";
+
