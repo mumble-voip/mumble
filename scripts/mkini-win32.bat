bc11185427db7e8484948d8158aa827d730c901b
Stefan Hacker <dd0t@users.sourceforge.net>
2014-09-25 22:55:39 +0200

    Update murmur.ini and scripts to disable dbus by default and enable ice.
    Fixes #1083

--- scripts/mkini-win32.bat
+++ scripts/mkini-win32.bat
@@ -1,4 +1,3 @@
 copy murmur.ini murmur.ini.win32
 
-perl -pi.bak -e "s,^(#|)ice=,ice=," murmur.ini.win32
 
--- scripts/mkini.sh
+++ scripts/mkini.sh
@@ -4,13 +4,7 @@ cp murmur.ini murmur.ini.win32
 cp murmur.ini murmur.ini.osx
 cp murmur.ini murmur.ini.system
 
-perl -pi.bak -e 's!^(#|)ice=!ice=!' murmur.ini.win32
-
-perl -pi.bak -e 's!^(#|)ice=!ice=!' murmur.ini.osx
-perl -pi.bak -e 's!^(#|)dbus=!#dbus!' murmur.ini.osx
-
-perl -pi.bak -e 's!^(#|)ice=!ice=!' murmur.ini.system
-perl -pi.bak -e 's/^(#|)dbus=.*$/dbus=system/' murmur.ini.system
+perl -pi.bak -e 's/^(#|)dbus=.*$/\1dbus=system/' murmur.ini.system
 perl -pi.bak -e 's/^(#|)logfile=.*$/logfile=\/var\/log\/mumble-server\/mumble-server.log/' murmur.ini.system
 perl -pi.bak -e 's/^(#|)pidfile=.*$/pidfile=\/var\/run\/mumble-server\/mumble-server.pid/' murmur.ini.system
 perl -pi.bak -e 's/^(#|)database=.*$/database=\/var\/lib\/mumble-server\/mumble-server.sqlite/' murmur.ini.system
