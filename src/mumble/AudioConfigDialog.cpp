7a18851220f5e9a0153e93ae89c8aeecb44af088
Stefan Hacker <dd0t@users.sourceforge.net>
2014-10-20 23:59:41 +0200

    Make AudioInputDialog and AudioOutputDialog use own icons.
    Introduced config_audio_input.png and config_audio_output.png
    for for that. Previously used config_basic.png.
    
    Part of #1438

--- src/mumble/AudioConfigDialog.cpp
+++ src/mumble/AudioConfigDialog.cpp
@@ -92,7 +92,7 @@ QString AudioInputDialog::title() const {
 }
 
 QIcon AudioInputDialog::icon() const {
-	return QIcon(QLatin1String("skin:config_basic.png"));
+	return QIcon(QLatin1String("skin:config_audio_input.png"));
 }
 
 void AudioInputDialog::load(const Settings &r) {
@@ -432,7 +432,7 @@ QString AudioOutputDialog::title() const {
 }
 
 QIcon AudioOutputDialog::icon() const {
-	return QIcon(QLatin1String("skin:config_basic.png"));
+	return QIcon(QLatin1String("skin:config_audio_output.png"));
 }
 
 void AudioOutputDialog::load(const Settings &r) {
