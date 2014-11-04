mumble-voip/mumble

commit fd782c3cb24235be2337b71f1e411240c4c112f1

Overlay, OverlayClient: add PID and process name diagnostics to 'Dead client detected' notice.

src/mumble/Overlay.cpp
@@ -314,7 +314,7 @@ void Overlay::updateOverlay() {
 
 	foreach(OverlayClient *oc, qlClients) {
 		if (! oc->update()) {
-			qWarning() << "Overlay: Dead client detected";
+			qWarning() << "Overlay: Dead client detected. PID" << oc->uiPid << oc->qsProcessName;
 			qlClients.removeAll(oc);
 			oc->scheduleDelete();
 			break;

src/mumble/OverlayClient.cpp
@@ -479,6 +479,18 @@ void OverlayClient::readyRead() {
 
 						OverlayMsgPid *omp = & omMsg.omp;
 						uiPid = omp->pid;
+#ifdef Q_OS_WIN
+						HANDLE h = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, (DWORD)uiPid);
+						if (h) {
+							wchar_t buf[MAX_PATH];
+							if (GetModuleFileNameEx(h, 0, buf, MAX_PATH) != 0) {
+								qsExecutablePath = QString::fromWCharArray(buf);
+							}
+							CloseHandle(h);
+						}
+#else
+						qsExecutablePath = QLatin1String("Unknown");
+#endif
 					}
 					break;
 				case OVERLAY_MSGTYPE_FPS: {

src/mumble/OverlayClient.h
@@ -63,8 +63,11 @@ class OverlayClient : public QObject {
 		QGraphicsPixmapItem *qgpiFPS;
 		QGraphicsPixmapItem *qgpiTime;
 
-		/// The process ID of the processing displaying an overlay.
+		/// The process ID of the process this OverlayClient is connected to.
 		quint64 uiPid;
+		/// The path to the executable of the process that this OverlayClient is connected to.
+		QString qsExecutablePath;
+
 		QGraphicsScene qgs;
 		OverlayUserGroup ougUsers;
 

src/mumble/mumble.pro
@@ -325,7 +325,7 @@ win32 {
   }
   HEADERS	*= GlobalShortcut_win.h TaskList.h
   SOURCES	*= GlobalShortcut_win.cpp TextToSpeech_win.cpp Overlay_win.cpp SharedMemory_win.cpp Log_win.cpp os_win.cpp TaskList.cpp ../../overlay/HardHook.cpp ../../overlay/ods.cpp
-  LIBS		*= -ldxguid -ldinput8 -lsapi -lole32 -lws2_32 -ladvapi32 -lwintrust -ldbghelp -llibsndfile-1 -lshell32 -lshlwapi -luser32 -lgdi32
+  LIBS		*= -ldxguid -ldinput8 -lsapi -lole32 -lws2_32 -ladvapi32 -lwintrust -ldbghelp -llibsndfile-1 -lshell32 -lshlwapi -luser32 -lgdi32 -lpsapi
   LIBS		*= -ldelayimp -delayload:speex.dll -delayload:shell32.dll
 
   DEFINES	*= WIN32

src/mumble/mumble_pch.hpp
@@ -106,6 +106,7 @@
 #include <delayimp.h>
 #include <shlobj.h>
 #include <tlhelp32.h>
+#include <psapi.h>
 #ifndef Q_CC_INTEL
 #include <math.h>
 #define lroundf(x) ( static_cast<long int>( (x) + ((x) >= 0.0f ? 0.5f : -0.5f) ) )

commit 2c0bfa0b8d6c4c02a3aea8d03f3c7da04409f6ee

Fix compilation of Overlay.cpp. It was accidentally broken by fd782c3c.

src/mumble/Overlay.cpp
@@ -314,7 +314,7 @@ void Overlay::updateOverlay() {
 
 	foreach(OverlayClient *oc, qlClients) {
 		if (! oc->update()) {
-			qWarning() << "Overlay: Dead client detected. PID" << oc->uiPid << oc->qsProcessName;
+			qWarning() << "Overlay: Dead client detected. PID" << oc->uiPid << oc->qsExecutablePath;
 			qlClients.removeAll(oc);
 			oc->scheduleDelete();
 			break;