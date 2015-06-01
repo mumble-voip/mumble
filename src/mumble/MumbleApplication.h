83da95640aa1b3de26e061fd62679a1ffae846fc
Mikkel Krautz <mikkel@krautz.dk>
2015-06-01 20:38:09 +0200

    Add MumbleApplication::applicationVersionRootPath().
    Also add MumbleApplication::instance() for easily getting
    the MumbleApplication singleton.

--- src/mumble/MumbleApplication.h
+++ src/mumble/MumbleApplication.h
@@ -44,8 +44,31 @@ class MumbleApplication : public QApplication {
 #endif
 		Q_OBJECT
 	public:
+
+		/// The instance function returns an instance
+		/// of the MumbleApplication singleton.
+		static MumbleApplication *instance();
+
 		MumbleApplication(int &pargc, char **pargv);
-		
+
+		/// applicationVersionRootPath returns
+		/// Mumble's "versioned root"-path.
+		///
+		/// This is a version-specific path that contains
+		/// supplementary binaries and other products
+		/// that Mumble needs to function.
+		///
+		/// In the current implementation, the versioned
+		/// root path is set by the MUMBLE_VERSION_ROOT
+		/// environment variable. This environment variable
+		/// is set in the mumble.exe launcher.
+		///
+		/// If a versioned root path has not been
+		/// configured in the environment, the function
+		/// returns the same path as Qt's own
+		/// QApplication::applicationDirPath().
+		QString applicationVersionRootPath();
+
 		bool event(QEvent *e) Q_DECL_OVERRIDE;
 #ifdef Q_OS_WIN
 # if QT_VERSION >= 0x050000
