fdefe2c1d83f73b5fdcd3f58f55109e3dc79fa47
Jordan Klassen <jordan@klassen.me.uk>
2015-06-30 10:29:40 -0700

    Use pure BSD license text

--- src/mumble/GKey.cpp
+++ src/mumble/GKey.cpp
@@ -1,6 +1,6 @@
 /* Copyright (C) 2015, Jordan J Klassen <forivall@gmail.com>
 
-   Some rights reserved.
+   All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions
