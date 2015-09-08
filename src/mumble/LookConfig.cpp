0528883ab4abfbdb1dd8de5e489c842c34ae67db
Stefan Hacker <dd0t@users.sourceforge.net>
2015-09-08 21:50:13 +0200

    Add an experimental translator comment

--- src/mumble/LookConfig.cpp
+++ src/mumble/LookConfig.cpp
@@ -98,6 +98,7 @@ LookConfig::LookConfig(Settings &st) : ConfigWidget(st) {
 		m_themeDirectoryWatcher->addPath(userThemeDirectory.path());
 		
 		QUrl userThemeDirectoryUrl = QUrl::fromLocalFile(userThemeDirectory.path());
+		//: This link is located next to the theme heading in the ui config and opens the user theme directory
 		qlThemesDirectory->setText(tr("<a href=\"%1\">Browse</a>").arg(userThemeDirectoryUrl.toString()));
 		qlThemesDirectory->setOpenExternalLinks(true);
 	}
