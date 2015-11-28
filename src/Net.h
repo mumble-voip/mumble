5dac7e81a4cf21dca6ab30d8b916c140dd801533
Mikkel Krautz <mikkel@krautz.dk>
2015-11-28 17:17:59 +0100

    Net: GCC 4.2 on FreeBSD 9.3 does not have __builtin_bswap.
    So work around it.
    
    From searching the web, it would seem that GCC 4.2 should
    include it. But in this case, it seems like it does not.
    
    Maybe the version check is not strict enough for all systems.
    
    But this works for now.

--- src/Net.h
+++ src/Net.h
@@ -91,7 +91,7 @@ struct Ban {
 #if Q_BYTE_ORDER == Q_BIG_ENDIAN
 #define SWAP64(x) (x)
 #else
-#ifdef __x86_64__
+#if defined(__x86_64__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 3))
 #define SWAP64(x) __builtin_bswap64(x)
 #else
 #define SWAP64(x) qbswap<quint64>(x)
