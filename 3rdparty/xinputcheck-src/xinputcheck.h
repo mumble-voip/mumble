beb0c111fe82f879a8c8bac9710a85b52363ade8
Mikkel Krautz <mikkel@krautz.dk>
2016-03-04 00:48:05 +0100

    XInputCheck: add documentation for XInputCheck_IsGuidProductXInputDevice.
    (cherry picked from commit 25b88a7444915c539248e811dd617b48b7c2ef23)
    
    Conflicts:
    	celt-0.11.0-src
    	sbcelt-src

--- 3rdparty/xinputcheck-src/xinputcheck.h
+++ 3rdparty/xinputcheck-src/xinputcheck.h
@@ -24,11 +24,30 @@
 
 #include <windows.h>
 
+/// XInputCheck_IsGuidProductXInputDevice checks whether the DirectInput
+/// guidProduct given in GuidProductFromDirectInput is a valid XInput device.
+///
+/// This function holds an internal cache of the system's device tree. This
+/// cache can be cleared by calling the XInputCheck_ClearDeviceCache function.
+/// The function does not ever clear this cache itself, so it is important to
+/// to call XInputCheck_ClearDeviceCache when new hardware has been added to
+/// the system.
+///
+/// This function is not thread safe.
+///
+/// @param  pGuidProductFromDirectInput  The DirectInput guidProduct to check.
+///                                      This parameter must not be NULL.
+/// @return  Returns true if the given device is an XInput device. Returns
+///          false if it is not.
+///
+/// @see  XInputCheck_ClearDeviceCache
 bool XInputCheck_IsGuidProductXInputDevice(const GUID *pGuidProductFromDirectInput);
 
 /// XInputCheck_ClearDeviceCache clears XInputCheck's internal device cache.
 /// Calling this function causes the next call to
 /// XInputCheck_IsGuidProductXInputDevice to query the system's device tree.
+///
+/// This function is not thread safe.
 void XInputCheck_ClearDeviceCache();
 
 #endif
