0502fa67b036bae9f07a586d9f05a8bf74c24291
Mikkel Krautz <mikkel@krautz.dk>
2016-04-10 22:58:46 +0200

    Drop OS X specific '64-bit' check.
    QSysInfo::WordSize == 64 seems good enough for us.

--- src/OSInfo.cpp
+++ src/OSInfo.cpp
@@ -423,9 +423,6 @@ void OSInfo::fillXml(QDomDocument &doc, QDomElement &root, const QString &os, co
 	BOOL bIsWow64 = FALSE;
 	IsWow64Process(GetCurrentProcess(), &bIsWow64);
 	bIs64 = bIsWow64;
-#elif defined(Q_OS_MAC)
-	size_t len = sizeof(bool);
-	sysctlbyname("hw.cpu64bit_capable", &bIs64, &len, NULL, 0);
 #else
 	bIs64 = (QSysInfo::WordSize == 64);
 #endif
