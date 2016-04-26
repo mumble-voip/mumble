5c17181b02e86d6bb61d17f3d4341bc2904a766c
Mikkel Krautz <mikkel@krautz.dk>
2016-04-26 22:26:35 +0200

    Move smallft to 3rdparty/smallft-src and add to 3rdPartyLicenses.
    The smallft code is not under Mumble's license, so it should
    live under 3rdparty, and have a proper license notice in our
    binaries.

--- src/mumble/licenses.h
+++ src/mumble/licenses.h
@@ -902,6 +902,37 @@ static const char *licenseQQBonjour =
 	"ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
 
 
+static const char *licenseSmallFT = 
+	"Copyright (c) 2002-2007 Xiph.org Foundation\n"
+	"\n"
+	"Redistribution and use in source and binary forms, with or without\n"
+	"modification, are permitted provided that the following conditions\n"
+	"are met:\n"
+	"\n"
+	"- Redistributions of source code must retain the above copyright\n"
+	"notice, this list of conditions and the following disclaimer.\n"
+	"\n"
+	"- Redistributions in binary form must reproduce the above copyright\n"
+	"notice, this list of conditions and the following disclaimer in the\n"
+	"documentation and/or other materials provided with the distribution.\n"
+	"\n"
+	"- Neither the name of the Xiph.org Foundation nor the names of its\n"
+	"contributors may be used to endorse or promote products derived from\n"
+	"this software without specific prior written permission.\n"
+	"\n"
+	"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS\n"
+	"`AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT\n"
+	"LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR\n"
+	"A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION\n"
+	"OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,\n"
+	"SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT\n"
+	"LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,\n"
+	"DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY\n"
+	"THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT\n"
+	"(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE\n"
+	"OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
+
+
 static const ThirdPartyLicense licenses3rdParties[] = {
 	ThirdPartyLicense("CELT", "http://www.celt-codec.org/", licenseCELT),
 	ThirdPartyLicense("Opus", "http://www.opus-codec.org/", licenseOpus),
@@ -924,6 +955,7 @@ static const ThirdPartyLicense licenses3rdParties[] = {
 	ThirdPartyLicense("Emoji One artwork", "http://emojione.com/", licenseEmojiOne),
 	ThirdPartyLicense("XInputCheck (SDL_IsXInput function)", "https://www.libsdl.org/", licenseXInputCheck),
 	ThirdPartyLicense("QQBonjour", "https://doc.qt.io/archives/qq/qq23-bonjour.html", licenseQQBonjour),
+	ThirdPartyLicense("smallft", "https://www.xiph.org", licenseSmallFT),
 	ThirdPartyLicense(),
 };
 
