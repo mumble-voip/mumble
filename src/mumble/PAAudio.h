d2bda147d9b2f3d6cb85ef109bb150c5b45d50f6
Mikkel Krautz <mikkel@krautz.dk>
2016-05-08 19:46:10 +0200

    src/mumble: update to use LICENSE.header.

--- src/mumble/PAAudio.h
+++ src/mumble/PAAudio.h
@@ -1,3 +1,8 @@
+// Copyright 2005-2016 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
 /* Copyright (C) 2005-2011, Thorvald Natvig <thorvald@natvig.com>
    Copyright (C) 2007, Stefan Gehn <mETz AT gehn DOT net>
 
