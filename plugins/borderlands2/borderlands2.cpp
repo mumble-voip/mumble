313be4b66b6151998ccaed4b70f5cdd2b0ce576b
Mikkel Krautz <mikkel@krautz.dk>
2016-05-08 20:46:07 +0200

    plugins: update to use LICENSE.header.

--- plugins/borderlands2/borderlands2.cpp
+++ plugins/borderlands2/borderlands2.cpp
@@ -1,3 +1,8 @@
+// Copyright 2005-2016 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
 /* Copyright (C) 2013, Steve Hill <github@cheesy.sackheads.org>
    Copyright (C) 2013, Gabriel Risterucci <cleyfaye@gmail.com>
    Copyright (C) 2005-2010, Thorvald Natvig <thorvald@natvig.com> 
