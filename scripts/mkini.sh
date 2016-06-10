f6a0138a19dfe38ba30b8699c3b8f26ef1319413
Mikkel Krautz <mikkel@krautz.dk>
2016-06-11 01:08:11 +0200

    scripts/mkini.sh: consider semicolons as comments to fix .ini generation.
    As-is, the murmur.ini.system used by our PPA builds
    (and Debian-based distros) are broken because the
    script doesn't set the "logfile" and "pidfile"
    options.

--- scripts/mkini.sh
+++ scripts/mkini.sh
@@ -9,10 +9,10 @@ cp murmur.ini murmur.ini.win32
 cp murmur.ini murmur.ini.osx
 cp murmur.ini murmur.ini.system
 
-perl -pi.bak -e 's/^(#|)dbus=.*$/\1dbus=system/' murmur.ini.system
-perl -pi.bak -e 's/^(#|)logfile=.*$/logfile=\/var\/log\/mumble-server\/mumble-server.log/' murmur.ini.system
-perl -pi.bak -e 's/^(#|)pidfile=.*$/pidfile=\/var\/run\/mumble-server\/mumble-server.pid/' murmur.ini.system
-perl -pi.bak -e 's/^(#|)database=.*$/database=\/var\/lib\/mumble-server\/mumble-server.sqlite/' murmur.ini.system
-perl -pi.bak -e 's/^(#|)uname=.*$/uname=mumble-server/' murmur.ini.system
+perl -pi.bak -e 's/^(#|;|)dbus=.*$/\1dbus=system/' murmur.ini.system
+perl -pi.bak -e 's/^(#|;|)logfile=.*$/logfile=\/var\/log\/mumble-server\/mumble-server.log/' murmur.ini.system
+perl -pi.bak -e 's/^(#|;|)pidfile=.*$/pidfile=\/var\/run\/mumble-server\/mumble-server.pid/' murmur.ini.system
+perl -pi.bak -e 's/^(#|;|)database=.*$/database=\/var\/lib\/mumble-server\/mumble-server.sqlite/' murmur.ini.system
+perl -pi.bak -e 's/^(#|;|)uname=.*$/uname=mumble-server/' murmur.ini.system
 
 perl -pi.bak -e 'BEGIN{undef $/;} s/\n/\r\n/smg' murmur.ini.win32
