e5e2f3713c32e5c74e405e1cae6fea35e55fa5b1
Mikkel Krautz <mikkel@krautz.dk>
2016-06-13 00:44:40 +0200

    mumble_plugin_win32.h: add explicit procptr32_t and procptr64_t pointer types and add peekProc overloads for them.
    Right now, a lot of our plugins expect to be able to use
        peekProc<BYTE *>(someAddr);
    to read a pointer value from a game.
    
    When the plugin is built as 32-bit, and the game is 32-bit, all is fine.
    The peekProc operation above will read a 32-bit pointer from the game's
    memory.
    
    However, when the plugin is built as 64-bit, and the game is 32-bit,
    things start to break down, because the `BYTE *` type suddenly isn't
    32-bit wide anymore.
    
    This commit tries to remedy that by introducing explicit types for
    32-bit and 64-bit pointers.
    
    We expect that 32-bit games have one plugin, and 64-bit games have
    another plugin (see for example bf4 and bf4_x86).
    
    With this change, plugins targetting 32-bit games should use the
    new procptr32_t type when querying pointers from the game. Plugins
    targetting 64-bit games should use procptr64_t.
    
    This commit also adds overloads for peekProc() to be able to use
    procptr32_t and procptr64_t as base addresses.
    
    This commit also adds pModule32 and pModule64 variables to
    mumble_plugin_win32.h. These are the base address of the game's
    executable in the game's address space. Plugin authors should use
    the the pModule32 variable for 32-bit games, and the pModule64 variable
    when targetting 64-bit games.
    
    Once we've transitioned the whole repository over to use these new
    explicit pointer types, we can drop the peekProc() overloads that
    take VOID *. We can also drop the 'VOID *'-typed pModule.

--- plugins/mumble_plugin_win32.h
+++ plugins/mumble_plugin_win32.h
@@ -18,9 +18,20 @@
 
 #include "mumble_plugin.h"
 
+#if _MSC_VER < 1800 && defined(M_IX86)
+typedef unsigned long long procptr64_t;
+typedef unsigned long procptr32_t;
+#else
+#include <stdint.h>
+typedef uint64_t procptr64_t;
+typedef uint32_t procptr32_t;
+#endif
+
 DWORD dwPid;
 static HANDLE hProcess;
 static BYTE *pModule;
+static procptr32_t pModule32;
+static procptr64_t pModule64;
 
 static inline DWORD getProcess(const wchar_t *exename) {
 	PROCESSENTRY32 pe;
@@ -47,7 +58,7 @@ static inline BYTE *getModuleAddr(DWORD pid, const wchar_t *modname) {
 	MODULEENTRY32 me;
 	BYTE *addr = NULL;
 	me.dwSize = sizeof(me);
-	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pid);
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE|TH32CS_SNAPMODULE32, pid);
 	if (hSnap != INVALID_HANDLE_VALUE) {
 		BOOL ok = Module32First(hSnap, &me);
 
@@ -69,9 +80,22 @@ static inline BYTE *getModuleAddr(const wchar_t *modname) {
 
 static inline bool peekProc(VOID *base, VOID *dest, SIZE_T len) {
 	SIZE_T r;
-	BOOL ok=ReadProcessMemory(hProcess, base, dest, len, &r);
+	BOOL ok=ReadProcessMemory(hProcess, reinterpret_cast<VOID *>(base), dest, len, &r);
 	return (ok && (r == len));
 }
+
+static inline bool peekProc(procptr32_t base, VOID *dest, SIZE_T len) {
+	SIZE_T r;
+	BOOL ok=ReadProcessMemory(hProcess, reinterpret_cast<VOID *>(base), dest, len, &r);
+	return (ok && (r == len));
+}
+
+static inline bool peekProc(procptr64_t base, VOID *dest, SIZE_T len) {
+	SIZE_T r;
+	BOOL ok=ReadProcessMemory(hProcess, reinterpret_cast<VOID *>(base), dest, len, &r);
+	return (ok && (r == len));
+}
+
 
 template<class T>
 bool peekProc(VOID *base, T &dest) {
@@ -87,9 +111,25 @@ T peekProc(VOID *base) {
 	return v;
 }
 
+template<class T>
+T peekProc(procptr32_t base) {
+	T v = 0;
+	peekProc(base, reinterpret_cast<T *>(&v), sizeof(T));
+	return v;
+}
+
+template<class T>
+T peekProc(procptr64_t base) {
+	T v = 0;
+	peekProc(reinterpret_cast<VOID *>(base), reinterpret_cast<T *>(&v), sizeof(T));
+	return v;
+}
+
 static bool inline initialize(const std::multimap<std::wstring, unsigned long long int> &pids, const wchar_t *procname, const wchar_t *modname = NULL) {
 	hProcess = NULL;
 	pModule = NULL;
+	pModule32 = NULL;
+	pModule64 = NULL;
 
 	if (! pids.empty()) {
 		std::multimap<std::wstring, unsigned long long int>::const_iterator iter = pids.find(std::wstring(procname));
@@ -110,11 +150,15 @@ static bool inline initialize(const std::multimap<std::wstring, unsigned long lo
 		dwPid = 0;
 		return false;
 	}
+	pModule32 = reinterpret_cast<procptr32_t>(pModule);
+	pModule64 = reinterpret_cast<procptr64_t>(pModule);
 
 	hProcess=OpenProcess(PROCESS_VM_READ, false, dwPid);
 	if (!hProcess) {
 		dwPid = 0;
 		pModule = NULL;
+		pModule32 = NULL;
+		pModule64 = NULL;
 		return false;
 	}
 
@@ -126,6 +170,8 @@ static void generic_unlock() {
 		CloseHandle(hProcess);
 		hProcess = NULL;
 		pModule = NULL;
+		pModule32 = NULL;
+		pModule64 = NULL;
 		dwPid = 0;
 	}
 }
