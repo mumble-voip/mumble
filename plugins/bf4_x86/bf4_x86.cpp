d6b3c279e1e3c64529e391dc029d28ec5a9ddc27
Davide Beatrici <davidebeatrici@users.noreply.github.com>
2016-06-14 14:44:29 +0200

    Fixed wrong size

--- plugins/bf4_x86/bf4_x86.cpp
+++ plugins/bf4_x86/bf4_x86.cpp
@@ -39,9 +39,9 @@ static int fetch(float *avatar_pos, float *avatar_front, float *avatar_top, floa
             peekProc(pModule32 + 0x1BB6A90, camera_pos, 12) && // Camera Position values (X, Y and Z).
             peekProc(pModule32 + 0x1BB6A70, avatar_top, 12) && // Avatar Top Vector values (X, Y and Z).
             peekProc(pModule32 + 0x1BB6A80, avatar_front, 12) && // Avatar Front Vector values (X, Y and Z).
-            peekProc(serverid_offset, serverid, 1) && // Server ID (36 characters).
-            peekProc(pModule32 + 0x1BA8A10, host, 1) && // Host value: "IP:Port" when in a server, "bot" when loading and empty when it's hidden.
-            peekProc(pModule32 + 0x1C814B5, team, 1) && // Team value: US (United States); RU (Russia); CH (China).
+            peekProc(serverid_offset, serverid, 37) && // Server ID (36 characters).
+            peekProc(pModule32 + 0x1BA8A10, host, 22) && // Host value: "IP:Port" when in a server, "bot" when loading and empty when it's hidden.
+            peekProc(pModule32 + 0x1C814B5, team, 3) && // Team value: US (United States); RU (Russia); CH (China).
             peekProc(squad_offset_1 + 0x15C, squad, 1) && // Squad value: 0 (not in a squad); 1 (Alpha); 2 (Bravo); 3 (Charlie)... 26 (Zulu).
             peekProc(squad_offset_1 + 0x160, squad_leader, 1) && // Squad leader value: 0 (False); 1 (True).
             peekProc(squad_offset_1 + 0x161, squad_state, 1); // Squad state value: 0 (Public); 1 (Private).
