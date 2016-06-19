e13d6c94bf9a58330a01c0f85d7ed0d0dd0fb6fd
Mikkel Krautz <mikkel@krautz.dk>
2016-06-19 20:29:26 +0200

    overlay_gl: call glDrawArrays with GL_TRIANGLES instead of GL_QUADS.
    GL_QUADS is deprecated in modern GL.

--- overlay_gl/overlay.c
+++ overlay_gl/overlay.c
@@ -468,15 +468,29 @@ static void drawOverlay(Context *ctx, unsigned int width, unsigned int height) {
 	float xmx = right  / w;
 	float ymx = bottom / h;
 
+	GLfloat vertex[] = {
+		left, bottom,
+		left, top,
+		right, top,
 
-	GLfloat vertex[] = {left, bottom,
-	        left, top,
-	        right, top,
-	        right, bottom};
-	GLfloat tex[] = {xm, ymx, xm, ym, xmx, ym, xmx, ymx};
+		left, bottom,
+		right, top,
+		right, bottom
+	};
 	glVertexPointer(2, GL_FLOAT, 0, vertex);
+
+	GLfloat tex[] = {
+		xm, ymx,
+		xm, ym,
+		xmx, ym,
+
+		xm, ymx,
+		xmx, ym,
+		xmx, ymx
+	};
 	glTexCoordPointer(2, GL_FLOAT, 0, tex);
-	glDrawArrays(GL_QUADS, 0, 4);
+
+	glDrawArrays(GL_TRIANGLES, 0, 6);
 
 	glPopMatrix();
 }
