aba01d871eb1df331da568e1fb0473d326972548
Mikkel Krautz <mikkel@krautz.dk>
2016-07-02 12:16:23 +0200

    Timer: use header-only boost::chrono if it is available.
    This re-introduces the functionality of PR #2333 but
    ensures that the breakage noted in mumble-voip/mumble#2366
    no longer happens.
    
    Fixes mumble-voip/mumble#2366.

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -5,6 +5,23 @@
 
 #include "murmur_pch.h"
 
+#include <boost/version.hpp>
+
+// Not all Boost versions can use a header-only
+// boost_system without build failures.
+//
+// See mumble-voip/mumble#2366.
+//
+// This was fixed in Boost 1.56.0.
+//
+// See:
+//  https://github.com/boostorg/system/blob/boost-1.56.0/include/boost/system/error_code.hpp#L514-L516
+// vs.
+//  https://github.com/boostorg/system/blob/boost-1.55.0/include/boost/system/error_code.hpp#L515-L517
+#if BOOST_VERSION >= 105600
+# define USE_BOOST_CHRONO
+#endif
+
 #include "Timer.h"
 
 Timer::Timer(bool start) {
@@ -44,7 +61,23 @@ bool Timer::operator>(const Timer &other) const {
 	return uiStart < other.uiStart;
 }
 
-#if defined(Q_OS_WIN)
+#ifdef USE_BOOST_CHRONO
+// Ensure boost_system is header only.
+#define BOOST_ERROR_CODE_HEADER_ONLY
+// Ensure boost_chrono is header only.
+#define BOOST_CHRONO_DONT_PROVIDE_HYBRID_ERROR_HANDLING
+#define BOOST_CHRONO_HEADER_ONLY
+
+#include <boost/chrono.hpp>
+
+quint64 Timer::now() {
+	using namespace boost::chrono;
+	time_point<steady_clock> now = steady_clock::now();
+	time_point<steady_clock>::duration epochDuration = now.time_since_epoch();
+	microseconds epochDurationUsec = duration_cast<microseconds>(epochDuration);
+	return static_cast<quint64>(epochDurationUsec.count());
+}
+#elif defined(Q_OS_WIN)
 #include <windows.h>
 
 quint64 Timer::now() {
