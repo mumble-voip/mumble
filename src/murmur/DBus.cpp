mumble-voip/mumble

commit f260bd19133086b993a4a0cd027293153b4a15f0

Implement correct write locking for addChannel/removeChannel/link/unlink.

src/murmur/DBus.cpp
@@ -374,7 +374,13 @@ void MurmurDBus::sendMessageChannel(int id, bool tree, const QString &text, cons
 void MurmurDBus::addChannel(const QString &name, int chanparent, const QDBusMessage &msg, int &newid) {
 	CHANNEL_SETUP_VAR(chanparent);
 
-	Channel *nc = server->addChannel(cChannel, name);
+	Channel *nc;
+
+	{
+		QWriteLocker wl(&server->qrwlVoiceThread);
+		server->addChannel(cChannel, name);
+	}
+
 	server->updateChannel(nc);
 	newid = nc->iId;
 
@@ -391,6 +397,8 @@ void MurmurDBus::removeChannel(int id, const QDBusMessage &msg) {
 		qdbc.send(msg.createErrorReply("net.sourceforge.mumble.Error.channel", "Invalid channel id"));
 		return;
 	}
+
+	QWriteLocker wl(&server->qrwlVoiceThread);
 	server->removeChannel(cChannel);
 }
 

src/murmur/Messages.cpp
@@ -1034,8 +1034,11 @@ void Server::msgChannelState(ServerUser *uSource, MumbleProto::ChannelState &msg
 			        QString(* c->cParent),
 			        QString(*p)));
 
-			c->cParent->removeChannel(c);
-			p->addChannel(c);
+			{
+				QWriteLocker wl(&qrwlVoiceThread);
+				c->cParent->removeChannel(c);
+				p->addChannel(c);
+			}
 		}
 		if (! qsName.isNull()) {
 			log(uSource, QString("Renamed channel %1 to %2").arg(QString(*c),

src/murmur/MurmurGRPCImpl.cpp
@@ -1519,7 +1519,13 @@ void V1_ChannelAdd::impl(bool) {
 
 	QString qsName = u8(request.name());
 
-	::Channel *nc = server->addChannel(parent, qsName);
+	::Channel *nc;
+
+	{
+		QWriteLocker wl(&server->qrwlVoiceThread);
+		nc = server->addChannel(parent, qsName);
+	}
+
 	server->updateChannel(nc);
 	int newid = nc->iId;
 
@@ -1541,7 +1547,12 @@ void V1_ChannelRemove::impl(bool) {
 	if (!channel->cParent) {
 		throw ::grpc::Status(::grpc::INVALID_ARGUMENT, "cannot remove the root channel");
 	}
-	server->removeChannel(channel);
+
+	{
+		QWriteLocker wl(&server->qrwlVoiceThread);
+		server->removeChannel(channel);
+	}
+
 	end();
 }
 

src/murmur/RPC.cpp
@@ -127,8 +127,13 @@ bool Server::setChannelStateGRPC(const MumbleProto::ChannelState &cs, QString &e
 				err = QLatin1String("channel cannot be nested in the given parent");
 				return false;
 			}
-			channel->cParent->removeChannel(channel);
-			parent->addChannel(channel);
+
+			{
+				QWriteLocker wl(&qrwlVoiceThread);
+				channel->cParent->removeChannel(channel);
+				parent->addChannel(channel);
+			}
+
 			mpcs.set_parent(parent->iId);
 
 			changed = true;
@@ -229,8 +234,11 @@ bool Server::setChannelState(Channel *cChannel, Channel *cParent, const QString 
 			return false;
 		}
 
-		cChannel->cParent->removeChannel(cChannel);
-		cParent->addChannel(cChannel);
+		{
+			QWriteLocker wl(&qrwlVoiceThread);
+			cChannel->cParent->removeChannel(cChannel);
+			cParent->addChannel(cChannel);
+		}
 
 		mpcs.set_parent(cParent->iId);
 

src/murmur/Server.cpp
@@ -1551,14 +1551,20 @@ void Server::removeChannel(Channel *chan, Channel *dest) {
 	if (dest == NULL)
 		dest = chan->cParent;
 
-	chan->unlink(NULL);
+	{
+		QWriteLocker wl(&qrwlVoiceThread);
+		chan->unlink(NULL);
+	}
 
 	foreach(c, chan->qlChannels) {
 		removeChannel(c, dest);
 	}
 
 	foreach(p, chan->qlUsers) {
-		chan->removeUser(p);
+		{
+			QWriteLocker wl(&qrwlVoiceThread);
+			chan->removeUser(p);
+		}
 
 		Channel *target = dest;
 		while (target->cParent && ! hasPermission(static_cast<ServerUser *>(p), target, ChanACL::Enter))

src/murmur/ServerDB.cpp
@@ -1255,7 +1255,10 @@ QByteArray Server::getUserTexture(int id) {
 }
 
 void Server::addLink(Channel *c, Channel *l) {
-	c->link(l);
+	{
+		QWriteLocker wl(&qrwlVoiceThread);
+		c->link(l);
+	}
 
 	if (c->bTemporary || l->bTemporary)
 		return;
@@ -1275,7 +1278,10 @@ void Server::addLink(Channel *c, Channel *l) {
 }
 
 void Server::removeLink(Channel *c, Channel *l) {
-	c->unlink(l);
+	{
+		QWriteLocker wl(&qrwlVoiceThread);
+		c->unlink(l);
+	}
 
 	if (c->bTemporary || l->bTemporary)
 		return;
@@ -1576,8 +1582,10 @@ void Server::readLinks() {
 
 		Channel *c = qhChannels.value(cid);
 		Channel *l = qhChannels.value(lid);
-		if (c && l)
+		if (c && l) {
+			QWriteLocker wl(&qrwlVoiceThread);
 			c->link(l);
+		}
 	}
 }
 

commit f45c1f78f1f5afae51fdbc7b1d062957c88eb8e1

Murmur: fix MurmurDBus::addChannel that was broken by Murmur's new locking.
This method was broken by commit f260bd19133086b993a4a0cd027293153b4a15f0.

In particular, the line:

    Channel *nc = server->addChannel(cChannel, name);

was removed by a locked block:

    {
        QWriteLocker wl(&server->qrwlVoiceThread);
        server->addChannel(cChannel, name);
    }

Note that nc is never assigned in the locked block.

See
https://github.com/mumble-voip/mumble/commit/f260bd19133086b993a4a0cd027293153b4a15f0#diff-a6537e937b1df62a77d014d7a8c3008bL377
for the full diff.

Fixes mumble-voip/mumble#2392

src/murmur/DBus.cpp
@@ -378,7 +378,7 @@ void MurmurDBus::addChannel(const QString &name, int chanparent, const QDBusMess
 
 	{
 		QWriteLocker wl(&server->qrwlVoiceThread);
-		server->addChannel(cChannel, name);
+		nc = server->addChannel(cChannel, name);
 	}
 
 	server->updateChannel(nc);