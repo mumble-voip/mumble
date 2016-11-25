f01f6e552857938a00dae111a8cb932262e1b871
Mikkel Krautz <mikkel@krautz.dk>
2016-11-25 23:17:33 +0100

    macx/compat: fix build of compat.m shim with Sierra SDK.
    The Sierra SDK seems to require an id<NSApplicationDelegate>
    for its setDelegate: call.
    
    To accomplish this, split off the existing adoption of
    NSFileManagerDelegate into a seperate class extension.
    
    Then, add another class extension for conforming to the
    NSApplicationDelegate protocol.
    
    Both of them are gated with MAC_OS_X_VERSION_MAX_ALLOWED
    to allow building with older SDKs.

--- macx/compat/compat.m
+++ macx/compat/compat.m
@@ -6,10 +6,17 @@
 #import <AppKit/AppKit.h>
 
 @interface CompatApp : NSObject
-#if MAC_OS_X_VERSION_MIN_REQUIRED >= 1070
-<NSFileManagerDelegate>
-#endif
 @end
+
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= 1060
+@interface CompatApp (NSApplicationDelegateExtension) <NSApplicationDelegate>
+@end
+#endif
+
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= 1070
+@interface CompatApp (NSFileManagerDelegateExtension) <NSFileManagerDelegate>
+@end
+#endif
 
 @implementation CompatApp
 
