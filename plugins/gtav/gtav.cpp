501651b1311f37fddab4a56b0c19a9b34994e359
Davide Beatrici <davidebeatrici@gmail.com>
2016-12-24 23:12:00 +0100

    plugins/gtav: update plugin to work with version 1.37 (Steam only)
    Game version: 1.37
    https://support.rockstargames.com/hc/en-us/articles/236222747
    
    The memory addresses of the retail version are different.
    I can't update them because replacing the executables isn't enough: a Social Club account with the retail version is needed.

--- plugins/gtav/gtav.cpp
+++ plugins/gtav/gtav.cpp
@@ -20,18 +20,18 @@ static int fetch(float *avatar_pos, float *avatar_front, float *avatar_top, floa
 	memcmp(buf, strlit, std::min(sizeof(buf), sizeof(strlit)-1)) == 0
 
 	// Steam version
-	if (peekProc(pModule + 0x17C6330, game_name) && VERSION_EQ(game_name, "Grand Theft Auto V")) {
-		state_address = pModule + 0x268C370;
-		in_game_address = pModule + 0x1B76454;
-		avatar_pos_address = pModule + 0x1F05230;
-		camera_pos_address = pModule + 0x1C08080;
-		avatar_base_address = pModule + 0x1B463D0;
-		camera_front_address = pModule + 0x1C09B40;
-		camera_top_address = pModule + 0x1EDA440;
-		player_address = pModule + 0x2696B0C;
-		vehicle_address = pModule + 0x228DDC0;
-		location_address = pModule + 0x228D79B;
-		street_address = pModule + 0x228A550;
+	if (peekProc(pModule + 0x18064D8, game_name) && VERSION_EQ(game_name, "Grand Theft Auto V")) {
+		state_address = pModule + 0x272B9E0;
+		in_game_address = pModule + 0x2453730;
+		avatar_pos_address = pModule + 0x1F76F40;
+		camera_pos_address = pModule + 0x1F73840;
+		avatar_base_address = pModule + 0x1B8E670;
+		camera_front_address = pModule + 0x1F76130;
+		camera_top_address = pModule + 0x1F76120;
+		player_address = pModule + 0x273615C;
+		vehicle_address = pModule + 0x2329D00;
+		location_address = pModule + 0x23296CB;
+		street_address = pModule + 0x2326410;
 	// Retail version
 	} else if (peekProc(pModule + 0x17C3280, game_name) && VERSION_EQ(game_name, "Grand Theft Auto V")) {
 		state_address = pModule + 0x2688DB0;
@@ -200,10 +200,10 @@ static int trylock(const std::multimap<std::wstring, unsigned long long int> &pi
 }
 
 static const std::wstring longdesc() {
-	return std::wstring(L"Supports Grand Theft Auto V version 1.35 with identity support."); // Plugin long description
+	return std::wstring(L"Supports Grand Theft Auto V version 1.37 with identity support."); // Plugin long description
 }
 
-static std::wstring description(L"Grand Theft Auto V (v1.35)"); // Plugin short description
+static std::wstring description(L"Grand Theft Auto V (v1.37)"); // Plugin short description
 static std::wstring shortname(L"Grand Theft Auto V"); // Plugin short name
 
 static int trylock1() {
