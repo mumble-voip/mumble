affa2c4b56ce1969347b7bd8003ab018a96e2b5b
Mikkel Krautz <mikkel@krautz.dk>
2017-02-20 23:44:57 +0100

    Meta: use Server::privateKeyFromPEM.

--- src/murmur/Meta.cpp
+++ src/murmur/Meta.cpp
@@ -404,22 +404,10 @@ void MetaParams::read(QString fname) {
 
 	if (! key.isEmpty() || ! crt.isEmpty()) {
 		if (! key.isEmpty()) {
-			qskKey = QSslKey(key, QSsl::Rsa, QSsl::Pem, QSsl::PrivateKey, qbaPassPhrase);
-			if (qskKey.isNull())
-				qskKey = QSslKey(key, QSsl::Dsa, QSsl::Pem, QSsl::PrivateKey, qbaPassPhrase);
-#if QT_VERSION >= 0x050000
-			if (qskKey.isNull())
-				qskKey = QSslKey(key, QSsl::Ec, QSsl::Pem, QSsl::PrivateKey, qbaPassPhrase);
-#endif
+			qskKey = Server::privateKeyFromPEM(key, qbaPassPhrase);
 		}
 		if (qskKey.isNull() && ! crt.isEmpty()) {
-			qskKey = QSslKey(crt, QSsl::Rsa, QSsl::Pem, QSsl::PrivateKey, qbaPassPhrase);
-			if (qskKey.isNull())
-				qskKey = QSslKey(crt, QSsl::Dsa, QSsl::Pem, QSsl::PrivateKey, qbaPassPhrase);
-#if QT_VERSION >= 0x050000
-			if (qskKey.isNull())
-				qskKey = QSslKey(crt, QSsl::Ec, QSsl::Pem, QSsl::PrivateKey, qbaPassPhrase);
-#endif
+			qskKey = Server::privateKeyFromPEM(crt, qbaPassPhrase);
 			if (! qskKey.isNull())
 				qCritical("Using private key found in certificate file.");
 		}
