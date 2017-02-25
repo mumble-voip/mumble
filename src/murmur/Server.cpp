e669044e76a97d9859966d4b39273d342053dc8b
Mikkel Krautz <mikkel@krautz.dk>
2017-02-26 00:51:54 +0100

    murmur: use SHA-256 keys for blobs in protocol messages.
    This changes Murmur to return SHA-256 hashes for large blobs in protocol
    messages. Previously, Murmur used SHA-1.
    
    This has no real effect on clients. Clients trust the content-addressable
    keys returned by Murmur for its content. No matter which hash function the
    server is using, it will use the server-advertised keys in its own
    blobstore. Thus, it is a fairly harmless operation to change the
    hash-function used for blobs keys. The only thing it will trigger is that
    existing blobs might need to be re-downloaded once.

--- src/murmur/Server.cpp
+++ src/murmur/Server.cpp
@@ -1917,7 +1917,7 @@ void Server::recheckCodecVersions(ServerUser *connectingUser) {
 void Server::hashAssign(QString &dest, QByteArray &hash, const QString &src) {
 	dest = src;
 	if (src.length() >= 128)
-		hash = sha1(src);
+		hash = namedSha256(src);
 	else
 		hash = QByteArray();
 }
@@ -1925,7 +1925,7 @@ void Server::hashAssign(QString &dest, QByteArray &hash, const QString &src) {
 void Server::hashAssign(QByteArray &dest, QByteArray &hash, const QByteArray &src) {
 	dest = src;
 	if (src.length() >= 128)
-		hash = sha1(src);
+		hash = namedSha256(src);
 	else
 		hash = QByteArray();
 }
