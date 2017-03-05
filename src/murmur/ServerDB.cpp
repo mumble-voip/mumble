1818476399feebb71b9a79e89eea3366b9731d72
Mikkel Krautz <mikkel@krautz.dk>
2017-03-05 22:42:57 +0100

    ServerDB, Meta: add support for SQLite WAL.
    Using SQLite's WAL (write-ahead log) can create less disk I/O while
    still providing good consistency and durability.
    
    This change uses SQLite's WAL with synchronous=NORMAL which can
    cause loss of transactions on power failure. Only the transactions
    which haven't been synced to the disk by the OS are lost. The
    database itself will still be in a consistent state, but it might
    not have all recent changes.

--- src/murmur/ServerDB.cpp
+++ src/murmur/ServerDB.cpp
@@ -147,6 +147,51 @@ ServerDB::ServerDB() {
 		qFatal("ServerDB: Failed initialization: %s",qPrintable(e.text()));
 	}
 
+	// Use SQLite in WAL mode if possible.
+	if (Meta::mp.qsDBDriver == "QSQLITE") {
+		if (Meta::mp.iSQLiteWAL == 0) {
+			qWarning("ServerDB: Using SQLite's default rollback journal.");
+		} else if (Meta::mp.iSQLiteWAL > 0 && Meta::mp.iSQLiteWAL <= 2) {
+			QSqlQuery query;
+
+			bool hasversion = false;
+			bool okversion = false;
+			QString version;
+			SQLDO("SELECT sqlite_version();");
+			if (query.next()) {
+				version = query.value(0).toString();
+			}
+
+			QStringList splitVersion = version.split(QLatin1String("."));
+			int major = 0, minor = 0;
+			if (splitVersion.count() >= 3) {
+				major = splitVersion.at(0).toInt();
+				minor = splitVersion.at(1).toInt();
+				hasversion = true;
+			}
+			if (major >= 3 || (major == 3 && minor >= 7)) {
+				okversion = true;
+			}
+
+			if (okversion) {
+				SQLDO("PRAGMA journal_mode=WAL;");
+				if (Meta::mp.iSQLiteWAL == 1) {
+					SQLDO("PRAGMA synchronous=NORMAL;");
+					qWarning("ServerDB: Configured SQLite for journal_mode=WAL, synchronous=NORMAL");
+				} else if (Meta::mp.iSQLiteWAL == 2) {
+					SQLDO("PRAGMA synchronous=FULL;");
+					qWarning("ServerDB: Configured SQLite for journal_mode=WAL, synchronous=FULL");
+				}
+			} else if (hasversion) {
+				qWarning("ServerDB: Unable to use SQLite in WAL mode due to incompatible SQLite version %i.%i", major, minor);
+			} else {
+				qWarning("ServerDB: Unable to use SQLite in WAL mode because the SQLite version could not be determined.");
+			}
+		} else {
+			qFatal("ServerDB: Invalid value '%i' for sqlite_wal. Please use 0 (no wal), 1 (wal), 2 (wal with synchronous=full)", Meta::mp.iSQLiteWAL);
+		}
+	}
+
 	TransactionHolder th;
 
 	QSqlQuery &query = *th.qsqQuery;
