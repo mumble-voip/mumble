287eb5ae8ac6c650c83545f8ab17d5fef4b82c73
Mikkel Krautz <mikkel@krautz.dk>
2017-05-04 01:41:26 +0200

    Simplify certificate and private key serialization.

--- src/murmur/Cert.cpp
+++ src/murmur/Cert.cpp
@@ -151,7 +151,7 @@ static bool selfSignedServerCert_SHA1_RSA_2048(QSslCertificate &qscCert, QSslKey
 		goto out;
 	}
 
-	if (ok) {
+	{
 		QByteArray crt;
 		int len = i2d_X509(x509, NULL);
 		if (len <= 0) {
@@ -169,10 +169,11 @@ static bool selfSignedServerCert_SHA1_RSA_2048(QSslCertificate &qscCert, QSslKey
 		qscCert = QSslCertificate(crt, QSsl::Der);
 		if (qscCert.isNull()) {
 			ok = false;
+			goto out;
 		}
 	}
 
-	if (ok) {
+	{
 		QByteArray key;
 		int len = i2d_PrivateKey(pkey, NULL);
 		if (len <= 0) {
@@ -190,6 +191,7 @@ static bool selfSignedServerCert_SHA1_RSA_2048(QSslCertificate &qscCert, QSslKey
 		qskKey = QSslKey(key, QSsl::Rsa, QSsl::Der);
 		if (qskKey.isNull()) {
 			ok = false;
+			goto out;
 		}
 	}
 
