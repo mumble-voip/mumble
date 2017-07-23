mumble-voip/mumble

commit 4d6e28e6dec7c812f77ad542a61e3ac600daf3ea

ConnectDialog: update the connect dialog to use ServerResolver for hostname lookups.

src/mumble/ConnectDialog.cpp
@@ -18,6 +18,7 @@
 #include "Global.h"
 #include "ServerHandler.h"
 #include "WebFetch.h"
+#include "ServerResolver.h"
 
 QMap<QString, QIcon> ServerItem::qmIcons;
 QList<PublicInfo> ConnectDialog::qlPublicServers;
@@ -1399,7 +1400,9 @@ void ConnectDialog::timeTick() {
 			qlDNSLookup.append(host);
 
 			qsDNSActive.insert(host);
-			QHostInfo::lookupHost(host, this, SLOT(lookedUp(QHostInfo)));
+			ServerResolver *sr = new ServerResolver();
+			QObject::connect(sr, SIGNAL(resolved()), this, SLOT(lookedUp()));
+			sr->resolve(host, 64738); // XXX: get port
 			break;
 		}
 	}
@@ -1532,24 +1535,31 @@ void ConnectDialog::stopDns(ServerItem *si) {
 	}
 }
 
-void ConnectDialog::lookedUp(QHostInfo info) {
-	QString host = info.hostName().toLower();
+void ConnectDialog::lookedUp() {
+	ServerResolver *sr = qobject_cast<ServerResolver *>(QObject::sender());
+	sr->deleteLater();
+
+	QString host = sr->hostname().toLower();
+
 	qsDNSActive.remove(host);
 
-	if (info.error() != QHostInfo::NoError)
+	if (sr->records().size() == 0) { // XXX: error
 		return;
+	}
 
 	QSet<qpAddress> qs;
+	foreach (ServerResolverRecord record, sr->records()) {
+		foreach(const HostAddress &ha, record.addresses()) {
+			qs.insert(qpAddress(ha, sr->port()));
+		}
+	}
 
 	foreach(ServerItem *si, qhDNSWait[host]) {
-		QList<qpAddress> addresses;
-		foreach(const QHostAddress &qha, info.addresses()) {
-			qpAddress addr(HostAddress(qha), si->usPort);
-			addresses.append(addr);
-			qs.insert(addr);
+		foreach (const qpAddress &addr, qs) {
 			qhPings[addr].insert(si);
 		}
-		si->qlAddressPorts = addresses;
+
+		si->qlAddressPorts = qs.toList();
 
 		if (si == qtwServers->currentItem()) {
 			on_qtwServers_currentItemChanged(si, si);

src/mumble/ConnectDialog.h
@@ -288,7 +288,7 @@ class ConnectDialog : public QDialog, public Ui::ConnectDialog {
 		void fetched(QByteArray xmlData, QUrl, QMap<QString, QString>);
 
 		void udpReply();
-		void lookedUp(QHostInfo);
+		void lookedUp();
 		void timeTick();
 
 		void on_qaFavoriteAdd_triggered();

commit bb4b36d9f834cd90f15d854943a5dc5066386c9a

ConnectDialog: re-arrange lookedUp() code to avoid recursive runloop problem.
This ensures that we apply all globally visible state from the lookedUp()
method before we attempt to auto-connect.

The previous block of code would, when auto-connect is enabled, call
accept() to perform the auto-connect. In case the selected ServerItem
has no username set, accept() will trigger a dialog box to appear asking
the user to input their desired username. Unfortunately, showing this
dialog causes Mumble to enter the event loop before the lookedUp()
method anticipated it to be entered. The point at which the auto-connect
happens, is before the lookedUp() method has had time to update the
global state of the ConnectDialog to reflect the successful host lookup.
In practice, this meant that the auto-connect would be triggered many,
many times in a row. In this scenario, Mumble would open a new username
dialog box for each auto-connect attempt. This would cause Mumble to
become unresponsive, because Mumble would spawn infinitely many username
dialog boxes.

The code of the lookedUp() method was re-arranged in
4d6e28e6dec7c812f77ad542a61e3ac600daf3ea, which introduced this problem.

Fixes mumble-voip/mumble#3191

src/mumble/ConnectDialog.cpp
@@ -1569,23 +1569,26 @@ void ConnectDialog::lookedUp() {
 		}
 	}
 
-	foreach(ServerItem *si, qhDNSWait[unresolved]) {
+	QSet<ServerItem *> waiting = qhDNSWait[unresolved];
+	foreach(ServerItem *si, waiting) {
 		foreach (const ServerAddress &addr, qs) {
 			qhPings[addr].insert(si);
 		}
 
 		si->qlAddresses = qs.toList();
+	}
 
+	qlDNSLookup.removeAll(unresolved);
+	qhDNSCache.insert(unresolved, qs.toList());
+	qhDNSWait.remove(unresolved);
+
+	foreach(ServerItem *si, waiting) {
 		if (si == qtwServers->currentItem()) {
 			on_qtwServers_currentItemChanged(si, si);
 			if (si == siAutoConnect)
 				accept();
 		}
 	}
-
-	qlDNSLookup.removeAll(unresolved);
-	qhDNSCache.insert(unresolved, qs.toList());
-	qhDNSWait.remove(unresolved);
 
 	if (bAllowPing) {
 		foreach(const ServerAddress &addr, qs) {