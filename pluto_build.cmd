d88b5dc5120b9a604255217a7dbd3cf6c7a2544b
Scott Rankin <scott@plutovr.com>
2017-08-17 19:10:40 -0700

    moving this out of a batch label and under the specific areas so I don't have to try and get start /wait to work

--- pluto_build.cmd
+++ pluto_build.cmd
@@ -9,19 +9,15 @@ IF ERRORLEVEL 2 GOTO ConfigDev
 IF ERRORLEVEL 1 GOTO ConfigRelease
 
 :ConfigRelease
-call:SetupSpeex
+git submodule init
+git submodule update
 qmake -recursive main.pro CONFIG+="release static no-game-plugins no-elevation no-g15 no-asio no-overlay no-server" CONFIG-=sse2 DEFINES+="NO_UPDATE_CHECK NO_CRASH_REPORT PLUTOVR_BUILD"
 goto Done
 
 :ConfigDev
-call:SetupSpeex
+git submodule init
+git submodule update
 qmake -recursive main.pro CONFIG+="static no-game-plugins no-elevation no-g15 no-asio no-overlay no-server" CONFIG-=sse2 DEFINES+="DEBUG NO_UPDATE_CHECK NO_CRASH_REPORT PLUTOVR_BUILD"
 
 :Done
 nmake
-exit
-
-:SetupSpeex
-git submodule init
-git submodule update
-goto:eof
