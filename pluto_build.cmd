d3cb8fa28c3b6e60521ec5a5174eabbc1d6d0430
Scott Rankin <scott@plutovr.com>
2017-08-17 20:07:43 -0700

    flailing here. not sure why it isn't working on my computer

--- pluto_build.cmd
+++ pluto_build.cmd
@@ -9,15 +9,23 @@ IF ERRORLEVEL 2 GOTO ConfigDev
 IF ERRORLEVEL 1 GOTO ConfigRelease
 
 :ConfigRelease
-git submodule init
-git submodule update
-qmake -recursive main.pro CONFIG+="release static no-game-plugins no-elevation no-g15 no-asio no-overlay no-server" CONFIG-=sse2 DEFINES+="NO_UPDATE_CHECK NO_CRASH_REPORT PLUTOVR_BUILD"
+echo ConfigRelease begin
+@echo on
+git submodule init & ^
+git submodule update & ^
+echo qmake -recursive main.pro CONFIG+="release static no-game-plugins no-elevation no-g15 no-asio no-overlay no-server" CONFIG-=sse2 DEFINES+="NO_UPDATE_CHECK NO_CRASH_REPORT PLUTOVR_BUILD" && ^
+echo ConfigRelease end & ^
 goto Done
 
 :ConfigDev
-git submodule init
-git submodule update
+@echo on
+git submodule init & ^
+git submodule update & ^
 qmake -recursive main.pro CONFIG+="static no-game-plugins no-elevation no-g15 no-asio no-overlay no-server" CONFIG-=sse2 DEFINES+="DEBUG NO_UPDATE_CHECK NO_CRASH_REPORT PLUTOVR_BUILD"
+@echo off
 
 :Done
-nmake
+echo Done begin
+@echo on
+echo nmake
+@echo off
