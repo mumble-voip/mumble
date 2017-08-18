c33f5de83c736b51ac19a3b62a7ef35306e45c1a
Jan Klass <kissaki@posteo.de>
2017-08-19 00:18:51 +0200

    Fix code formatting

--- src/mumble/ConnectDialog.cpp
+++ src/mumble/ConnectDialog.cpp
@@ -813,7 +813,7 @@ ConnectDialogEdit::~ConnectDialogEdit() {
 	delete m_si;
 }
 
-void ConnectDialogEdit::showNotice(const QString & text) {
+void ConnectDialogEdit::showNotice(const QString &text) {
 	QLabel *label = qwInlineNotice->findChild<QLabel *>(QLatin1String("qlPasteNotice"));
 	Q_ASSERT(label);
 	label->setText(text);
