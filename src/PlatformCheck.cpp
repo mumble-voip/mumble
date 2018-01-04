3c400f4f0224b43d6b236355dd7519a4c5f35890
Mikkel Krautz <mikkel@krautz.dk>
2018-01-04 10:57:57 +0100

    Add PlatformCheck class, for runtime platform detection.

---
+++ src/PlatformCheck.cpp
@@ -0,0 +1,20 @@
+// Copyright 2005-2018 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#include "murmur_pch.h"
+
+#include <QtCore/QLibrary>
+
+#include "PlatformCheck.h"
+
+bool PlatformCheck::IsWine() {
+#ifdef Q_OS_WIN
+	void *ptr = QLibrary::resolve(QLatin1String("ntdll.dll"), "wine_get_version");
+	if (ptr != NULL) {
+		return true;
+	}
+#endif
+	return false;
+}
