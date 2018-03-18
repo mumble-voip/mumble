004a10537718a258581a997913cf8e4ad204b8f5
Mikkel Krautz <mikkel@krautz.dk>
2018-03-18 16:06:49 +0100

    SvgIcon: add new class to work around issues with SVG QIcons in Plasma/KStatusNotifierItem
    This commit adds new class SvgIcon which allows you to create a QPixmap-backed QIcon
    from an SVG.
    
    We need this to fix mumble-voip/mumble#3124.
    
    Basically, SVG-backed QIcons returns an empty list for QIcon::availableSizes().
    There is some confusion as to whether this is the expected behavior or not.
    (Tracked in mumble-voip/mumble#3374).
    
    However: Plasma/KStatusNotifierItem currently expects QIcon::availableSizes()
    to return a valid list of available sizes. For our SVG icons, that is not the case.
    
    This commit works around this by constructing a QIcon backed by pixmaps, such that
    QIcon::availableSizes() returns something sensible.
    
    This is implemented via the static method, SvgIcon::addSvgPixmapsToIcon(), which renders
    SVG icon in various common sizes (including 22x22 and 44x44, the Plasma notification area
    size, and its @2x size).

---
+++ src/mumble/SvgIcon.cpp
@@ -0,0 +1,40 @@
+// Copyright 2005-2018 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#include "SvgIcon.h"
+
+#include <QSvgRenderer>
+#include <QPainter>
+
+void SvgIcon::addSvgPixmapsToIcon(QIcon &icon, QString fn) {
+	QSvgRenderer svg(fn);
+
+	QList<QSize> commonSizes;
+	commonSizes << QSize(8, 8);
+	commonSizes << QSize(16, 16);
+	commonSizes << QSize(22, 22); // Plasma notification area size
+	commonSizes << QSize(24, 24);
+	commonSizes << QSize(32, 32);
+	commonSizes << QSize(44, 44); // Plasma notification area size @x2
+	commonSizes << QSize(48, 48);
+	commonSizes << QSize(64, 64);
+	commonSizes << QSize(96, 96);
+	commonSizes << QSize(128, 128);
+	commonSizes << QSize(256, 256);
+
+	foreach (QSize size, commonSizes) {
+		QPixmap pm(size);
+		pm.fill(Qt::transparent);
+
+		QPainter p(&pm);
+		p.setRenderHint(QPainter::Antialiasing);
+		p.setRenderHint(QPainter::TextAntialiasing);
+		p.setRenderHint(QPainter::SmoothPixmapTransform);
+		p.setRenderHint(QPainter::HighQualityAntialiasing);
+		svg.render(&p);
+
+		icon.addPixmap(pm);
+	}
+}
