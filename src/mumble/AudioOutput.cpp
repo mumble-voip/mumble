fe3cdae138f18b41f4d19e0e440c860c48a40032
olzzon <github@olzzon.dk>
2018-04-11 21:24:14 +0200

    If #BACKGROUND_SOURCE is inserted in a Users Commentfield Mumble will turn down the volume while others are speaking.
    It's great for background music or intorcom on top of a TV/Radio-Program
    
    Edit: Changed Tabs instead of spaces

--- src/mumble/AudioOutput.cpp
+++ src/mumble/AudioOutput.cpp
@@ -476,24 +476,21 @@ bool AudioOutput::mix(void *outbuff, unsigned int nsamp) {
 			validListener = true;
 		}
 
-        int numberOfSpeakers = qlMix.count(); // numberOfSpeakers are used for turning down background users
+		int numberOfSpeakers = qlMix.count(); // numberOfSpeakers are used for turning down background users
 
 		foreach(AudioOutputUser *aop, qlMix) {
 			const float * RESTRICT pfBuffer = aop->pfBuffer;
-            float volumeAdjustment = 1;
+			float volumeAdjustment = 1;
 
-            AudioOutputSpeech *speech = qobject_cast<AudioOutputSpeech *>(aop);
+			AudioOutputSpeech *speech = qobject_cast<AudioOutputSpeech *>(aop);
 			if (speech) {
-                const ClientUser *user = speech->p;
+				const ClientUser *user = speech->p;
 				volumeAdjustment *= user->fLocalVolume;
-
-
-                // Turn down user while others are speaking - if #BACKGROUND_SOURCE tag is added in user comment
-                if (numberOfSpeakers>1 && (std::string::npos != user->qsComment.toStdString().find("#BACKGROUND_SOURCE"))) {
-                    volumeAdjustment = 0.3;
-                }
-
-                if (prioritySpeakerActive) {
+				// Turn down user while others are speaking - if #BACKGROUND_SOURCE tag is added in user comment
+				if (numberOfSpeakers>1 && (user->qsComment.toStdString().find("#BACKGROUND_SOURCE") != std::string::npos)) {
+					volumeAdjustment = 0.2;
+				}
+				if (prioritySpeakerActive) {
 					
 					if (user->tsState != Settings::Whispering
 					    && !user->bPrioritySpeaker) {
