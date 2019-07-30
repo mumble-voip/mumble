e9655daac5ab036588cb16f2dae87fc78ff37696
Davide Beatrici <davidebeatrici@gmail.com>
2019-07-30 22:03:50 +0200

    ACLEditor.cpp: replace deprecated QList::swap() overload with QList::swapItemsAt()
    https://github.com/qt/qtbase/commit/7f4d0405b409b1d3aa9d91e31972669576ec698c

--- src/mumble/ACLEditor.cpp
+++ src/mumble/ACLEditor.cpp
@@ -752,7 +752,11 @@ void ACLEditor::on_qpbACLUp_clicked() {
 	if (idx <= numInheritACL + 1)
 		return;
 
+#if QT_VERSION >= QT_VERSION_CHECK(5, 13, 0)
+	qlACLs.swapItemsAt(idx - 1, idx);
+#else
 	qlACLs.swap(idx - 1, idx);
+#endif
 	qlwACLs->setCurrentRow(qlwACLs->currentRow() - 1);
 	refillACL();
 }
@@ -766,7 +770,11 @@ void ACLEditor::on_qpbACLDown_clicked() {
 	if (idx >= qlACLs.count())
 		return;
 
+#if QT_VERSION >= QT_VERSION_CHECK(5, 13, 0)
+	qlACLs.swapItemsAt(idx - 1, idx);
+#else
 	qlACLs.swap(idx - 1, idx);
+#endif
 	qlwACLs->setCurrentRow(qlwACLs->currentRow() + 1);
 	refillACL();
 }
