mumble-voip/mumble

commit 3cd2984e8ae6203edb3d4a0efe42041a846bf639

SSL.cpp: replace deprecated QSslSocket::systemCaCertificates() with QSslConfiguration::systemCaCertificates()
https://github.com/qt/qtbase/commit/92cda9474245c79b635c21cd140c5d0a3a6d2e5b

src/SSL.cpp
@@ -214,7 +214,7 @@ void MumbleSSL::addSystemCA() {
 
 #if QT_VERSION >= 0x040800
 	// Don't perform on-demand loading of root certificates
-	QSslSocket::addDefaultCaCertificates(QSslSocket::systemCaCertificates());
+	QSslSocket::addDefaultCaCertificates(QSslConfiguration::systemCaCertificates());
 #endif
 
 #ifdef Q_OS_WIN

commit 501d7c7350f543ad4273ea6ab53bbaa2b44407c1

Use QSslSocket for Ca Certs on Qt5 < 5.5
Fixes regression caused by 3cd2984e8a

src/SSL.cpp
@@ -212,9 +212,11 @@ void MumbleSSL::addSystemCA() {
 	QSslSocket::setDefaultCaCertificates(ql);
 #endif // NO_SYSTEM_CA_OVERRIDE
 
-#if QT_VERSION >= 0x040800
 	// Don't perform on-demand loading of root certificates
+#if QT_VERSION >= 0x050500
 	QSslSocket::addDefaultCaCertificates(QSslConfiguration::systemCaCertificates());
+#elif QT_VERSION >= 0x040800
+	QSslSocket::addDefaultCaCertificates(QSslSocket::systemCaCertificates());
 #endif
 
 #ifdef Q_OS_WIN