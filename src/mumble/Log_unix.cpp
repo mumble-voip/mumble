6ae06f41d9b68b8cc1d1ec08b7846f264c2a4854
c_14 <git@c-14.de>
2019-10-03 17:13:13 +0200

    Add include guards for builds without dbus
    This commit adds include guards around a QDBusInterface include added in
    commit 15831dbca without the appropriate guards.

--- src/mumble/Log_unix.cpp
+++ src/mumble/Log_unix.cpp
@@ -7,7 +7,9 @@
 #include "MainWindow.h"
 #include "Settings.h"
 
+#ifdef USE_DBUS
 #include <QtDBus/QDBusInterface>
+#endif
 
 // We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name (like protobuf 3.7 does). As such, for now, we have to make this our last include.
 #include "Global.h"
