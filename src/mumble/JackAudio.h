2e1d3f112ffd3abbc6e7d899b866714119867f0f
Davide Beatrici <davidebeatrici@gmail.com>
2019-10-31 09:24:33 +0100

    JackAudio: load symbols at runtime
    We check for the following libraries:
    
    - Windows: "libjack64.dll" and "libjack32.dll". Those are the libraries that are shipped with the official installer, which copies them in "C:\Windows".
    - macOS: "libjack.dylib" and "libjack.0.dylib".
    - Everything else: "libjack.so" and "libjack.so.0".
    
    This commit also:
    
    - Adds a debug message so that a user knows which library was loaded and its version.
    - Renames "inputBuffer" to "outputBuffer" in JackAudioOutput::process(), the previous name was only correct from the perspective of the JACK port.
    - Renames "input" to "inputBuffer" in JackAudioInput::process(), the previous name was unclear.

--- src/mumble/JackAudio.h
+++ src/mumble/JackAudio.h
@@ -9,10 +9,11 @@
 #include "AudioInput.h"
 #include "AudioOutput.h"
 
+#include <QtCore/QLibrary>
 #include <QtCore/QVector>
 #include <QtCore/QWaitCondition>
 
-#include <jack/jack.h>
+#include <jack/types.h>
 
 #define JACK_MAX_OUTPUT_PORTS 2
 
@@ -28,8 +29,10 @@ class JackAudioSystem : public QObject {
 		Q_DISABLE_COPY(JackAudioSystem)
 
 	protected:
+		bool bAvailable;
 		uint8_t users;
 		QMutex qmWait;
+		QLibrary qlJack;
 		QWaitCondition qwcWait;
 		jack_client_t *client;
 
@@ -37,6 +40,29 @@ class JackAudioSystem : public QObject {
 		static int sampleRateCallback(jack_nframes_t, void *);
 		static int bufferSizeCallback(jack_nframes_t frames, void *);
 		static void shutdownCallback(void *);
+
+		const char *(*jack_get_version_string)();
+		const char **(*jack_get_ports)(jack_client_t *client, const char *port_name_pattern, const char *type_name_pattern, unsigned long flags);
+		char *(*jack_get_client_name)(jack_client_t *client);
+		char *(*jack_port_name)(jack_port_t *port);
+		int (*jack_client_close)(jack_client_t *client);
+		int (*jack_activate)(jack_client_t *client);
+		int (*jack_deactivate)(jack_client_t *client);
+		int (*jack_set_process_callback)(jack_client_t *client, JackProcessCallback process_callback, void *arg);
+		int (*jack_set_sample_rate_callback)(jack_client_t *client, JackSampleRateCallback process_callback, void *arg);
+		int (*jack_set_buffer_size_callback)(jack_client_t *client, JackBufferSizeCallback process_callback, void *arg);
+		int (*jack_on_shutdown)(jack_client_t *client, JackShutdownCallback process_callback, void *arg);
+		int (*jack_connect)(jack_client_t *client, const char *source_port, const char *destination_port);
+		int (*jack_port_disconnect)(jack_client_t *client, jack_port_t *port);
+		int (*jack_port_unregister)(jack_client_t *client, jack_port_t *port);
+		int (*jack_port_flags)(const jack_port_t *port);
+		void *(*jack_port_get_buffer)(jack_port_t *port, jack_nframes_t frames);
+		void (*jack_free)(void *ptr);
+		jack_client_t *(*jack_client_open)(const char *client_name, jack_options_t options, jack_status_t *status, ...);
+		jack_nframes_t(*jack_get_sample_rate)(jack_client_t *client);
+		jack_nframes_t(*jack_get_buffer_size)(jack_client_t *client);
+		jack_port_t *(*jack_port_by_name)(jack_client_t *client, const char *port_name);
+		jack_port_t *(*jack_port_register)(jack_client_t *client, const char *port_name, const char *port_type, unsigned long flags, unsigned long buffer_size);
 
 	public:
 		QHash<QString, QString> qhInput;
@@ -47,6 +73,7 @@ class JackAudioSystem : public QObject {
 		jack_nframes_t sampleRate();
 		jack_nframes_t bufferSize();
 		JackPorts getPhysicalPorts(const uint8_t &flags);
+		void *getPortBuffer(jack_port_t *port, const jack_nframes_t &frames);
 		jack_port_t *registerPort(const char *name, const uint8_t &flags);
 		bool unregisterPort(jack_port_t *port);
 		bool connectPort(jack_port_t *sourcePort, jack_port_t *destinationPort);
