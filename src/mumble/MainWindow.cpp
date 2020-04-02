a6bd9aacebe472740b1a3d59eb965c04af702fd0
Robert Adam <dev@robert-adam.de>
2020-04-02 18:20:50 +0200

    src/mumble/MainWindow.cpp: Add option to stop listening to the listener proxy's context menu as well

--- src/mumble/MainWindow.cpp
+++ src/mumble/MainWindow.cpp
@@ -1608,6 +1608,10 @@ void MainWindow::qmListener_aboutToShow() {
 
 	if (self) {
 		qmListener->addAction(qaListenerLocalVolume);
+		if (cContextChannel) {
+			qmListener->addAction(qaChannelListen);
+			qaChannelListen->setChecked(cContextChannel->isListening(ClientUser::get(g.uiSession)));
+		}
 	} else {
 		qmListener->addAction(qaEmpty);
 	}
