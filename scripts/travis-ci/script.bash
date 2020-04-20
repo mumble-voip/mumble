4ab0e03492f65ec29009250733124d4c05b62303
Robert Adam <dev@robert-adam.de>
2020-04-20 08:56:21 +0200

    CI: Include GRPC support
    We have had multiple cases now in which there have been some errors in
    changes related to GRPC that weren't detected by the CI because it
    excluded the GRPC code from the build process. This will bw changed now.
    
    As all other CIs have been stubborn, for now we'll only build GRPC on
    travis as these use a new-enough Ubuntu version to be able to build
    GRPC.
    MacOS CI I haven't got to work with the whole homebrew stuff and Windows
    is done in its own repo (which I won't touch either).
    
    m

--- scripts/travis-ci/script.bash
+++ scripts/travis-ci/script.bash
@@ -17,11 +17,11 @@ if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
 		if [ "${MUMBLE_NO_PCH}" == "1" ]; then
 			EXTRA_CONFIG="no-pch ${EXTRA_CONFIG}"
 		fi
-		qmake CONFIG+="release tests g15-emulator ${EXTRA_CONFIG}" DEFINES+="MUMBLE_VERSION=${TRAVIS_COMMIT:0:7}" -recursive
+		qmake CONFIG+="release tests g15-emulator grpc ${EXTRA_CONFIG}" DEFINES+="MUMBLE_VERSION=${TRAVIS_COMMIT:0:7}" -recursive
 		make -j2
 		make check
 	elif [ "${MUMBLE_HOST}" == "aarch64-linux-gnu" ]; then
-		qmake CONFIG+="release tests warnings-as-errors ${EXTRA_CONFIG}" -recursive
+		qmake CONFIG+="release tests warnings-as-errors grpc ${EXTRA_CONFIG}" -recursive
 		make -j $(nproc)
 	elif [ "${MUMBLE_HOST}" == "i686-w64-mingw32" ]; then
 		wget http://www.steinberg.net/sdk_downloads/asiosdk2.3.zip -P ../
