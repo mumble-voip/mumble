428f1626e37492ebfb3dc5cc391477bdc56d1367
Robert Adam <dev@robert-adam.de>
2020-04-29 11:11:11 +0200

    src/mumble/ConfigDialog: Added possibility to get ConfigWidget by name

--- src/mumble/ConfigDialog.h
+++ src/mumble/ConfigDialog.h
@@ -11,11 +11,15 @@
 
 #include "ui_ConfigDialog.h"
 
+#include <QtCore/QMutex>
+
 class ConfigDialog : public QDialog, public Ui::ConfigDialog {
 	private:
 		Q_OBJECT
 		Q_DISABLE_COPY(ConfigDialog)
 	protected:
+		static QMutex s_existingWidgetsMutex;
+		static QHash<QString, ConfigWidget *> s_existingWidgets;
 		QHash<ConfigWidget *, QWidget *> qhPages;
 		QMap<unsigned int, ConfigWidget *> qmWidgets;
 		QMap<QListWidgetItem *, ConfigWidget *> qmIconWidgets;
@@ -26,6 +30,10 @@ class ConfigDialog : public QDialog, public Ui::ConfigDialog {
 	public:
 		ConfigDialog(QWidget *p = NULL);
 		~ConfigDialog() Q_DECL_OVERRIDE;
+
+		/// @returns The pointer to the existing ConfigWidget with the given name or nullptr,
+		/// 	if no such widget exists.
+		static ConfigWidget *getConfigWidget(const QString &name);
 	public slots:
 		void on_pageButtonBox_clicked(QAbstractButton *);
 		void on_dialogButtonBox_clicked(QAbstractButton *);
