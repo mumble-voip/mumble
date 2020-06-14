mumble-voip/mumble

commit b691108d3820695d51e2f4d76d1ee080e9bfaa91

REFAC(crypto): remove QSslSocket::addDefaultCaCertificates
The function in question has been deprecated in Qt 5.15. Instead it is
recommended to set the CA certificates on the QSslConfiguration object.
According to the docs of QSslConfiguration however the system's CA
certificates is what is used by default anyways.

Quote (from 5.15 docs of QSslConfiguration::systemCaCertificates):
"This function provides the CA certificate database provided by the
operating system. The CA certificate database returned by this function
is used to initialize the database returned by caCertificates() on the
default QSslConfiguration."

src/SSL.cpp
@@ -108,8 +108,6 @@ out:
 }
 
 void MumbleSSL::addSystemCA() {
-	QSslSocket::addDefaultCaCertificates(QSslConfiguration::systemCaCertificates());
-
 #ifdef Q_OS_WIN
 	// Work around issue #1271.
 	// Skype's click-to-call feature creates an enormous

commit ba9ae91614c5ac5218e6b9f0f2af31a20aadc629

FIX(certificate): Properly process signed certificates
This fixes a regression introduced in
b691108d3820695d51e2f4d76d1ee080e9bfaa91. In there it was thought that
there is no need to explicitly add the system CaCertificates to the SSL
configuration. This turned out to be wrong.

It was brought up that by removing this line, properly signed
certificates would also be shown as self-signed.

Fixes #4289

src/SSL.cpp
@@ -108,6 +108,17 @@ out:
 }
 
 void MumbleSSL::addSystemCA() {
+#if QT_VERSION >= QT_VERSION_CHECK(5,15,0)
+	// Qt 5.15 introduced adding certificates to the QSslConfiguration and deprecated doing so on QSslSocket
+	auto config = QSslConfiguration::defaultConfiguration();
+
+	config.addCaCertificates(QSslConfiguration::systemCaCertificates());
+
+	QSslConfiguration::setDefaultConfiguration(config);
+#else
+	QSslSocket::addDefaultCaCertificates(QSslConfiguration::systemCaCertificates());
+#endif
+
 #ifdef Q_OS_WIN
 	// Work around issue #1271.
 	// Skype's click-to-call feature creates an enormous