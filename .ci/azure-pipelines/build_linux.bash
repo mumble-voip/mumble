61e7e8468eefa312322a899e534a56d2384e119f
Robert Adam <dev@robert-adam.de>
2020-08-15 21:14:45 +0200

    CI(azure): Outsource AppImage build
    The packaging of the AppImage is now a separate step instead of being
    integrated into the build step.

--- .ci/azure-pipelines/build_linux.bash
+++ .ci/azure-pipelines/build_linux.bash
@@ -25,25 +25,3 @@ cmake --build .
 ctest --verbose
 cmake --install .
 
-# Get the AppImage-tool - always use the latest available version
-wget -c https://github.com/$(wget -q https://github.com/probonopd/go-appimage/releases -O - | grep "appimagetool-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)
-chmod +x appimagetool-*.AppImage
-
-# find the appdir that was created by cmake --install
-appDir=$(find $HOME -type d -name 'appdir'| head -n 1)
-
-# Bundle the AppDir
-./appimagetool-*.AppImage -s deploy $appDir/usr/share/applications/*.desktop # Bundle EVERYTHING
-
-# Actually create the AppImage
-VERSION="$VER" ARCH="x86_64" ./appimagetool-*.AppImage $appDir
-
-for f in Mumble*.AppImage; do
-	# Embed update information into AppImage
-	echo -n "zsync|https://dl.mumble.info/nightly/latest-x86_64.AppImage.zsync" | dd of=$f seek=175720 conv=notrunc oflag=seek_bytes
-
-	# Generate zsync file for AppImage
-	zsyncmake $f
-done
-
-mv Mumble*.AppImage* $BUILD_ARTIFACTSTAGINGDIRECTORY
