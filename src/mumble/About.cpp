mumble-voip/mumble

commit 3575a4c8a7d3a4e419e3e1fcaa77d56cadf6ab29

About: allow text in "About" tab to be selected

src/mumble/About.cpp
@@ -45,6 +45,7 @@ AboutDialog::AboutDialog(QWidget *p) : QDialog(p) {
 	icon->setPixmap(g.mw->qiIcon.pixmap(g.mw->qiIcon.actualSize(QSize(128, 128))));
 
 	QLabel *text = new QLabel(about);
+	text->setTextInteractionFlags(Qt::TextSelectableByMouse);
 	text->setOpenExternalLinks(true);
 	text->setText(tr(
 		"<h3>Mumble (%1)</h3>"

commit 82ecc8a9986e1186c95890f8508118741aaddf09

FIX(client): Make link in about dialog clickable again
Fix the regression introduced with commit 3575a4c
by using the proper Qt text interaction flag.

src/mumble/About.cpp
@@ -52,7 +52,7 @@ AboutDialog::AboutDialog(QWidget *p) : QDialog(p) {
 	icon->setPixmap(g.mw->qiIcon.pixmap(g.mw->qiIcon.actualSize(QSize(128, 128))));
 
 	QLabel *text = new QLabel(about);
-	text->setTextInteractionFlags(Qt::TextSelectableByMouse);
+	text->setTextInteractionFlags(Qt::TextBrowserInteraction);
 	text->setOpenExternalLinks(true);
 	text->setText(tr(
 		"<h3>Mumble (%1)</h3>"