ff93cce54c12d752f9cc1bc2675ac6d42b5de32e
Thomas Lange <thomas-lange2@gmx.de>
2020-09-04 07:19:47 +0200

    FIX(client): Make link in about dialog clickable again
    Fix the regression introduced with commit 3575a4c
    by using the proper Qt text interaction flag.

--- src/mumble/About.cpp
+++ src/mumble/About.cpp
@@ -47,7 +47,7 @@ AboutDialog::AboutDialog(QWidget *p) : QDialog(p) {
 	icon->setPixmap(g.mw->qiIcon.pixmap(g.mw->qiIcon.actualSize(QSize(128, 128))));
 
 	QLabel *text = new QLabel(about);
-	text->setTextInteractionFlags(Qt::TextSelectableByMouse);
+	text->setTextInteractionFlags(Qt::TextBrowserInteraction);
 	text->setOpenExternalLinks(true);
 	text->setText(tr(
 		"<h3>Mumble (%1)</h3>"
