mumble-voip/mumble

commit 01db6e219d098a2eccbb7b72ef3a24ad6c2dbbda

REFAC(client): replace NULL and Q_NULLPTR with nullptr
This changes all occurances of NULL and Q_NULLPTR in the mumble source
dir to nullptr. Additionally comparisons with NULL were changed to `if
(pointer)` if it would not be confusing. (e.g. booleans as parameters `
setEnabled(pointer != nullptr)`)

src/mumble/MumbleApplication.cpp
@@ -69,7 +69,7 @@ bool MumbleApplication::event(QEvent *e) {
 ///          by GlobalShortcutWin. Otherwise, returns false.
 static bool gswForward(MSG *msg) {
 	GlobalShortcutWin *gsw = static_cast<GlobalShortcutWin *>(GlobalShortcutEngine::engine);
-	if (gsw == NULL) {
+	if (gsw) {
 		return false;
 	}
 	switch (msg->message) {

commit c21f59ace747d720628d999c610e99523b794207

FIX(client): typo in gswForward() causing "WM_" messages not to be injected
This could also cause a crash, in case gswForward() was called before GlobalShortcutsWin's initialization.

For reference, the bug was introduced in 01db6e219d098a2eccbb7b72ef3a24ad6c2dbbda.

src/mumble/MumbleApplication.cpp
@@ -66,7 +66,7 @@ bool MumbleApplication::event(QEvent *e) {
 ///          by GlobalShortcutWin. Otherwise, returns false.
 static bool gswForward(MSG *msg) {
 	GlobalShortcutWin *gsw = static_cast< GlobalShortcutWin * >(GlobalShortcutEngine::engine);
-	if (gsw) {
+	if (!gsw) {
 		return false;
 	}
 	switch (msg->message) {