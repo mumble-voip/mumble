c03521593c4f20306b2c6af0b0e8c39eba10ee6f
Robert Adam <krzmbrzl@gmail.com>
2020-11-25 19:06:25 +0100

    CI: Introduce GitHub Actions
    Now that Travis CI decided to stop offering their services for free, we
    had to look for an alternative solution. GitHub Actions seems to fit the
    picture really well and has the benefit of being tightly integrated into
    GitHub (obviously).
    
    For now only shared builds for Ubuntu 18.04 and 20.04 are included. The
    scripts and the framework for also including static builds is part of
    this commit as well but as it stands the static build always failed and
    therefore it was given up upon for now.
    
    Adding support for other OS will require a few tweaks and a couple of
    new scripts but in general the framework was built with this kind of
    extension in mind.

---
+++ .github/actions/install-dependencies/install_ubuntu_static_64bit.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+sudo apt update
+
+sudo apt -y install \
+	build-essential \
+	g++-multilib \
+	ninja-build \
+	libcap-dev \
+	libdbus-1-dev \
+	libxi-dev \
+	libxkbcommon-dev \
+	libxkbcommon-x11-dev \
+	libxcb-icccm4-dev \
+	libxcb-image0-dev \
+	libxcb-keysyms1-dev \
+	libxcb-render-util0-dev \
+	libxcb-xinerama0-dev \
+	libxcb-xinput-dev
+	libxcb-xkb-dev \
+	libasound2-dev \
+	libpulse-dev \
+	libspeechd-dev \
+	libl1-mesa-dev \
+	libavahi-compat-libdnssd-dev \
+	libglu1-mesa-dev \
+	mesa-common-dev \
+	libxrandr-dev \
+	libxxf86vm-dev \
+	libgl-dev
+
+
+if [[ "$MUMBLE_ENVIRONMENT_SOURCE" == "" ]]; then
+	echo "MUMBLE_ENVIRONMENT_SOURCE not set!"
+	exit 1
+fi
+if [[ "$MUMBLE_ENVIRONMENT_VERSION" == "" ]]; then
+	echo "MUMBLE_ENVIRONMENT_VERSION not set!"
+	exit 1
+fi
+if [[ "$MUMBLE_ENVIRONMENT_DIR" == ""  ]]; then
+	echo "MUMBLE_ENVIRONMENT_DIR not set!"
+	exit 1
+fi
+if [[ "$MUMBLE_BUILD_ENV_PATH" == ""  ]]; then
+	echo "MUMBLE_BUILD_ENV_PATH not set!"
+	exit 1
+fi
+
+envDir="$MUMBLE_BUILD_ENV_PATH"
+
+echo "$MUMBLE_BUILD_ENV_PATH"
+echo "$envDir"
+echo "$MUMBLE_ENVIRONMENT_PA"
+
+ls -al "$envDir"
+
+if [[ -d "$envDir" && -n "$(ls -A '$envDir')" ]]; then
+	echo "Environment is cached"
+else
+	sudo apt install axel
+
+	envArchive="$MUMBLE_ENVIRONMENT_VERSION.tar.xz"
+
+	axel -n 5 --output="$envArchive" "$MUMBLE_ENVIRONMENT_SOURCE/$envArchive"
+
+	echo "Extracting archive..."
+	if [[ ! -d "$MUMBLE_ENVIRONMENT_DIR" ]]; then
+		mkdir -p "$envDir"
+	fi
+
+	"$(dirname $0)/extractWithProgress.sh" "$envArchive" "$MUMBLE_ENVIRONMENT_DIR"
+
+	if [[ ! -d "$envDir" || -n "$(ls -A '$envDir')" ]]; then
+		echo "Environment did not follow expected form"
+		ls -al "$MUMBLE_ENVIRONMENT_PATH"
+		exit 1
+	fi
+
+	ls -al "$envDir"
+fi
+
+
