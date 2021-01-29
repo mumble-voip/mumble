a7f98517b5b35b94204b787672a865a160789297
Davide Beatrici <git@davidebeatrici.dev>
2021-01-29 20:50:59 +0100

    FEAT(client): Introduce GlobalShortcutButtons, for better shortcut management
    This is a dialog that allows to efficiently view/add/remove buttons for a specific shortcut.
    
    The tree design allows to easily identify the buttons and the device they're attached to.
    
    The capture process can be started by pressing on "Add".
    Once all buttons are released the process stops automatically.
    
    There's only a cosmetic issue, that should be fixed in our theme: no distinction between active and focused QPushButton.

---
+++ src/mumble/GlobalShortcutButtons.h
@@ -0,0 +1,42 @@
+// Copyright 2021 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#ifndef MUMBLE_MUMBLE_GLOBALSHORTCUTKEYS_H
+#define MUMBLE_MUMBLE_GLOBALSHORTCUTKEYS_H
+
+#include <memory>
+
+#include <QtWidgets/QDialog>
+
+namespace Ui {
+class GlobalShortcutButtons;
+}
+
+class GlobalShortcutButtons : public QDialog {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(GlobalShortcutButtons)
+	Q_PROPERTY(QList< QVariant > buttons READ buttons WRITE setButtons USER true)
+
+protected:
+	QList< QVariant > m_previousButtons;
+	std::unique_ptr< Ui::GlobalShortcutButtons > m_ui;
+
+	void addItem(const QVariant &button);
+	void removeItem();
+	void toggleCapture(const bool enabled);
+	void updateButtons(const bool last);
+
+	virtual bool eventFilter(QObject *, QEvent *event);
+
+public:
+	QList< QVariant > buttons();
+	void setButtons(const QList< QVariant > &buttons);
+
+	GlobalShortcutButtons(QWidget *p = nullptr);
+	~GlobalShortcutButtons();
+};
+
+#endif
