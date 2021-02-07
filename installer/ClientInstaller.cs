9cc486f24f820fd64999122c1781704e465f7e99
Davide Beatrici <git@davidebeatrici.dev>
2021-02-08 00:01:03 +0100

    FIX(installer): Change version regex so that it accepts multiple digits
    Now that we reached build number 10 the current expression is not valid anymore.
    
    As a result, the version is not set and the build fails.
    
    This commit rewrites the regex so that it accepts multiple digits and is also shorter.
    
    Co-Authored-By: Jan Klass <kissaki@posteo.de>

--- installer/ClientInstaller.cs
+++ installer/ClientInstaller.cs
@@ -172,7 +172,7 @@ class BuildInstaller
 		bool isAllLangs = false;
 
 		for (int i = 0; i < args.Length; i++) {
-			if (args[i] == "--version" && Regex.IsMatch(args[i + 1], @"^[0-9]\.[0-9]\.[0-9]\.[0-9]$")) {
+			if (args[i] == "--version" && Regex.IsMatch(args[i + 1], @"^([0-9]+\.){3}[0-9]+$")) {
 				version = args[i + 1];
 			}
 
