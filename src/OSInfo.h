4fc7df5ddeb2a52cafdd67cbc0b0d3182f14cfdb
Davide Beatrici <git@davidebeatrici.dev>
2021-02-12 08:27:15 +0100

    FEAT: Improve OSInfo so that architecture and OS are properly reported
    This originally started as a Linux-only improvement (support for "/etc/os-release"), but it became pretty clear that OSInfo was not in a pretty state.
    
    Please note that the auto-update system is not affected, because it doesn't rely on OSInfo.
    
    The most important change involves the OS name.
    Before this commit we had "Win32", "WinX64", "OSX" and "X11". The latter was particularly misleading now that Wayland is a thing.
    We now have "Windows", "Linux", "macOS", "FreeBSD", "NetBSD", "OpenBSD", "BSD", "UNIX" and "Unknown".
    
    The second most important change is the way we retrieve the version on OSes other than Windows and macOS.
    OSInfo had several methods, but none was aware of "/etc/os-release". We initially wanted to implement a new method, to be used as primary, that gathers info from that file.
    However, we later realized we can just use QSysInfo because it does exactly what we planned to do on our own.
    The reason why we're retaining our own methods for Windows and macOS is because they're MUCH more descriptive. For example, the build number is not in the strings returned by QSysInfo.
    In order to make the code easier to read, the parts that handled versions of Windows we no longer support is removed.
    
    The final change is about the info that is sent to our server.
    "is64bit" was set to the correct value on all platforms except for Windows, because IsWow64Process() returns true only when the process is 32 bit and running on a 64 bit OS.
    "arch" and "osarch" are introduced and represent the architecture of the program and the OS, respectively. "is64bit" is removed as it's now redundant.
    "osverbose" is also introduced and represents the full operating system version.

--- src/OSInfo.h
+++ src/OSInfo.h
@@ -1,4 +1,4 @@
-// Copyright 2005-2020 The Mumble Developers. All rights reserved.
+// Copyright 2005-2021 The Mumble Developers. All rights reserved.
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
@@ -15,10 +15,30 @@ class QHostAddress;
 
 class OSInfo {
 public:
+	/// If \p build is false, returns the operating system architecture.
+	/// If \p build is true, returns the build architecture.
+	static QString getArchitecture(const bool build);
+
+	/// Returns an hash generated from the primary network interface's MAC address.
 	static QString getMacHash(const QList< QHostAddress > & = QList< QHostAddress >());
+
+	/// Returns the operating system's basename.
+	/// Examples: "Windows", "Linux", "macOS", "FreeBSD", "Unknown", etc.
 	static QString getOS();
+
+	/// Returns the complete operating system version.
+	///
+	/// Examples:
+	/// "Windows 10 Enterprise 2009 19042.804 [x64]"
+	/// "Debian GNU/Linux bullseye/sid [x64]"
+	///
+	/// The architecture is not explicitly added when \p appendArch is false.
+	static QString getOSDisplayableVersion(const bool appendArch = true);
+
+	/// Returns the operating system's version.
+	/// Examples: "10.0.19042.1", "10.16.0 20C69"
 	static QString getOSVersion();
-	static QString getOSDisplayableVersion();
+
 	static void fillXml(QDomDocument &doc, QDomElement &root, const QString &os = OSInfo::getOS(),
 						const QString &osver          = OSInfo::getOSVersion(),
 						const QList< QHostAddress > & = QList< QHostAddress >());
