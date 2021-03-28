ccd9240624ac52456876d3ae11b24ff47dbb144e
Robert Adam <dev@robert-adam.de>
2021-03-28 10:25:37 +0200

    TEST: Enforce Opus usage on client

--- src/mumble/Messages.cpp
+++ src/mumble/Messages.cpp
@@ -1204,6 +1204,14 @@ void MainWindow::msgPermissionQuery(const MumbleProto::PermissionQuery &msg) {
 ///
 /// @param msg The message object
 void MainWindow::msgCodecVersion(const MumbleProto::CodecVersion &msg) {
+	if (!msg.opus()) {
+		qWarning() << "Client was instructed to not use Opus!";
+	}
+
+	// Always use Opus
+	Global::get().bOpus = true;
+
+	return;
 	int alpha = msg.has_alpha() ? msg.alpha() : -1;
 	int beta  = msg.has_beta() ? msg.beta() : -1;
 	bool pref = msg.prefer_alpha();
