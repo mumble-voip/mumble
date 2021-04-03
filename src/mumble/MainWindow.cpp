0a95dea3e14cd2981dde152fb2337dd2716b21a1
Robert Adam <dev@robert-adam.de>
2021-04-03 09:12:29 +0200

    FIX(client, markdown): Wrong line break HTML code
    The Markdown code was using </br> instead of the correct <br/>.
    
    This error was introduced in 2da3f0d39e37e6bf10318d174744bb7e7924794b.
    
    Fixes #4899

--- src/mumble/MainWindow.cpp
+++ src/mumble/MainWindow.cpp
@@ -1952,6 +1952,10 @@ void MainWindow::on_qaQuit_triggered() {
 
 void MainWindow::sendChatbarText(QString qsText) {
 	qsText = qsText.toHtmlEscaped();
+	// Markdown::markdownToHTML also takes care of replacing line breaks (\n) with the respective
+	// HTML code <br/>. Therefore if Markdown support is ever going to be removed from this
+	// function, this job has to be done explicitly as otherwise line breaks won't be shown on
+	// the receiving end of this text message.
 	qsText = Markdown::markdownToHTML(qsText);
 	sendChatbarMessage(qsText);
 
