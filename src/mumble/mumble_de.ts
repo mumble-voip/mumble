73b73f954d971af033378efa15f18d03644efa1f
Hosted Weblate <hosted@weblate.org>
2021-08-18 20:34:54 +0200

    TRANSLATION: Update German translation
    Currently translated at 100.0% (2077 of 2077 strings)
    
    TRANSLATION: Update German translation
    
    Currently translated at 100.0% (2077 of 2077 strings)
    
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Robert Adam <dev@robert-adam.de>
    Co-authored-by: Vri 🌈 <weblate@vrifox.cc>
    Translate-URL: https://hosted.weblate.org/projects/mumble/mumble-client/de/
    Translation: Mumble/Mumble Client

--- src/mumble/mumble_de.ts
+++ src/mumble/mumble_de.ts
@@ -4347,7 +4347,7 @@ Die Einstellung gilt nur für neue Nachrichten, die bereits angezeigten behalten
     </message>
     <message>
         <source>Lock layout</source>
-        <translation>Sperren Layout</translation>
+        <translation>Layout sperren</translation>
     </message>
     <message>
         <source>Talking UI</source>
@@ -7768,12 +7768,13 @@ Infos hierzu finden Sie im &lt;a href=&quot;https://wiki.mumble.info/wiki/Instal
     </message>
     <message>
         <source>Shortcuts migration incomplete</source>
-        <translation type="unfinished"></translation>
+        <translation>Migration der Tastenkürzel unvollständig</translation>
     </message>
     <message>
         <source>Unfortunately %1 shortcut(s) could not be migrated.
 You can register them again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Leider konnte(n) %1 Tastenkürzel nicht migriert werden.
+Sie können sie jedoch erneut registrieren.</translation>
     </message>
 </context>
 <context>
