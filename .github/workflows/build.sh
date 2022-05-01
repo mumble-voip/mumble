5a3d370ea4bb8967d622eba3731f8d6420f5754b
Robert Adam <dev@robert-adam.de>
2022-05-01 14:30:41 +0200

    CI: Also fetch build number token on GA
    GitHub Actions previously did not use the build number token. This is
    changed by this commit. In order to to so, the build process has been
    outsourced into its own script.
    
    (cherry picked from commit 26d64edcc5a1820c0e1ea6414ef18125d66ea538)
    
    # Conflicts:
    #	.github/workflows/build.yml

---
+++ .github/workflows/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+buildDir="${GITHUB_WORKSPACE}/build"
+
+mkdir "$buildDir"
+
+cd "$buildDir"
+
+VERSION=$("${GITHUB_WORKSPACE}/scripts/mumble-version.py")
+BUILD_NUMBER=$("${GITHUB_WORKSPACE}/scripts/mumble-build-number.py" --commit "${GITHUB_SHA}" --version "${VERSION}" \
+	--password "${MUMBLE_BUILD_NUMBER_TOKEN}" --default 0)
+
+# Run cmake with all necessary options
+cmake -G Ninja \
+	  -S "$GITHUB_WORKSPACE" \
+	  -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+	  -DBUILD_NUMBER=$BUILD_NUMBER \
+	  $CMAKE_OPTIONS \
+      -DCMAKE_UNITY_BUILD=ON \
+	  $ADDITIONAL_CMAKE_OPTIONS \
+	  $VCPKG_CMAKE_OPTIONS
+
+# Actually build
+cmake --build . --config $BUILD_TYPE
+
