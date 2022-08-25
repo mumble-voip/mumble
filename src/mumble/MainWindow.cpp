mumble-voip/mumble

commit 6aa88691e16c807b998301f2cbecfda599ef4592

FIX(client, ui): Minimal view not always working
When activating minimal view, sometimes it would work as expected,
hiding all UI elements but the channel tree, but other times
various other elements would remain visible until opening and applying
the settings.

The reason for this was that inside the updateView function, we called
restoreGeometry after we have set the visibility of the respective UI
elements, which could undo our previous visibility changes, leaving
these elements visible.

By applying the visibility changes after the geometry restoration, we
avoid this problem.

src/mumble/MainWindow.cpp
@@ -1307,13 +1307,6 @@ void MainWindow::setupView(bool toggle_minimize) {
 		menuBar()->removeAction(qmDeveloper->menuAction());
 	}
 
-	if (!showit) {
-		qdwLog->setVisible(showit);
-		qdwChat->setVisible(showit);
-		qtIconToolbar->setVisible(showit);
-	}
-	menuBar()->setVisible(showit);
-
 	if (toggle_minimize) {
 		if (!showit) {
 			if (!Global::get().s.qbaMinimalViewGeometry.isNull())
@@ -1332,6 +1325,13 @@ void MainWindow::setupView(bool toggle_minimize) {
 			   geometry().height() - newgeom.height() + geom.height());
 		move(geom.x(), geom.y());
 	}
+
+
+	// Hide/Show respective UI elements
+	qdwLog->setVisible(showit);
+	qdwChat->setVisible(showit);
+	qtIconToolbar->setVisible(showit);
+	menuBar()->setVisible(showit);
 
 	// Display the Transmit Mode Dropdown, if configured to do so, otherwise
 	// hide it.

commit 203e675ce0064bacf173e0b61bdf1f652d8521dc

FIX(client, ui): Make hiding UI elements persistent again
Due to a regression introduced in
6aa88691e16c807b998301f2cbecfda599ef4592 when hiding individual UI
components (e.g. the icon toolbar) - possible when using a custom
layout - these elements would re-appear after restarting Mumble (or
after applying the settings or after exiting minimal view).

The reason is that the state of whether or not a given UI component is
shown or not is saved as the window's geometry. In the above commit we
added code that explicitly shows or hides these elements after the
geometry has been restored, thus overwriting the visibility changes of
this geometry restoration.

The fix for this problem is to only explicitly hide the elements when
entering minimal view but let the showing be handled by restoring the
window's geometry. Thus, only those elements are shown again, that have
previously been visible as well (and thus have not been explicitly
hidden by the user).

Fixes #5832

src/mumble/MainWindow.cpp
@@ -1334,10 +1334,13 @@ void MainWindow::setupView(bool toggle_minimize) {
 	}
 
 
-	// Hide/Show respective UI elements
-	qdwLog->setVisible(showit);
-	qdwChat->setVisible(showit);
-	qtIconToolbar->setVisible(showit);
+	// Explicitly hide UI elements, if we're entering minimal view
+	// Note that showing them again is handled above via restoreState/restoreGeometry calls
+	if (!showit) {
+		qdwLog->setVisible(false);
+		qdwChat->setVisible(false);
+		qtIconToolbar->setVisible(false);
+	}
 	menuBar()->setVisible(showit);
 
 	if (showit) {