40414b56afe23735cca5e7919232cd92e737e231
Robert Adam <dev@robert-adam.de>
2022-11-15 18:55:32 +0100

    Add explicit move-support for NonCopyable

--- src/NonCopyable.h
+++ src/NonCopyable.h
@@ -15,6 +15,9 @@ struct NonCopyable {
 
 	NonCopyable(const NonCopyable &) = delete;
 	NonCopyable &operator=(const NonCopyable &) = delete;
+
+	NonCopyable(NonCopyable &&) = default;
+	NonCopyable &operator=(NonCopyable &&) = default;
 };
 
 #endif // MUMBLE_NONCOPYABLE_H_
