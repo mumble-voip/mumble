19e3af3612d04d3ea284660a7a5515bad9a3c244
Hartmnt <hartmunt@protonmail.com>
2022-11-29 17:50:34 +0000

    FIX(a11y): Introduce accessibility source files

---
+++ src/mumble/Accessibility.h
@@ -0,0 +1,13 @@
+// Copyright 2024 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#ifndef MUMBLE_MUMBLE_ACCESSIBILITY_H_
+#define MUMBLE_MUMBLE_ACCESSIBILITY_H_
+
+namespace Mumble {
+namespace Accessibility {} // namespace Accessibility
+}                          // namespace Mumble
+
+#endif // MUMBLE_MUMBLE_ACCESSIBILITY_H_
