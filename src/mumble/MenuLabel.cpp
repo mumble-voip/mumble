0e0b8fae82f8d0ba270ab2713c4ebc0302eae3a0
Hartmnt <hartmunt@protonmail.com>
2022-12-11 18:46:59 +0000

    FIX(a11y): Improve inline volume slider accessibility
    Previously, the accessibility of the inline slider was very
    limited due to the cumbersome and lacking system Qt provides.
    
    This commit gently tricks Qt into behaving like a nice
    framework in regards to accessibility.
    
    Fixes #6109
    
    (cherry picked from commit ed988e646af57eeeb2e47a0d077549999c23957f)

--- src/mumble/MenuLabel.cpp
+++ src/mumble/MenuLabel.cpp
@@ -4,13 +4,35 @@
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
 #include "MenuLabel.h"
+#include "widgets/EventFilters.h"
 
+#include <QGridLayout>
 #include <QLabel>
 
 MenuLabel::MenuLabel(const QString &text, QObject *parent) : QWidgetAction(parent), m_text(text) {
+	setMenuRole(QAction::NoRole);
 }
 
 QWidget *MenuLabel::createWidget(QWidget *parent) {
+	QWidget *widget = new QWidget(parent);
+
+	// Not setting any focus is not an alternative here, as the
+	// QWidgetAction WILL get and automatically forward focus to the child.
+	// The widget needs tab focus policy, so it can process the event filters
+	// and also forward the focus.
+	widget->setFocusPolicy(Qt::TabFocus);
+	widget->installEventFilter(new SkipFocusEventFilter(widget));
+
+	// Using a widget and layout here instead of a plain label
+	// because otherwise screen readers might partially read the
+	// label text.
+	QGridLayout *layout = new QGridLayout();
+	layout->setContentsMargins(0, 0, 0, 0);
+	layout->setSpacing(0);
+
 	QLabel *label = new QLabel(m_text, parent);
-	return label;
+	layout->addWidget(label, 0, 0);
+	widget->setLayout(layout);
+
+	return widget;
 }
