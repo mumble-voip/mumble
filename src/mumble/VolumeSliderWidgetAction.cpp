8d65b12c2f1f90364bd57461907148aac300ae4e
Robert Adam <dev@robert-adam.de>
2023-01-08 15:54:35 +0100

    MAINT: Update copyright headers to 2023
    (cherry picked from commit 249d2c6298cbce059a619dd32b1fde6131ab022f)
    
    # Conflicts:
    #	src/mumble/AudioOutputToken.h

--- src/mumble/VolumeSliderWidgetAction.cpp
+++ src/mumble/VolumeSliderWidgetAction.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022 The Mumble Developers. All rights reserved.
+// Copyright 2022-2023 The Mumble Developers. All rights reserved.
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
