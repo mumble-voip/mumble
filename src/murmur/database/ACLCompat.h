ee91e75e8d5618a349ab1a1f5488a8f6a78c73bc
Robert Adam <dev@robert-adam.de>
2023-02-21 20:19:18 +0100

    Outsourced legacy group format converter functions

---
+++ src/murmur/database/ACLCompat.h
@@ -0,0 +1,40 @@
+// Copyright 2023 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#ifndef MUMBLE_SERVER_DATABASE_ACLCOMPAT_H_
+#define MUMBLE_SERVER_DATABASE_ACLCOMPAT_H_
+
+#include "DBAcl.h"
+
+#include <string>
+#include <vector>
+
+#include <boost/optional.hpp>
+
+namespace mumble {
+namespace server {
+	namespace db {
+
+		class GroupTable;
+
+		struct LegacyGroupData {
+			std::string name;
+			bool isAccessToken = false;
+			std::vector< std::string > modifiers;
+		};
+
+		LegacyGroupData parseLegacyGroupSpecification(std::string groupSpec);
+
+		boost::optional< DBAcl::MetaGroup > parseMetaGroup(const std::string &str);
+
+		std::string metaGroupName(DBAcl::MetaGroup group);
+
+		std::string getLegacyGroupData(const DBAcl &acl, GroupTable &groupTable);
+
+	} // namespace db
+} // namespace server
+} // namespace mumble
+
+#endif // MUMBLE_SERVER_DATABASE_ACLCOMPAT_H_
