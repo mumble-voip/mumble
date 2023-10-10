01181defe623687cece8f8a52826c89ae302be49
Hartmnt <hartmunt@protonmail.com>
2023-10-10 17:49:05 +0000

    FIX(client,positional-audio): Fix ManualPlacement plugin orientation indicator
    Previously, the ManualPlacement plugin for the users position/orientation
    was pointing in the wrong (opposite) direction.
    
    This fix swaps the orientation of the sprite.
    It also set the default azimuth to 0 instead of 180 to make the
    orientation of the listener intuitive for the user by default.
    The new default azimuth will not change the logic of the plugin
    whatsoever.

--- src/mumble/ManualPlugin.cpp
+++ src/mumble/ManualPlugin.cpp
@@ -23,7 +23,7 @@ static QPointer< Manual > mDlg = nullptr;
 static bool bLinkable          = false;
 static bool bActive            = true;
 
-static int iAzimuth   = 180;
+static int iAzimuth   = 0;
 static int iElevation = 0;
 
 static const QString defaultContext  = QString::fromLatin1("Mumble");
@@ -52,8 +52,8 @@ Manual::Manual(QWidget *p) : QDialog(p) {
 	// The center of the indicator's circle will represent the current position
 	indicator.addEllipse(QRectF(-indicatorDiameter / 2, -indicatorDiameter / 2, indicatorDiameter, indicatorDiameter));
 	// A line will indicate the indicator's orientation (azimuth)
-	indicator.moveTo(0, indicatorDiameter / 2);
-	indicator.lineTo(0, indicatorDiameter);
+	indicator.moveTo(0, -indicatorDiameter / 2);
+	indicator.lineTo(0, -indicatorDiameter);
 
 	m_qgiPosition = m_qgsScene->addPath(indicator);
 
