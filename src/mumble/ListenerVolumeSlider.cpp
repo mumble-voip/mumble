97a99c44e1db06cfe062ad99da876f6b47bcf85c
Hartmnt <hartmunt@protonmail.com>
2023-10-11 16:13:04 +0000

    FIX(client): Fix using the keyboad to change local volume adjustment
    Previously, when using keyboard arrows or the mouse wheel,
    the new slider widget action applied the new local volume
    but did not save it. That was because it used the sliderReleased
    event to save the value, which is not triggered by keyboard or mouse
    wheel updates.
    
    We could simply save the local volume on every sliderChange instead of
    on sliderRelease, but that would cause many writes to the disk
    in a short period of time and possibly lag the client.
    Additionally, the ListenerVolumeSlider would spam packets to the
    server, as the listener volume is saved server-side.
    
    This commit introduces event filters in addition to the sliderChanged event,
    which are observing key releases and mouse wheel events.
    It also introduces a delay timer for packets send to the server from the
    ListenerVolumeSlider.
    
    Fixes #6211
    
    (cherry picked from commit 6213ed4bad24add7ddb5e92e76e2574ad4f45550)

--- src/mumble/ListenerVolumeSlider.cpp
+++ src/mumble/ListenerVolumeSlider.cpp
@@ -7,10 +7,14 @@
 #include "Channel.h"
 #include "ChannelListenerManager.h"
 #include "ServerHandler.h"
-#include "VolumeAdjustment.h"
 #include "Global.h"
 
-ListenerVolumeSlider::ListenerVolumeSlider(QWidget *parent) : VolumeSliderWidgetAction(parent) {
+ListenerVolumeSlider::ListenerVolumeSlider(QWidget *parent) : VolumeSliderWidgetAction(parent), m_currentSendDelay(0) {
+	connect(&m_sendTimer, &QTimer::timeout, this, &ListenerVolumeSlider::sendToServer);
+	connect(&m_resetTimer, &QTimer::timeout, this, [=]() { m_currentSendDelay = 0; });
+
+	m_sendTimer.setSingleShot(true);
+	m_resetTimer.setSingleShot(true);
 }
 
 void ListenerVolumeSlider::setListenedChannel(const Channel &channel) {
@@ -28,7 +32,7 @@ void ListenerVolumeSlider::on_VolumeSlider_valueChanged(int value) {
 	displayTooltip(value);
 }
 
-void ListenerVolumeSlider::on_VolumeSlider_sliderReleased() {
+void ListenerVolumeSlider::on_VolumeSlider_changeCompleted() {
 	ServerHandlerPtr handler = Global::get().sh;
 
 	if (!handler || !m_channel || !m_volumeSlider) {
@@ -39,18 +43,42 @@ void ListenerVolumeSlider::on_VolumeSlider_sliderReleased() {
 
 	if (handler->m_version >= Mumble::Protocol::PROTOBUF_INTRODUCTION_VERSION) {
 		// With the new audio protocol, volume adjustments for listeners are handled on the server and thus we want
-		// to avoid spamming updates to the adjustments, which is why we only update them once the slider is released.
-		MumbleProto::UserState mpus;
-		mpus.set_session(Global::get().uiSession);
+		// to avoid spamming updates to the adjustments, which is why we only update them with a delay.
 
-		MumbleProto::UserState::VolumeAdjustment *adjustmentMsg = mpus.add_listening_volume_adjustment();
-		adjustmentMsg->set_listening_channel(m_channel->iId);
-		adjustmentMsg->set_volume_adjustment(adjustment.factor);
+		if (m_cachedChannelID == m_channel->iId && m_cachedAdjustment == adjustment) {
+			return;
+		}
 
-		handler->sendMessage(mpus);
+		m_cachedChannelID  = m_channel->iId;
+		m_cachedAdjustment = adjustment;
+
+		// Timer values: 0, 50, 150, 350, 750, 1000 (ms)
+		m_resetTimer.stop();
+		m_sendTimer.start(m_currentSendDelay);
+		m_currentSendDelay = std::min(1000u, (m_currentSendDelay + 25) * 2);
+
 	} else {
 		// Before the new audio protocol, volume adjustments for listeners are handled locally
 		Global::get().channelListenerManager->setListenerVolumeAdjustment(Global::get().uiSession, m_channel->iId,
 																		  adjustment);
 	}
+}
+
+void ListenerVolumeSlider::sendToServer() {
+	ServerHandlerPtr handler = Global::get().sh;
+
+	m_resetTimer.start(3000);
+
+	if (!handler) {
+		return;
+	}
+
+	MumbleProto::UserState mpus;
+	mpus.set_session(Global::get().uiSession);
+
+	MumbleProto::UserState::VolumeAdjustment *adjustmentMsg = mpus.add_listening_volume_adjustment();
+	adjustmentMsg->set_listening_channel(m_cachedChannelID);
+	adjustmentMsg->set_volume_adjustment(m_cachedAdjustment.factor);
+
+	handler->sendMessage(mpus);
 }
---
+++ src/mumble/widgets/EventFilters.cpp
@@ -0,0 +1,68 @@
+// Copyright 2023 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#include "EventFilters.h"
+
+#include <algorithm>
+
+#include <QKeyEvent>
+#include <QWheelEvent>
+#include <QWidget>
+
+KeyEventObserver::KeyEventObserver(QObject *parent, QEvent::Type eventType, bool consume, std::vector< Qt::Key > keys)
+	: QObject(parent), m_eventType(eventType), m_consume(consume), m_keys(std::move(keys)) {
+}
+
+bool KeyEventObserver::eventFilter(QObject *obj, QEvent *event) {
+	QWidget *widget = static_cast< QWidget * >(obj);
+
+	if (!widget || !widget->hasFocus()) {
+		return false;
+	}
+
+	QKeyEvent *keyEvent = static_cast< QKeyEvent * >(event);
+
+	if (!keyEvent || keyEvent->type() != m_eventType) {
+		return false;
+	}
+
+	Qt::Key key = static_cast< Qt::Key >(keyEvent->key());
+
+	if (std::find(m_keys.begin(), m_keys.end(), key) == m_keys.end()) {
+		return false;
+	}
+
+	emit keyEventObserved();
+
+	return m_consume;
+}
+
+MouseWheelEventObserver::MouseWheelEventObserver(QObject *parent, std::vector< Qt::ScrollPhase > phases, bool consume)
+	: QObject(parent), m_phases(std::move(phases)), m_consume(consume) {
+}
+
+bool MouseWheelEventObserver::eventFilter(QObject *obj, QEvent *event) {
+	QWidget *widget = static_cast< QWidget * >(obj);
+
+	if (!widget || !widget->isVisible()) {
+		return false;
+	}
+
+	QWheelEvent *wheelEvent = static_cast< QWheelEvent * >(event);
+
+	if (!wheelEvent) {
+		return false;
+	}
+
+	Qt::ScrollPhase phase = wheelEvent->phase();
+
+	if (std::find(m_phases.begin(), m_phases.end(), phase) == m_phases.end()) {
+		return false;
+	}
+
+	emit wheelEventObserved(wheelEvent->pixelDelta());
+
+	return m_consume;
+}
