67e48131d62c77f13cdce8e4c3b9b5309257767d
Hartmnt <hartmunt@protonmail.com>
2024-01-07 13:49:44 +0000

    FIX(a11y): Make plugin config accessible
    This commit switches the plugin config tree to a new
    MultiColumnTreeWidget and makes it keyboard navigatable.
    It also makes sceen readers read actual column values.

---
+++ src/mumble/widgets/MultiColumnTreeWidget.cpp
@@ -0,0 +1,30 @@
+// Copyright 2024 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#include "MultiColumnTreeWidget.h"
+
+#include <algorithm>
+
+MultiColumnTreeWidget::MultiColumnTreeWidget(QWidget *parent) : QTreeWidget(parent) {
+}
+
+QModelIndex MultiColumnTreeWidget::moveCursor(QAbstractItemView::CursorAction cursorAction,
+											  Qt::KeyboardModifiers modifiers) {
+	QModelIndex mi = QTreeWidget::moveCursor(cursorAction, modifiers);
+
+	if (cursorAction == QAbstractItemView::MoveLeft) {
+		mi = model()->index(mi.row(), std::max(0, mi.column() - 1));
+	}
+
+	if (cursorAction == QAbstractItemView::MoveRight) {
+		mi = model()->index(mi.row(), std::min(model()->columnCount() - 1, mi.column() + 1));
+	}
+
+	if (cursorAction == QAbstractItemView::MoveUp || cursorAction == QAbstractItemView::MoveDown) {
+		mi = model()->index(mi.row(), 0);
+	}
+
+	return mi;
+}
---
+++ src/mumble/widgets/MultiColumnTreeWidget.h
@@ -0,0 +1,22 @@
+// Copyright 2024 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#ifndef MUMBLE_MUMBLE_WIDGETS_MULTICOLUMNTREEWIDGET_H_
+#define MUMBLE_MUMBLE_WIDGETS_MULTICOLUMNTREEWIDGET_H_
+
+#include <QAbstractItemView>
+#include <QModelIndex>
+#include <QTreeWidget>
+
+class MultiColumnTreeWidget : public QTreeWidget {
+	Q_OBJECT
+
+public:
+	MultiColumnTreeWidget(QWidget *parent = nullptr);
+
+	QModelIndex moveCursor(QAbstractItemView::CursorAction cursorAction, Qt::KeyboardModifiers modifiers) override;
+};
+
+#endif
