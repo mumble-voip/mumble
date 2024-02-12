8cae373d9c3609ac85f76e39838082bb0942c84b
Abdullah Ömer Yamaç <aomeryamac@gmail.com>
2024-02-12 19:55:28 +0000

    FEAT(client): Introduced new mumble API to link and unlink
    This patch introduces a new API to link and unlink the channels
    
    Co-Authored-By: Abdullah Ömer Yamaç <aomeryamac@gmail.com>

--- src/mumble/ServerHandler.h
+++ src/mumble/ServerHandler.h
@@ -173,7 +173,10 @@ public:
 	void setTokens(const QStringList &tokens);
 	void removeChannel(unsigned int channel);
 	void addChannelLink(unsigned int channel, unsigned int link);
+	void addChannelLinks(const unsigned int channelCount, const unsigned int *channelSet);
 	void removeChannelLink(unsigned int channel, unsigned int link);
+	void removeChannelLinks(const unsigned int channel, const unsigned int channelCount,
+							const unsigned int *channelSet);
 	void requestChannelPermissions(unsigned int channel);
 	void setSelfMuteDeafState(bool mute, bool deaf);
 	void announceRecordingState(bool recording);
