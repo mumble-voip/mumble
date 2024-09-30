330c356e716646570d2b865692c82fd2069adbd4
Robert Adam <dev@robert-adam.de>
2024-09-30 18:06:20 +0200

    MAINT: Remove copyright year from all copyright notices
    Keeping these up-to-date is just super tedious and they don't really
    fulfill any purpose these days.

--- scripts/generate-ApplicationPalette-class.py
+++ scripts/generate-ApplicationPalette-class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright 2014-2023 The Mumble Developers. All rights reserved.
+# Copyright The Mumble Developers. All rights reserved.
 # Use of this source code is governed by a BSD-style license
 # that can be found in the LICENSE file at the root of the
 # Mumble source tree or at <https://www.mumble.info/LICENSE>.
