mumble-voip/mumble

commit 2b19d746f959b53541f3f904cdd7d42b1f06a6e8

REFAC: Logger impl to work in OBJECT lib setup

src/Logger.cpp
@@ -1,0 +1,77 @@
+// Copyright The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#include "Logger.h"
+
+#include <QString>
+#include <QtLogging>
+
+#include <cstdlib>
+
+#include <spdlog/sinks/dup_filter_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+using MasterSink = spdlog::sinks::dup_filter_sink_st;
+using StdOutSink = spdlog::sinks::stdout_color_sink_st;
+
+#ifdef Q_OS_WIN
+#	include <spdlog/sinks/msvc_sink.h>
+
+using DebuggerSink = spdlog::sinks::msvc_sink_st;
+#endif
+
+using namespace mumble;
+
+static std::shared_ptr< MasterSink > masterSink;
+
+static void qtMessageHandler(const QtMsgType type, const QMessageLogContext &, const QString &msg) {
+	switch (type) {
+		default:
+			log::trace("{}", qPrintable(msg));
+			break;
+		case QtDebugMsg:
+			log::debug("{}", qPrintable(msg));
+			break;
+		case QtInfoMsg:
+			log::info("{}", qPrintable(msg));
+			break;
+		case QtWarningMsg:
+			log::warn("{}", qPrintable(msg));
+			break;
+		case QtCriticalMsg:
+			log::error("{}", qPrintable(msg));
+			break;
+		case QtFatalMsg:
+			log::fatal("{}", qPrintable(msg));
+			break;
+	}
+}
+
+void log::addSink(std::shared_ptr< spdlog::sinks::sink > sink) {
+	if (!masterSink) {
+		log::fatal("Attempted to addSink before master sink has been initialized");
+		return;
+	}
+
+	masterSink->add_sink(std::move(sink));
+}
+
+void log::init(spdlog::level::level_enum logLevel) {
+	// Skips the message if the previous one is identical and less than 5 seconds have passed.
+	masterSink = std::make_shared< MasterSink >(std::chrono::seconds(5));
+#ifdef Q_OS_WIN
+	masterSink->add_sink(std::make_shared< DebuggerSink >());
+#endif
+	masterSink->add_sink(std::make_shared< StdOutSink >());
+
+	auto logger = std::make_shared< spdlog::logger >(MainLoggerName, masterSink);
+
+	logger->set_level(logLevel);
+	logger->set_pattern("%^<%L>%$%Y-%m-%d %H:%M:%S.%e %v");
+
+	set_default_logger(std::move(logger));
+
+	qInstallMessageHandler(qtMessageHandler);
+}

src/mumble/Logger.cpp
@@ -1,117 +1,0 @@
-// Copyright The Mumble Developers. All rights reserved.
-// Use of this source code is governed by a BSD-style license
-// that can be found in the LICENSE file at the root of the
-// Mumble source tree or at <https://www.mumble.info/LICENSE>.
-
-#include "Logger.h"
-
-#include "Global.h"
-
-#include <cstdlib>
-
-#include <spdlog/sinks/dup_filter_sink.h>
-#include <spdlog/sinks/qt_sinks.h>
-#include <spdlog/sinks/rotating_file_sink.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
-
-using MasterSink = spdlog::sinks::dup_filter_sink_st;
-using QtSink     = spdlog::sinks::qt_color_sink_st;
-using FileSink   = spdlog::sinks::rotating_file_sink_st;
-using StdOutSink = spdlog::sinks::stdout_color_sink_st;
-
-#ifdef Q_OS_WIN
-#	include <spdlog/sinks/msvc_sink.h>
-
-using DebuggerSink = spdlog::sinks::msvc_sink_st;
-#endif
-
-using namespace mumble;
-
-static std::shared_ptr< MasterSink > masterSink;
-
-static void qtMessageHandler(const QtMsgType type, const QMessageLogContext &, const QString &msg) {
-	switch (type) {
-		default:
-			log::trace("{}", qPrintable(msg));
-			break;
-		case QtDebugMsg:
-			log::debug("{}", qPrintable(msg));
-			break;
-		case QtInfoMsg:
-			log::info("{}", qPrintable(msg));
-			break;
-		case QtWarningMsg:
-			log::warn("{}", qPrintable(msg));
-			break;
-		case QtCriticalMsg:
-			log::error("{}", qPrintable(msg));
-			break;
-		case QtFatalMsg:
-			log::fatal("{}", qPrintable(msg));
-			break;
-	}
-}
-
-#ifdef NDEBUG
-static void addFileSink(MasterSink &parent) {
-	constexpr auto maxFiles = 3;
-	constexpr auto maxSize  = 5 * 1024 * 1024; // 5 MiB
-#	ifndef Q_OS_MACOS
-	const auto filePath = Global::get().qdBasePath.filePath(QLatin1String("Console.txt")).toStdString();
-	parent.add_sink(std::make_shared< FileSink >(filePath, maxSize, maxFiles));
-#	else
-	std::string filePath = "/Library/Logs/Mumble.log";
-	if (const char *homePath = std::getenv("HOME")) {
-		filePath.insert(0, homePath);
-		parent.add_sink(std::make_shared< FileSink >(filePath, maxSize, maxFiles));
-	}
-#	endif
-#else
-static void addFileSink(MasterSink &) {
-#endif
-}
-
-void log::init(QTextEdit *textBox) {
-	// Skips the message if the previous one is identical and less than 5 seconds have passed.
-	masterSink = std::make_shared< MasterSink >(std::chrono::seconds(5));
-#ifdef Q_OS_WIN
-	masterSink->add_sink(std::make_shared< DebuggerSink >());
-#endif
-	masterSink->add_sink(std::make_shared< StdOutSink >());
-
-	addFileSink(*masterSink);
-
-	if (textBox) {
-		// Only display up to 1000 lines.
-		masterSink->add_sink(std::make_shared< QtSink >(textBox, 1000));
-	}
-
-	auto logger = std::make_shared< spdlog::logger >("Main", masterSink);
-
-	// TODO: Ideally we should add a user option, perhaps along with a launch parameter, to set the log level.
-	// However, the messages across the codebase are very inconsistent in terms of right now.
-	// For example, there are a lot of debug messages that should be info instead.
-	// Thus, for the time being let's show all messages regardless of their advertised level.
-	logger->set_level(spdlog::level::trace);
-	logger->set_pattern("%^<%L>%$%Y-%m-%d %H:%M:%S.%e %v");
-
-	set_default_logger(std::move(logger));
-
-	qInstallMessageHandler(qtMessageHandler);
-}
-
-void log::prepareToExit() {
-	debug("Preparing to exit...");
-
-	auto &sinks = masterSink->sinks();
-
-	// We iterate through the (sub)sinks to find the Qt one (if present).
-	// It must be removed because the widget it writes into is going to be deleted soon.
-	for (auto iter = sinks.cbegin(); iter != sinks.cend(); ++iter) {
-		auto textBoxSink = std::dynamic_pointer_cast< QtSink >(*iter);
-		if (textBoxSink) {
-			masterSink->sinks().erase(iter);
-			return;
-		}
-	}
-}

src/CMakeLists.txt
@@ -66,6 +66,7 @@ set(SHARED_SOURCES
 	"HTMLFilter.cpp"
 	"License.cpp"
 	"LogEmitter.cpp"
+	"Logger.cpp"
 	"MumbleProtocol.cpp"
 	"OSInfo.cpp"
 	"PasswordGenerator.cpp"
@@ -101,6 +102,7 @@ set(SHARED_HEADERS
 	"HTMLFilter.h"
 	"License.h"
 	"LogEmitter.h"
+	"Logger.h"
 	"MumbleProtocol.h"
 	"Net.h"
 	"OSInfo.h"

src/Logger.h
@@ -6,16 +6,18 @@
 #ifndef MUMBLE_LOGGER_H_
 #define MUMBLE_LOGGER_H_
 
-#include <memory>
-
 #include <spdlog/spdlog.h>
+
+#include <memory>
 
 class QTextEdit;
 
 namespace mumble {
 namespace log {
-	void init(QTextEdit *textBox = nullptr);
-	void prepareToExit();
+	constexpr const char *MainLoggerName = "Main";
+
+	void init(spdlog::level::level_enum logLevel = spdlog::level::trace);
+	void addSink(std::shared_ptr< spdlog::sinks::sink > sink);
 
 	template< typename... Args > static void inline trace(spdlog::format_string_t< Args... > fmt, Args &&... args) {
 		spdlog::trace(fmt, std::forward< Args >(args)...);

src/mumble/CMakeLists.txt
@@ -170,7 +170,6 @@ set(MUMBLE_SOURCES
 	"Log.cpp"
 	"Log.h"
 	"Log.ui"
-	"Logger.cpp"
 	"LookConfig.cpp"
 	"LookConfig.h"
 	"LookConfig.ui"
@@ -387,7 +386,7 @@ add_custom_command(
 target_sources(mumble PRIVATE "ApplicationPalette.h")
 target_include_directories(mumble PRIVATE "${CMAKE_CURRENT_BINARY_DIR}")
 
-target_link_libraries(mumble mumble_client_object_lib)
+target_link_libraries(mumble PRIVATE mumble_client_object_lib spdlog::spdlog)
 
 target_compile_definitions(mumble_client_object_lib
 	PUBLIC

src/mumble/main.cpp
@@ -60,8 +60,13 @@
 #include <QtWidgets/QMessageBox>
 #include <QtWidgets/QTextBrowser>
 
+#include <algorithm>
 #include <iostream>
 #include <memory>
+
+#include <spdlog/sinks/dist_sink.h>
+#include <spdlog/sinks/qt_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 
 #ifdef USE_DBUS
 #	include <QtDBus/QDBusInterface>
@@ -84,6 +89,71 @@ using namespace mumble;
 
 extern void os_init();
 extern char *os_lang;
+
+using QtLogSink = spdlog::sinks::qt_color_sink_st;
+
+void initLog(QTextBrowser *textBox = nullptr) {
+	// TODO: Ideally we should add a user option, perhaps along with a launch parameter, to set the log level.
+	// However, the messages across the codebase are very inconsistent in terms of right now.
+	// For example, there are a lot of debug messages that should be info instead.
+	// Thus, for the time being let's show all messages regardless of their advertised level.
+	log::init(spdlog::level::trace);
+
+	// Set up file log
+	using FileSink = spdlog::sinks::rotating_file_sink_st;
+	// 5MB
+	static constexpr std::size_t maxSize  = 5 * 1024 * 2024;
+	static constexpr std::size_t maxFiles = 3;
+
+#ifndef Q_OS_MACOS
+	const auto filePath = Global::get().qdBasePath.filePath(QLatin1String("Console.txt")).toStdString();
+	log::addSink(std::make_shared< FileSink >(filePath, maxSize, maxFiles));
+#else
+	std::string filePath = "/Library/Logs/Mumble.log";
+	if (const char *homePath = std::getenv("HOME")) {
+		filePath.insert(0, homePath);
+		log::addSink(std::make_shared< FileSink >(filePath, maxSize, maxFiles));
+	}
+#endif
+
+	if (textBox) {
+		static constexpr std::size_t maxLines = 1000;
+		log::addSink(std::make_shared< QtLogSink >(textBox, maxLines));
+	}
+}
+
+void prepareLogForShutdown() {
+	log::debug("Preparing to exit...");
+
+	std::shared_ptr< spdlog::logger > logger = spdlog::get(log::MainLoggerName);
+
+	if (!logger) {
+		return;
+	}
+
+	auto &sinks = logger->sinks();
+
+	// Remove Qt sinks because the widgets they write to are going to be deleted soon
+	auto remove_qt_sinks = [](auto &container) {
+		container.erase(
+			std::remove_if(container.begin(), container.end(),
+						   [](const spdlog::sink_ptr &sink) { return std::dynamic_pointer_cast< QtLogSink >(sink); }),
+			container.end());
+	};
+
+	remove_qt_sinks(sinks);
+	for (spdlog::sink_ptr &sink : sinks) {
+		auto st_dist_sink = std::dynamic_pointer_cast< spdlog::sinks::dist_sink_st >(sink);
+		auto mt_dist_sink = std::dynamic_pointer_cast< spdlog::sinks::dist_sink_mt >(sink);
+
+		if (st_dist_sink) {
+			remove_qt_sinks(st_dist_sink->sinks());
+		}
+		if (mt_dist_sink) {
+			remove_qt_sinks(mt_dist_sink->sinks());
+		}
+	}
+}
 
 QPoint getTalkingUIPosition() {
 	QPoint talkingUIPos = QPoint(0, 0);
@@ -195,7 +265,7 @@ int main(int argc, char **argv) {
 	}
 
 	auto logBox = new QTextBrowser();
-	log::init(logBox);
+	initLog(logBox);
 	Global::get().c = new DeveloperConsole(logBox);
 
 	os_init();
@@ -846,7 +916,7 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	log::prepareToExit();
+	prepareLogForShutdown();
 
 	QCoreApplication::processEvents();
 

commit 4b1f3fc5f30dec1a8ebee55d695b1e2e4099a3c9

FIX: Apply logger format to each sink explicitly
This fixes our format not being applied to the Qt sink.

For reference, the issue was introduced in 2b19d746f959b53541f3f904cdd7d42b1f06a6e8 because the sink is now added after the logger is initialized.

src/Logger.cpp
@@ -55,6 +55,8 @@ void log::addSink(std::shared_ptr< spdlog::sinks::sink > sink) {
 		return;
 	}
 
+	sink->set_pattern("%^<%L>%$%Y-%m-%d %H:%M:%S.%e %v");
+
 	masterSink->add_sink(std::move(sink));
 }
 
@@ -62,14 +64,13 @@ void log::init(spdlog::level::level_enum logLevel) {
 	// Skips the message if the previous one is identical and less than 5 seconds have passed.
 	masterSink = std::make_shared< MasterSink >(std::chrono::seconds(5));
 #ifdef Q_OS_WIN
-	masterSink->add_sink(std::make_shared< DebuggerSink >());
+	addSink(std::make_shared< DebuggerSink >());
 #endif
-	masterSink->add_sink(std::make_shared< StdOutSink >());
+	addSink(std::make_shared< StdOutSink >());
 
 	auto logger = std::make_shared< spdlog::logger >(MainLoggerName, masterSink);
 
 	logger->set_level(logLevel);
-	logger->set_pattern("%^<%L>%$%Y-%m-%d %H:%M:%S.%e %v");
 
 	set_default_logger(std::move(logger));
 