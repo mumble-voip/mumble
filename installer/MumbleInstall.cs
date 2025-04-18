759af18c30611f1d51afea687a4be15245b83ad6
sqwishy <somebody@froghat.ca>
2025-04-18 15:51:31 -0700

    FEAT(installer): bundle c++ redistributables
    After building each server and client msi, the installer script creates
    an exe bundle that includes a step to download and install the visual
    c++ redistributables.

--- installer/MumbleInstall.cs
+++ installer/MumbleInstall.cs
@@ -4,7 +4,12 @@
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
 using System;
+using System.Xml;
+using System.Xml.Linq;
+using System.Collections.Generic;
 using WixSharp;
+using WixSharp.Bootstrapper;
+using WixSharp.CommonTasks;
 
 // base class with info across installers
 public class MumbleInstall : Project {
@@ -23,5 +28,71 @@ public class MumbleInstall : Project {
 		this.ControlPanelInfo.ProductIcon = @"..\icons\mumble.ico";
 		this.ControlPanelInfo.UrlInfoAbout = "https://mumble.info";
 		this.Properties = new Property[] { allUsersProp };
+	}
+
+	public Bundle BundleMsi(string msiPath) {
+		var bootstrapper = new Bundle(
+				this.Name,
+				new MsiPackage(msiPath)
+				{ },
+				new ExePackage()
+				{
+					Id = "VCREDIST_EXE",
+					Name = "VC_redist.x64.exe",
+					SourceFile = @"..\VC_redist.x64.exe",
+					DisplayName = "Microsoft Visual C++ 2015-2022 Redistributable (x64)",
+					/* I couldn't find a page from Microsoft that lists permalinks to particular versions
+					 * of the redistributables. I could only find a link that redirects you to a link for
+					 * the latest redistributable version. The page is at [1] and the link is [2].
+					 *
+					 * The VC_redist.x64.exe downloaded from the `DownloadUrl` value below should be
+					 * placed in the build/installer/client folder, since WixSharp will use the file
+					 * to include a hash in the installer to verify the download at the time of install.
+					 *
+					 * The URL in DownloadUrl is for version 14.42.34438.0
+					 *
+					 * [1] https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170#latest-microsoft-visual-c-redistributable-version
+					 * [2] https://aka.ms/vs/17/release/vc_redist.x64.exe */
+					DownloadUrl = "https://download.visualstudio.microsoft.com/download/pr/285b28c7-3cf9-47fb-9be8-01cf5323a8df/8F9FB1B3CFE6E5092CF1225ECD6659DAB7CE50B8BF935CB79BFEDE1F3C895240/VC_redist.x64.exe",
+					/* This condition being true should mean that it is already installed. */
+					DetectCondition = "VCREDIST_INSTALLED >= VCREDIST_REQUIRED",
+					InstallCommand = "/install /quiet /norestart /ChainingPackage \"[WixBundleName]\"",
+					/* I don't think this has anything to do with compression.
+					 * A true value means the exe is "embedded", false means its "external".
+					 * Since this is a download, it is not embedded in the bundle. */
+					Compressed = false,
+					/* By default, if the installer does not have an internet connection, the download
+					 * will fail and the entire install will fail. Setting Vital to false means that
+					 * if the install for the redistributables can fail silently and Mumble's install
+					 * will continue. */
+					Vital = false,
+					/* Permanent just means it won't uninstall this when uninstalling Mumble.
+					 * This will still show up in Add/Remove Programs and can be removed there,
+					 * even when Permanent is true. */
+					Permanent = true,
+				});
+		bootstrapper.Variables = new[] {
+				new Variable("VCREDIST_REQUIRED", "14.0.0.0")
+				{ Type = WixSharp.VariableType.version }
+				};
+		// Visual C++ 14 2015-2022 Redistributable (x64)
+		bootstrapper.AddWixFragment(
+				"Wix/Bundle",
+				new UtilProductSearch
+				{
+					UpgradeCode = "{36F68A90-239C-34DF-B58C-64B30153CE35}",
+					Variable = "VCREDIST_INSTALLED"
+				});
+
+		bootstrapper.SetVersionFromFile(msiPath);
+		bootstrapper.UpgradeCode = this.UpgradeCode ?? Guid.Empty;
+		/* The options UI allows specifying an alternate install path,
+		 * SuppressOptionsUI hides the button for that screen. */
+		bootstrapper.Application.SuppressOptionsUI = true;
+		bootstrapper.Application.LicensePath = @"..\..\licenses\Mumble.rtf";
+		bootstrapper.Application.LogoFile = @"..\dlgbmp.bmp";
+		bootstrapper.Application.ThemeFile = @"..\Theme.xml";
+		bootstrapper.Include(WixExtension.Util);
+		return bootstrapper;
 	}
 }
