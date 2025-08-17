042e6bfbaa4ba79181f2b95708c61408040da09e
Robert Adam <dev@robert-adam.de>
2025-08-17 17:32:07 +0200

    CI(ga): Add static Linux instance

--- .github/actions/install-dependencies/install_ubuntu_static_x86_64.sh
+++ .github/actions/install-dependencies/install_ubuntu_static_x86_64.sh
@@ -9,29 +9,14 @@ sudo apt -y install \
 	build-essential \
 	g++-multilib \
 	ninja-build \
-	libcap-dev \
-	libdbus-1-dev \
-	libxi-dev \
-	libxkbcommon-dev \
-	libxkbcommon-x11-dev \
-	libxcb-icccm4-dev \
-	libxcb-image0-dev \
-	libxcb-keysyms1-dev \
-	libxcb-render-util0-dev \
-	libxcb-xinerama0-dev \
-	libxcb-xinput-dev \
-	libxcb-xkb-dev \
-	libasound2-dev \
-	libpulse-dev \
+	`# Still required for qtbase vcpkg package` \
+	'^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev libegl1-mesa-dev \
+	`# TODO: can we get rid of these by replacing with vcpkg packages?` \
+	libsm-dev \
 	libspeechd-dev \
-	libgl1-mesa-dev \
 	libavahi-compat-libdnssd-dev \
-	libglu1-mesa-dev \
-	mesa-common-dev \
-	libxrandr-dev \
-	libxxf86vm-dev \
-	libgl-dev
-
+	libasound2-dev
+	
 
 if [[ "$MUMBLE_ENVIRONMENT_SOURCE" == "" ]]; then
 	echo "MUMBLE_ENVIRONMENT_SOURCE not set!"
@@ -48,7 +33,7 @@ fi
 
 envDir="$MUMBLE_ENVIRONMENT_DIR"
 
-if [[ -d "$envDir" && -n "$(ls -A '$envDir')" ]]; then
+if [[ -d "$envDir" && -n "$(ls -A "$envDir")" ]]; then
 	echo "Environment is cached"
 else
 	sudo apt install aria2
@@ -64,7 +49,7 @@ else
 
 	"$(dirname $0)/extractWithProgress.sh" "$envArchive" "$envDir"
 
-	if [[ ! -d "$envDir" || -n "$(ls -A '$envDir')" ]]; then
+	if [[ ! -d "$envDir" || -z "$(ls -A "$envDir")" ]]; then
 		echo "Environment did not follow expected form"
 		ls -al "$envDir"
 		exit 1
