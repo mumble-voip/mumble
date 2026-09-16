afede8c550c5125171baa58afce912de4292657d
Davide Beatrici <git@davidebeatrici.dev>
2026-09-16 06:04:17 +0200

    REFAC(client): Introduce min() and max() in VolumeSliderWidgetAction

--- src/mumble/VolumeSliderWidgetAction.h
+++ src/mumble/VolumeSliderWidgetAction.h
@@ -19,6 +19,9 @@ class VolumeSliderWidgetAction : public QWidgetAction {
 public:
 	VolumeSliderWidgetAction(QWidget *parent = nullptr);
 
+	static constexpr auto min() { return -30; }
+	static constexpr auto max() { return 30; }
+
 protected:
 	qt_unique_ptr< QWidget > m_widget;
 	QSlider *m_volumeSlider;
