818c1c0575d03689ddcc67e3551bf13693d9c38f
Hosted Weblate <hosted@weblate.org>
2026-09-17 11:51:42 +0200

    TRANSLATION: Update Turkish translation
    Currently translated at 100.0% (2387 of 2387 strings)
    
    Co-authored-by: Oğuz Ersen <oguz@ersen.moe>
    Translate-URL: https://hosted.weblate.org/projects/mumble/mumble-client/tr/
    Translation: Mumble/Mumble Client

--- src/mumble/mumble_tr.ts
+++ src/mumble/mumble_tr.ts
@@ -5211,11 +5211,11 @@ Bu ayar sadece yeni mesajlara uygulanır, zaten görüntülenmiş olanlar öncek
     </message>
     <message>
         <source>Whether to change the local volume adjustment for the selected user when scrolling the mouse wheel up (increase) or down (decrease) while Control is pressed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Control tuşu basılıyken fare tekerleği yukarı (artırma) veya aşağı (azaltma) kaydırıldığında, seçilen kullanıcı için yerel ses ayarının değiştirilip değiştirilmeyeceği.</translation>
     </message>
     <message>
         <source>CTRL + Scroll to change selected user&apos;s volume</source>
-        <translation type="unfinished"></translation>
+        <translation>CTRL tuşuna basıp kaydırarak seçili kullanıcının ses seviyesini değiştir</translation>
     </message>
 </context>
 <context>
